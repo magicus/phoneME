41be77f1e7269d26434836ef3244abe7018132e5
hinkmond <hinkmond@6dfe35fe-931f-0410-af5f-a91b034811cd>
2006-12-09 00:53:27 +0000

    Originating revision: r519 | kwwong | 2006-12-07 12:00:39 -0800 (Thu, 07 Dec 2006) | 2 lines
    Updated copyright.  Reviewed by Chris

--- branches/resyncs/post-launch-resync/build/win32/defs_midp.mk
+++ branches/resyncs/post-launch-resync/build/win32/defs_midp.mk
@@ -1,10 +1,27 @@
 #
-# @(#)defs_midp.mk	1.0 06/11/02
+# Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
 #
-# Copyright © 2006 Sun Microsystems, Inc. All rights reserved.  
-# SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 only, as published by the Free Software Foundation.
 #
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License version 2 for more details (a copy is
+# included at /legal/license.txt).
 #
+# You should have received a copy of the GNU General Public License
+# version 2 along with this work; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+# Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+# Clara, CA 95054 or visit www.sun.com if you need additional
+# information or have any questions.
+#
+
 GNU_TOOLS_BINDIR  =
 
 #
@@ -21,7 +38,7 @@ PCSL_MAKE_OPTIONS = USE_CYGWIN=true
 MIDP_MAKEFILE_DIR = build/$(WIN32_PLATFORM)
 
 CONFIGURATION_OVERRIDE	= \
-        c:/ws/cyclops/midp_cdc/src/configuration/wince/sp176x220.xml
+        $(MIDP_DIR)/src/configuration/wince/sp176x220.xml
 
 MIDP_OBJECTS      = \
         $(MIDP_OUTPUT_DIR)/obj$(DEBUG_POSTFIX)/$(TARGET_CPU)/*.o \
