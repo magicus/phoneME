9f31acf571975726a61792018eb10939236828f6
ritus <ritus@6dfe35fe-931f-0410-af5f-a91b034811cd>
2006-12-18 17:15:07 +0000

    Created a directory and added first pass of protocol handlers for MIDP GCF. These classes extend from their respective CDC GCF protocol handler. Reviewed by Steve.

---
+++ src/protocol/cdc_application/classes/com/sun/midp/io/j2me/datagram/Protocol.java
@@ -0,0 +1,13 @@
+package com.sun.midp.io.j2me.datagram;
+
+public class Protocol extends com.sun.cdc.io.j2me.datagram.Protocol {
+
+    /*
+     * throws SecurityException if MIDP permission check fails 
+    */
+    private void checkMIDPPermission(String host, int port) {
+        //The actual MIDP permission checks happens here
+    }
+
+
+}
