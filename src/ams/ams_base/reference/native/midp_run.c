b9029e5f859a75d0140c49d71d1003998f7aef3a
alexey_z <alexey_z@6dfe35fe-931f-0410-af5f-a91b034811cd>
2006-12-21 17:52:57 +0000

    Minor change.

--- src/ams/ams_base/reference/native/midp_run.c
+++ src/ams/ams_base/reference/native/midp_run.c
@@ -390,8 +390,11 @@ midpInitializeUI(void) {
         return -1;
     }
 
-    lcdlf_ui_init();
-    return 0;
+    if (0 == lcdlf_ui_init()) {
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
 /**
