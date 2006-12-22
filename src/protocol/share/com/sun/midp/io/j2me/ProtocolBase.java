73dd7f90f4ae59d20afd091bdf6274722f184ab4
mishab <mishab@6dfe35fe-931f-0410-af5f-a91b034811cd>
2006-12-22 10:58:25 +0000

    Resolve CRs 6503626 and 6503600 Merged from jsr120-mr2-cr-6503155 Reviewed by Sergey, thanks!

--- src/protocol/share/com/sun/midp/io/j2me/ProtocolBase.java
+++ src/protocol/share/com/sun/midp/io/j2me/ProtocolBase.java
@@ -234,12 +234,29 @@ public abstract class ProtocolBase implements MessageConnection,
 
     /**
      * Ensures that the connection is open.
-     * @exception InterruptedIOException if the connection is closed
+     * @exception IOException if the connection is closed
      */
-    public void ensureOpen() throws InterruptedIOException {
+    public void ensureOpen() throws IOException {
 	if (!open) {
-	    throw new InterruptedIOException("Connection closed");
+	    throw new IOException("Connection closed");
 	}
+    }
+
+    /**
+     * Generates InterruptedIOException when connection is closed.
+     * @param ex input IOException
+     * @param name name of operation: sending or receiving
+     * @exception IOException if the connection is not closed
+     */
+    protected void io2InterruptedIOExc(IOException ex, String name) 
+        throws IOException, InterruptedIOException {
+        try {
+            ensureOpen();
+        } catch (IOException ioe) {
+            throw new InterruptedIOException("Connection closed " +
+                                         "during " + name);
+        }
+        throw ex;
     }
 
     /**
@@ -311,9 +328,9 @@ public abstract class ProtocolBase implements MessageConnection,
         /* Kill listener when need */
         if (needStopReceiver) {
             String save_appID = getAppID();
-            setAppID(null);
             /* Close thread without deregistering */
             close00(connHandle, 0);
+            setAppID(null);
             try {
                 m_listenerThread.join();
             } catch (InterruptedException ie) {
--- src/protocol/sms/classes/com/sun/midp/io/j2me/sms/Protocol.java
+++ src/protocol/sms/classes/com/sun/midp/io/j2me/sms/Protocol.java
@@ -368,6 +368,8 @@ public class Protocol extends ProtocolBase {
 	} catch (InterruptedIOException ex) {
             length = 0;
             throw new InterruptedIOException("MessageConnection is closed");
+        } catch (IOException ex) {
+            io2InterruptedIOExc(ex, "receiving");
 	} finally {
 	    if (length < 0) {
 		throw new InterruptedIOException("Connection closed error");
@@ -489,11 +491,15 @@ public class Protocol extends ProtocolBase {
             throw new IllegalArgumentException("Message type not supported");
 	}
 
-        int status = send0(connHandle, messageType,
+        try {
+            send0(connHandle, messageType,
                            url.host,
                            url.port,
 			   sourcePort,
                            msgBuffer);
+        } catch (IOException ex) {
+            io2InterruptedIOExc(ex, "sending");
+        }
     }
 
     /**
@@ -989,7 +995,7 @@ public class Protocol extends ProtocolBase {
                                     String host,
 				    int destPort,
 				    int sourcePort,
-				    byte[] message);
+				    byte[] message) throws IOException;
 
     /**
      * Receives SMS message
