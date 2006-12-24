d92699957e895795d1fb82f7135318f89c7b1356
stephen_flores <stephen_flores@6dfe35fe-931f-0410-af5f-a91b034811cd>
2006-12-24 01:17:11 +0000

    The MIDletEventConsumer interface no longer has to be implemented in order to preempt the displays of a MIDlet suite.

--- src/pki/classes/com/sun/satsa/pki/Dialog.java
+++ src/pki/classes/com/sun/satsa/pki/Dialog.java
@@ -40,7 +40,7 @@ import javax.microedition.lcdui.*;
 /**
  * This class represents simple dialog form.
  */
-class Dialog implements CommandListener, MIDletEventConsumer {
+class Dialog implements CommandListener {
 
     /** Answer that indicates that the dialog was cancelled. */
     public static final int CANCELLED = -1;
@@ -106,7 +106,7 @@ class Dialog implements CommandListener, MIDletEventConsumer {
 	    displayEventHandler  = DisplayEventHandlerFactory
 		.getDisplayEventHandler(token);
 	}
-        preemptToken = displayEventHandler.preemptDisplay(this, form, true);
+        preemptToken = displayEventHandler.preemptDisplay(form, true);
 
         synchronized (this) {
             if (preemptToken == null) {
@@ -136,42 +136,5 @@ class Dialog implements CommandListener, MIDletEventConsumer {
             displayEventHandler.donePreempting(preemptToken);
             notify();
         }
-    }
-
-    /**
-     * Start the currently suspended state. This is not apply to
-     * this dialog.
-     *
-     * @param midlet midlet that the event applies to
-     */
-    public void startMIDlet(MIDletPeer midlet) {}
-
-    /**
-     * Pause the current foreground MIDlet and return to the
-     * AMS or "selector" to possibly run another MIDlet in the
-     * currently active suite.
-     * <p>
-     * This is not apply to this dialog.
-     *
-     * MIDletEventConsumer I/F method.
-     */
-    public void handleMIDletPauseEvent() {}
-
-    /**
-     * Destroy the MIDlet given midlet.
-     * <p>
-     * This is not apply to this dialog.
-     *
-     * MIDletEventConsumer I/F method.
-     */
-    public void handleMIDletDestroyEvent() {}
-
-    /**
-     * Activate a MIDlet.
-     *
-     * MIDletEventConsumer I/F method.
-     */
-    public void handleMIDletActivateEvent() {
-	/* RFC - not needed for Dialog interaction. */
     }
 }
