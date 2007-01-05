094ed60094e7a0e0f762701c6941f5f890455d23
rajaa <rajaa@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-01-05 00:39:39 +0000

    Tag phoneme_advanced-mr2-dev-b05

---
+++ cdc/src/share/native/sun/misc/Version.c
@@ -0,0 +1,36 @@
+/*
+ * @(#)Version.c	1.7 06/10/10
+ *
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.  
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER  
+ *   
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License version  
+ * 2 only, as published by the Free Software Foundation.   
+ *   
+ * This program is distributed in the hope that it will be useful, but  
+ * WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  
+ * General Public License version 2 for more details (a copy is  
+ * included at /legal/license.txt).   
+ *   
+ * You should have received a copy of the GNU General Public License  
+ * version 2 along with this work; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+ * 02110-1301 USA   
+ *   
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa  
+ * Clara, CA 95054 or visit www.sun.com if you need additional  
+ * information or have any questions. 
+ *
+ */
+
+#include "jni.h"
+#include "sun_misc_Version.h"
+#include "generated/javavm/include/build_defs.h"
+
+JNIEXPORT jstring JNICALL
+Java_sun_misc_Version_getBuildOptionString(JNIEnv* env, jclass cls)
+{
+    return (*env)->NewStringUTF(env, CVM_BUILD_OPTIONS);
+}
