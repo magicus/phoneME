5eb759c8cdc38802d97126ff080b2753d1a296d9
hinkmond <hinkmond@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-01-19 20:34:26 +0000

    Test commit #03 in sandbox to test svn commit notifications message filtering.

--- sandbox/hinkmond/tutorial-10jan2007/code-rev-ex/A.java
+++ sandbox/hinkmond/tutorial-10jan2007/code-rev-ex/A.java
@@ -3,7 +3,7 @@ public class A
 
     public static void status() {
 
-        System.out.println("In A.java: 10jan2007 branch bug fix");
+        System.out.println("In A.java: 19Jan2007 branch bug fix");
         System.out.println("In A.java: newwork-02 branch bug fix");
         System.out.println("In A.java: Change number bug fix 999999");
 
