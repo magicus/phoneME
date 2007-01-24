72f42cc585a93afc38345f8906bd90051bd235a5
chihiro_saito <chihiro_saito@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-01-24 17:42:17 +0000

    Cleanup of sun.mtask.xlet classes for them to work with the jump app container. Reviewed by Nedim.

--- trunk/build/solaris/defs.mk
+++ trunk/build/solaris/defs.mk
@@ -105,16 +105,11 @@ override CVM_INCLUDE_JUMP = true
 endif
 
 ifeq ($(CVM_MTASK), true)
-CVM_SRCDIRS += \
-	$(CVM_SHAREROOT)/native/sun/mtask
+CLASSLIB_CLASSES += \
+       sun.mtask.Warmup
 CVM_DEFINES   += -DCVM_MTASK
 CVM_SHAREOBJS_SPACE += \
-	mtask.o \
-	Listener.o
-CLASSLIB_CLASSES += \
-	sun.mtask.Warmup \
-	sun.mtask.AppModelManager \
-	sun.mtask.Listener
+	mtask.o 
 endif
 
 ifeq ($(CVM_JIT), true)
