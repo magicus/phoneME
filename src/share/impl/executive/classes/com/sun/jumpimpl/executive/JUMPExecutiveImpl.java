a3b578ae8e726eca5da7ad492220df3250af354e
zandig <zandig@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-01-30 19:14:13 +0000

    First implementation of the SimpleBasisAMS JUMP presentation mode.  A "presentation mode", a term which may or may not exist in the future, refers to an AMS type of application which uses JUMP APIs while running in the executive process.  A presentation mode usually contains an interactive UI, but a graphical UI is not required.
    The provided SimpleBasisAMS can be used as a base presentation mode class which can be extended for more advanced features.  Note, the functionality of SimpleBasisAMS is still evolving and deliberately has no specification.  It is provided as a simple way to launch, kill, and switch between JUMP applications.  Currently, the only JUMP applications which can be run are ones installed with the JUMPInstallerTool, which is located in the installer module and has instructions for it's use within the JUMPInstallerTool.java file.
    
    To create a presentation mode within JUMP:
    
    1) Create a class which implements the com.sun.jump.presentation.JUMPLauncher interface.
    2) Set the jump.launcher property within build/modules-config.properties to this class, including full package name.
    3) Run the JUMP server and the JUMP executive, the presentation mode class will automatically run within the JUMP executive process.
    
    To use the provided SimpleBasisAMS presentation mode:
    
    1) Create a content store repository and use the JUMPInstallerTool (see instructions within JUMPInstallerTool.java ) to install applications.
    
    2) Leave the jump.launcher property in build/modules-config.properties to com.sun.jumpimpl.presentation.simplebasis.SimpleBasisAMS
    
    To not use any presentation mode by default when the JUMP executive starts:
    
    1) Remove or comment out the jump.launcher property in build/modules-config.properties

--- src/share/impl/executive/classes/com/sun/jumpimpl/executive/JUMPExecutiveImpl.java
+++ src/share/impl/executive/classes/com/sun/jumpimpl/executive/JUMPExecutiveImpl.java
@@ -26,6 +26,7 @@
 
 package com.sun.jumpimpl.executive;
 
+import com.sun.jump.presentation.JUMPLauncher;
 import com.sun.jump.executive.JUMPExecutive;
 import com.sun.jump.executive.JUMPUserInputManager;
 import com.sun.jump.executive.JUMPIsolateProxy;
@@ -57,11 +58,11 @@ import java.io.BufferedInputStream;
 public class JUMPExecutiveImpl extends JUMPExecutive {
     private JUMPProcessProxyImpl pp;
     private JUMPOSInterface os;
-
+    
     private static void
-    overrideDefaultConfig(String fname) {
+            overrideDefaultConfig(String fname) {
         Properties props = new Properties();
-
+        
         InputStream in = null;
         try {
             in = new BufferedInputStream(new FileInputStream(fname));
@@ -77,102 +78,125 @@ public class JUMPExecutiveImpl extends JUMPExecutive {
                 e.printStackTrace();
             }
         }
-
+        
         JUMPFactories.getDefaultConfig().putAll(props);
     }
-
+    
     private void
-    handleCommandLine(String[] args) {
+            handleCommandLine(String[] args) {
         for(int i = 0; i < args.length; ++i) {
             if("--config-file".equals(args[i])) {
                 if(!(++i < args.length)) {
                     throw new IllegalArgumentException(
-                        "configuration file not specified");
+                            "configuration file not specified");
                 }
                 overrideDefaultConfig(args[i]);
             }
         }
     }
-
+    
     public JUMPExecutiveImpl() {
-	super();
+        super();
     }
-
+    
     /*
      * Main entry point to the executive
      */
     public static void main(String[] args) {
-	JUMPExecutiveImpl jei = new JUMPExecutiveImpl();
-
+        JUMPExecutiveImpl jei = new JUMPExecutiveImpl();
+        
         jei.handleCommandLine(args);
-
-	// Initialize os interface
-	new com.sun.jumpimpl.os.JUMPOSInterfaceImpl();
-
-	// Get critical objects
-	jei.os = JUMPOSInterface.getInstance();
-	jei.pp = JUMPProcessProxyImpl.createProcessProxyImpl(jei.os.getProcessID());
-
+        
+        // Initialize os interface
+        new com.sun.jumpimpl.os.JUMPOSInterfaceImpl();
+        
+        // Get critical objects
+        jei.os = JUMPOSInterface.getInstance();
+        jei.pp = JUMPProcessProxyImpl.createProcessProxyImpl(jei.os.getProcessID());
+        
         JUMPFactories.init();
-
-	if (false) {
-	    // Sample code to create blank isolate upon startup
-	    JUMPLifeCycleModuleFactory lcmf =
-		JUMPLifeCycleModuleFactory.getInstance();
-	    JUMPLifeCycleModule lcm = lcmf.getModule();
-	    JUMPIsolateProxy ip = lcm.newIsolate(JUMPAppModel.XLET);
-	    System.err.println("New isolate created="+ip);
-
-	    JUMPInstallerModuleFactory imf = 
-		JUMPInstallerModuleFactory.getInstance();
-	    JUMPInstallerModule xletInstaller = 
-		imf.getModule(JUMPAppModel.XLET);
-	    JUMPContent[] content = xletInstaller.getInstalled();
-	    if (content != null) {
-		for (int i = 0; i < content.length; i++) {
-		    JUMPApplication app = (JUMPApplication)content[i];
-		    System.err.println("App["+i+"] = "+app);
-		}
-		JUMPApplicationProxy appProxy = ip.startApp((JUMPApplication)content[0], null);
-		System.err.println("Executive started app="+appProxy);
-	    } else {
-		System.err.println("No content available");
-		System.exit(1);
-	    }
-	}
-
-	// Take it away, someone -- presentation mode?
-	try {
-	    Thread.sleep(0L);
-	} catch(Throwable e) {
-	}
+        
+        if (false) {
+            // Sample code to create blank isolate upon startup
+            JUMPLifeCycleModuleFactory lcmf =
+                    JUMPLifeCycleModuleFactory.getInstance();
+            JUMPLifeCycleModule lcm = lcmf.getModule();
+            JUMPIsolateProxy ip = lcm.newIsolate(JUMPAppModel.XLET);
+            System.err.println("New isolate created="+ip);
+            
+            JUMPInstallerModuleFactory imf =
+                    JUMPInstallerModuleFactory.getInstance();
+            JUMPInstallerModule xletInstaller =
+                    imf.getModule(JUMPAppModel.XLET);
+            JUMPContent[] content = xletInstaller.getInstalled();
+            if (content != null) {
+                for (int i = 0; i < content.length; i++) {
+                    JUMPApplication app = (JUMPApplication)content[i];
+                    System.err.println("App["+i+"] = "+app);
+                }
+                JUMPApplicationProxy appProxy = ip.startApp((JUMPApplication)content[0], null);
+                System.err.println("Executive started app="+appProxy);
+            } else {
+                System.err.println("No content available");
+                System.exit(1);
+            }
+        }
+        
+        // Take it away, someone -- presentation mode?
+        try {
+            JUMPLauncher l = getLauncher();
+            if (l != null) {
+                l.start();
+            }
+            Thread.sleep(0L);
+        } catch(Throwable e) {
+        }
     }
-
+    
+    public static JUMPLauncher getLauncher() {
+        JUMPLauncher launcher = null;
+        String launcherClass = (String)JUMPFactories.getDefaultConfig().get("jump.launcher");
+        if (launcherClass != null) {
+            try {
+                Class c = Class.forName(launcherClass);
+                Object obj = c.newInstance();
+                if (obj instanceof com.sun.jump.presentation.JUMPLauncher) {
+                    launcher = (JUMPLauncher) c.newInstance();
+                } else {
+                    System.err.println("Could not instantiate jump.launcher class: " + launcherClass);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return launcher;
+    }
+    
     public int
-    getProcessId() {
+            getProcessId() {
         return os.getProcessID();
     }
-
+    
     public JUMPMessageDispatcher
-    getMessageDispatcher() {
+            getMessageDispatcher() {
         return pp.getMessageDispatcher();
     }
-
+    
     public JUMPOutgoingMessage
-    newOutgoingMessage(String mesgType) {
+            newOutgoingMessage(String mesgType) {
         return pp.newOutgoingMessage(mesgType);
     }
-
+    
     public JUMPOutgoingMessage
-    newOutgoingMessage(JUMPMessage requestMessage) {
+            newOutgoingMessage(JUMPMessage requestMessage) {
         return pp.newOutgoingMessage(requestMessage);
     }
-
+    
     public JUMPMessage
-    newMessage(byte[] rawData) {
+            newMessage(byte[] rawData) {
         return pp.newMessage(rawData);
     }
-
+    
     public JUMPUserInputManager getUserInputManager() {
         return null;
     }
