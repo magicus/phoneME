cc0b56d4cd5e9768d2ffba80212fccfeec56914c
chihiro_saito <chihiro_saito@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-02-08 00:41:42 +0000

    Fix for 6521626.  Better system property initialization.  Also making the suite ID and the class name as a property in the MIDletApplication, as agreed at the jump/midp meeting.

--- src/ams/jump_application/classes/com/sun/midp/jump/installer/MIDLETInstallerImpl.java
+++ src/ams/jump_application/classes/com/sun/midp/jump/installer/MIDLETInstallerImpl.java
@@ -58,18 +58,35 @@ public class MIDLETInstallerImpl implements JUMPInstallerModule {
     }
     
     public void load(Map map) {
-       
+
         // Check for some system properties and set them if there's no default.
-        // Need to do this before initializing classes in com.sun.midp.*.
+        // Need to do this before initializing MIDletSuiteStorageAccessor.
+ 
+        // For "microedition.profiles" value.
+        // Set the property with the module configuration data if the value is provided.
+        // Else, check if the property is already set, and if not, set default.
+        String profilename = (String) map.get(midpProfileKey);
+	if (profilename != null) {
+            System.setProperty(midpProfileKey, profilename);
+        } else {
+            profilename = System.getProperty(midpProfileKey);
+            if (profilename == null || profilename.equals("") ) {
+               System.setProperty(midpProfileKey, "MIDP-2.0"); // Default
+            }
+	}     
 
-        String profilename = System.getProperty(midpProfileKey);
-        if (profilename == null) {
-            System.setProperty(midpProfileKey, "MIDP-2.1");
-        }
-
-        String homeDir = System.getProperty(midpHomeKey);
-        if (homeDir == null) {
-            System.setProperty(midpHomeKey, "midp/midp_fb");
+        // For "sun.midp.home.path" value.
+        // Set the property with the configuration data if the value is provided.
+        // Else, check if the property is already set, and if not, set default.
+        String homeDir = (String) map.get(midpHomeKey);
+	if (homeDir != null) {
+            System.setProperty(midpHomeKey, homeDir);
+        } else {
+            homeDir = System.getProperty(midpHomeKey);
+            if (homeDir == null || homeDir.equals("") ) {
+               String javahome = System.getProperty("java.home", ".");
+               System.setProperty(midpHomeKey, javahome + "/midp/midp_fb"); // Default
+            }
         }
 
         JumpInit.init();
