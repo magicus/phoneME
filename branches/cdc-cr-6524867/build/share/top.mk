94e712df5d073bb00a1b2d0cad45363213db52a5
NikitaGutsalov <NikitaGutsalov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-02-15 12:05:14 +0000

    defs_*.mk files inclusion order fixed

--- branches/cdc-cr-6524867/build/share/top.mk
+++ branches/cdc-cr-6524867/build/share/top.mk
@@ -372,6 +372,7 @@ include  ../share/defs.mk
 -include ../share/defs_midp.mk
 -include ../share/defs_jump.mk
 include ../share/defs_$(J2ME_CLASSLIB).mk
+-include ../share/defs_op.mk
 ifneq ($(OPT_PKGS_DEFS_FILES),)
 include $(OPT_PKGS_DEFS_FILES)
 endif
