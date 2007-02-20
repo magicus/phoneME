f4c0d74b5c9b3409a56c9ff49f15bcc98715c488
aka1278 <aka1278@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-02-20 15:20:49 +0000

    JSR120: Fix for build failure after CR 6486629 changes.

--- src/core/common/classes/com/sun/midp/wma/WMACleanupMonitor.java
+++ src/core/common/classes/com/sun/midp/wma/WMACleanupMonitor.java
@@ -113,11 +113,12 @@ public class WMACleanupMonitor implements MIDletProxyListListener {
      * @param externalAppId ID assigned by the external application manager
      * @param suiteId Suite ID of the MIDlet
      * @param className Class name of the MIDlet
-     * @param error start error code
+     * @param errorCode start error code
+     * @param errorDetails start error details
      */
     public void midletStartError(int externalAppId, int suiteId,
-                                 String className, int error) {
+                                 String className, int errorCode,
+                                 String errorDetails) {
     }
 
 }
-
