f34695090b6f4386f502bc4b0c06797cbc6faca3
alexey_z <alexey_z@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-02-21 11:52:56 +0000

    Common task of the Javacall applications was moved to ams_common_port library.

--- src/ams/example/ams_common_port/include/javaTask.h
+++ src/ams/example/ams_common_port/include/javaTask.h
@@ -3,43 +3,57 @@
  *
  * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
- * 2 only, as published by the Free Software Foundation.
- *
+ * 2 only, as published by the Free Software Foundation. 
+ * 
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
- * included at /legal/license.txt).
- *
+ * included at /legal/license.txt). 
+ * 
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
- *
+ * 02110-1301 USA 
+ * 
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
- * information or have any questions.
+ * information or have any questions. 
  */
 
+
+#ifndef _JAVA_TASK_H_
+#define _JAVA_TASK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * This interface is javacall-specific.
+ */
+ 
 #include <midp_jc_event_defs.h>
 /* #include <javacall_events.h> */
 #include <javacall_lifecycle.h>
-#include <runMidlet.h>
 
-/* unsigned char enable_java_debugger = 0; */
+/**
+ * An entry point of a thread devoted to run java.
+ */
+void JavaTask(void);
 
+/**
+ * This function is IMPORTED by the library.
+ */
 javacall_result
 midpHandleStartArbitraryArgEvent(
-        midp_jc_event_start_arbitrary_arg startArbitraryArgEvent) {
-    javacall_result res;
-
-    res = runMidlet(startArbitraryArgEvent.argc, startArbitraryArgEvent.argv);
-
-    javacall_lifecycle_state_changed(JAVACALL_LIFECYCLE_MIDLET_SHUTDOWN,
-                                     (res == 1) ? JAVACALL_OK: JAVACALL_FAIL);
-
-    return res;
+        midp_jc_event_start_arbitrary_arg startArbitraryArgEvent);
+	
+#ifdef __cplusplus
 }
+#endif
+
+#endif /* _JAVA_TASK_H_ */
