9594608d380633b0e59a3422ac3d479cbcfd8cb0
ritus <ritus@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-02-28 20:02:55 +0000

    Fixed CR 6529468 -Broken build fix - migrating GCF changes did not go to the correct MIDP repository.

--- src/protocol/cdc_application/classes/com/sun/midp/io/j2me/socket/Protocol.java
+++ src/protocol/cdc_application/classes/com/sun/midp/io/j2me/socket/Protocol.java
@@ -24,14 +24,62 @@
  * information or have any questions. 
  */
 
+
 package com.sun.midp.io.j2me.socket;
 
+import java.io.*;
+import java.net.*;
+import javax.microedition.io.*;
+
 public class Protocol extends com.sun.cdc.io.j2me.socket.Protocol {
+    
+    
+    /** Number of input streams that were opened. */
+    protected int iStreams = 0;
+    /**
+     * Maximum number of open input streams. Set this
+     * to zero to prevent openInputStream from giving out a stream in
+     * write-only mode.
+     */
+    protected int maxIStreams = 1;
+
+    /*
+     * Open the input stream if it has not already been opened.
+     * @exception IOException is thrown if it has already been
+     * opened.
+     */
+    public InputStream openInputStream() throws IOException {
+        if (maxIStreams == 0) {
+            throw new IOException("no more input streams available");
+        }
+        InputStream i = super.openInputStream();
+        maxIStreams--;
+        iStreams++;
+        return i;
+    }
+    
+    public DataInputStream openDataInputStream() throws IOException {
+        return new DataInputStream(openInputStream());
+    }
+
+    /* This class overrides the setSocketOption() to allow 0 as a valid value for sendBufferSize
+     * and receiveBufferSize. The underlying CDC networking layer considers 0 as illegal 
+     * value and throws IAE which causes the TCK test to fail.
+     */
+    public void setSocketOption(byte option,  int value) throws IllegalArgumentException, IOException {
+        if (option == SocketConnection.SNDBUF || option == SocketConnection.RCVBUF ) {
+            if (value == 0) {
+                value = 1;
+                super.setSocketOption(option, value);
+            }
+        }            
+        super.setSocketOption(option, value);
+    }
 
     /*
      * throws SecurityException if MIDP permission check fails 
-    */
-    private void checkMIDPPermission(String host, int port) {
+     */
+    protected void checkMIDPPermission(String host, int port) {
         //The actual MIDP permission check happens here
     }
 
