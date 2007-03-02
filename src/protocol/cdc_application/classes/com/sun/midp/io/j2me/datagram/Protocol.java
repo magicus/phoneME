c37350645076dea6776acc8683ec2594daa04ee9
osana <osana@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-02 17:32:08 +0000

    merged -r2887:2991 from midp/trunk

--- src/protocol/cdc_application/classes/com/sun/midp/io/j2me/datagram/Protocol.java
+++ src/protocol/cdc_application/classes/com/sun/midp/io/j2me/datagram/Protocol.java
@@ -31,7 +31,7 @@ public class Protocol extends com.sun.cdc.io.j2me.datagram.Protocol {
     /*
      * throws SecurityException if MIDP permission check fails 
     */
-    private void checkMIDPPermission(String host, int port) {
+    protected void checkMIDPPermission(String host, int port) {
         //The actual MIDP permission checks happens here
     }
 
