9b2d3df25bf9e5258045252e903c4f15b70f0b8f
tommay <tommay@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-02 05:41:07 +0000

    Sync up with trunk r2956.

--- src/share/impl/lifecycle/classes/com/sun/jumpimpl/module/lifecycle/OneLiveOnlyApplicationLifecycleModule.java
+++ src/share/impl/lifecycle/classes/com/sun/jumpimpl/module/lifecycle/OneLiveOnlyApplicationLifecycleModule.java
@@ -24,15 +24,19 @@
 
 package com.sun.jumpimpl.module.lifecycle;
 
+import com.sun.jump.executive.JUMPIsolateProxy;
+import com.sun.jump.module.isolatemanager.JUMPIsolateManagerModule;
+import com.sun.jump.module.isolatemanager.JUMPIsolateManagerModuleFactory;
 import com.sun.jump.module.lifecycle.JUMPApplicationLifecycleModule;
 import com.sun.jump.common.JUMPApplication;
 import com.sun.jump.executive.JUMPApplicationProxy;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
-public class OneLiveOnlyApplicationLifecycleModule 
-    implements JUMPApplicationLifecycleModule {
-
+public class OneLiveOnlyApplicationLifecycleModule
+        implements JUMPApplicationLifecycleModule {
+    
     private final HashMap mapAppToAppProxy = new HashMap();
     
     //
@@ -40,48 +44,68 @@ public class OneLiveOnlyApplicationLifecycleModule
     // information here.
     // Examples could be -- maximum number of instances allowed, etc.
     //
-    public void load(Map config) {}
-    public void unload() {}
-
+    public void load(Map config) {
+    }
+    
+    public void unload() {
+        mapAppToAppProxy.clear();
+    }
+    
     /*
      * Launch or return a running application for an installed
      * application, according to the lifecycle policy for this
      * lifecycle module.
      */
-    public JUMPApplicationProxy launchApplication(JUMPApplication app) 
-    {
-	// This implementation allows for one live instance only.
-	// Either create it, or return existing instance.
-	synchronized(this) {
-	    // launch or return existing proxy 
-	    // Maintain map of
-	    // JUMPApplication to existing running
-	    // JUMPApplicationProxy.
-	    // 
-	    JUMPApplicationProxy proxy = 
-		(JUMPApplicationProxy)mapAppToAppProxy.get(app);
-	    
-	    if (proxy == null) {
-		// Get a handle to the isolate manager.
-		// Create isolate, which returns JUMPIsolateProxy
-		// Create JUMPApplicationProxy by calling startApp()
-		// on the JUMPIsolateProxy
-		// ...
-		// And put returned app proxy in map 
-		mapAppToAppProxy.put(app, proxy);
-	    }
-	    
-	    return proxy;
-	}
+    public JUMPApplicationProxy launchApplication(JUMPApplication app, String args[]) {
+        // This implementation allows for one live instance only.
+        // Either create it, or return existing instance.
+        synchronized(this) {
+            
+            if (app == null) {
+                return null;
+            }
+            
+            // launch or return existing proxy
+            // Maintain map of
+            // JUMPApplication to existing running
+            // JUMPApplicationProxy.
+            //
+            JUMPApplicationProxy proxy =
+                    (JUMPApplicationProxy)mapAppToAppProxy.get(app);
+            
+            if (proxy == null) {
+                // Get a handle to the isolate manager.
+                JUMPIsolateManagerModuleFactory ismf = JUMPIsolateManagerModuleFactory.getInstance();
+                JUMPIsolateManagerModule ism = ismf.getModule();
+                
+                // Create isolate, which returns JUMPIsolateProxy
+                JUMPIsolateProxy ip = ism.newIsolate(app.getAppType());
+                
+                // Create JUMPApplicationProxy by calling startApp()
+                // on the JUMPIsolateProxy
+                proxy = ip.startApp(app, args);
+                
+                // ...
+                // And put returned app proxy in map
+                mapAppToAppProxy.put(app, proxy);
+            }
+            
+            return proxy;
+        }
     }
-
+    
     /**
      * Returns any running instances of a given JUMPApplication.
      * Can be found by iterating over mapAppToAppProxy.
      */
-    public JUMPApplicationProxy[] getApplications(JUMPApplication app)
-    {
-	return null;
+    public JUMPApplicationProxy[] getApplications(JUMPApplication app) {
+        JUMPApplicationProxy proxy[] = new JUMPApplicationProxy[1];
+        proxy[0] = (JUMPApplicationProxy)mapAppToAppProxy.get(app);
+        if (proxy[0] == null) {
+            return null;
+        } else {
+            return proxy;
+        }
     }
     
     
@@ -89,9 +113,13 @@ public class OneLiveOnlyApplicationLifecycleModule
      * Returns any running application instances for this lifecycle module
      * Can be found by iterating over mapAppToAppProxy.
      */
-    public JUMPApplicationProxy[] getApplications()
-    {
-	return null;
+    public JUMPApplicationProxy[] getApplications() {
+        Vector v =  (Vector)mapAppToAppProxy.values();
+        if (v == null) {
+            return null;
+        } else {
+            return (JUMPApplicationProxy[])v.toArray(new JUMPApplicationProxy[]{});
+        }
     }
 }
 
