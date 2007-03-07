63b5e2c496b354f4b9353bfd9600d501109c0dc5
ffg <ffg@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-07 15:23:59 +0000

    Modified CDC component for GCI to be built independently on PBP. GCI is built passing CVM_INCLUDE_GCI=true and GCI_DIR to make.

--- build/share/top.mk
+++ build/share/top.mk
@@ -373,6 +373,7 @@ include ../$(TARGET_OS)/top.mk
 include  ../share/defs.mk
 -include ../share/defs_midp.mk
 -include ../share/defs_jump.mk
+-include ../share/defs_gci.mk
 include ../share/defs_$(J2ME_CLASSLIB).mk
 -include ../share/defs_op.mk
 ifneq ($(OPT_PKGS_DEFS_FILES),)
@@ -384,6 +385,7 @@ endif
 include  ../share/rules.mk
 -include ../share/rules_midp.mk
 -include ../share/rules_jump.mk
+-include ../share/rules_gci.mk
 include ../share/rules_$(J2ME_CLASSLIB).mk
 ifneq ($(J2ME_PLATFORM),)
 include ../share/rules_$(J2ME_PLATFORM).mk
