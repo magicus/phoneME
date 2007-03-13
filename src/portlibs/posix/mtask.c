4b8bc040aeae06414ef9aee19fff561f381b0ba7
tommay <tommay@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-13 01:53:35 +0000

    Added calls to new function jumpProcessSetId() at init time and after forking.
    Reviewed by Nedim.

--- src/portlibs/posix/mtask.c
+++ src/portlibs/posix/mtask.c
@@ -1277,6 +1277,11 @@ waitForNextRequest(JNIEnv* env, ServerState* state)
 	    if ((pid = fork()) == 0) {
 		int mypid = getpid();
 
+		/* Remember the pid of our process for compatibility
+		   with non-Posix compliant systems on which getpid()
+		   returns a thread id. */
+		jumpProcessSetId(mypid);
+
 		/* Make sure each launched process knows the id of the
 		   executive */
                 if (amExecutive) {
@@ -1463,6 +1468,10 @@ MTASKserverInitialize(ServerState* state,
 {    
     const char* clist = CVMgetParsedSubOption(serverOpts, "initClasses");
     const char* mlist = CVMgetParsedSubOption(serverOpts, "precompileMethods");
+
+    /* Remember the pid of our process for compatibility with
+       non-Posix compliant systems on which getpid() returns a thread id. */
+    jumpProcessSetId(getpid());
     
     jumpMessageStart();
 
