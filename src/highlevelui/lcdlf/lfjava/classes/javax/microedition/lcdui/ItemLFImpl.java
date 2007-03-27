8d13ab26287b0ae8ce0c903783c4393abb4e129e
olga_milovskaya <olga_milovskaya@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-27 15:17:21 +0000

    Fix problem with pointer coordinates and delete focus border for List and TextBox

--- src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/ItemLFImpl.java
+++ src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/ItemLFImpl.java
@@ -163,14 +163,14 @@ abstract class ItemLFImpl implements ItemLF {
      * overriding is the ChoiceGroupPopupLFImpl.  
      */
     boolean itemContainsPointer(int x, int y) {
-        int contentX = bounds[X] + contentBounds[X];
-        int contentY = bounds[Y] + contentBounds[Y];
+        int contentX = bounds[X] + contentBounds[X] + ScreenSkin.PAD_FORM_ITEMS - 2;
+        int contentY = bounds[Y] + contentBounds[Y] + ScreenSkin.PAD_FORM_ITEMS - 2;
 
         int myX = x - contentX;
         int myY = y - contentY;
 
-        return (myX >= 0 && myX <= contentBounds[WIDTH] &&
-                myY >= 0 && myY <= contentBounds[HEIGHT]);
+        return (myX >= 0 && myX <= contentBounds[WIDTH] + ScreenSkin.PAD_FORM_ITEMS - 2 &&
+                myY >= 0 && myY <= contentBounds[HEIGHT] + ScreenSkin.PAD_FORM_ITEMS - 2);
     }
 
     
@@ -1067,7 +1067,7 @@ abstract class ItemLFImpl implements ItemLF {
 
         // Clip the dirty region to only include the item
         // g.clipRect(tX, tY, bounds[WIDTH], bounds[HEIGHT]);
-        
+
         // "inner" bounds
         g.clipRect(tX, tY, 
                    getInnerBounds(WIDTH),
@@ -1136,12 +1136,11 @@ abstract class ItemLFImpl implements ItemLF {
         
         // NTS: This may need to special case StringItem?
         g.setColor(ScreenSkin.COLOR_TRAVERSE_IND);
-        
 
         g.drawRect(x + contentBounds[X] + ScreenSkin.PAD_FORM_ITEMS - 2,
-                   y + contentBounds[Y] + ScreenSkin.PAD_FORM_ITEMS - 2, 
-                   contentBounds[WIDTH] + 3,
-                   contentBounds[HEIGHT] + 3);
+                   y + contentBounds[Y] + ScreenSkin.PAD_FORM_ITEMS - 2,
+                   contentBounds[WIDTH] + ScreenSkin.PAD_FORM_ITEMS - 2,
+                   contentBounds[HEIGHT] + ScreenSkin.PAD_FORM_ITEMS - 2);
     }
     
 
