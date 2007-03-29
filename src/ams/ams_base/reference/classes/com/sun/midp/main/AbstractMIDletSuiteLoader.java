6622002cc7619c677c1e81791ac25a3f331e8322
leonidr <leonidr@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-03-29 15:04:15 +0000

    Allocate reserved resources on MIDlet startup after suite environment has been created, otherwise calling reportError will throw NPE

--- src/ams/ams_base/reference/classes/com/sun/midp/main/AbstractMIDletSuiteLoader.java
+++ src/ams/ams_base/reference/classes/com/sun/midp/main/AbstractMIDletSuiteLoader.java
@@ -305,12 +305,6 @@ abstract class AbstractMIDletSuiteLoader
         init();
 
         try {
-            // Regard resource policy for the suite task
-            if (!allocateReservedResources()) {
-                reportError(Constants.MIDLET_RESOURCE_LIMIT);
-                return;
-            }
-
             /*
              * Prepare MIDlet suite environment, classes that only need
              * the isolate ID can be create here.
@@ -329,6 +323,12 @@ abstract class AbstractMIDletSuiteLoader
 
                 throw new
                     RuntimeException("Suite environment not complete.");
+            }
+
+            // Regard resource policy for the suite task
+            if (!allocateReservedResources()) {
+                reportError(Constants.MIDLET_RESOURCE_LIMIT);
+                return;
             }
 
             // Create suite instance ready for start
