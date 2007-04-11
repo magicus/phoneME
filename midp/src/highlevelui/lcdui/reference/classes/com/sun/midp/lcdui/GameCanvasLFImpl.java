7efae7b761ca71e60307cecae835c3aba14352ae
amy_wang <amy_wang@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-04-11 00:10:04 +0000

    Copy https://phoneme.dev.java.net/svn/phoneme/components/midp/tags/midp-advanced-mr2-promo-b14 to https://phoneme.dev.java.net/svn/phoneme/builds.

---
+++ midp/src/highlevelui/lcdui/reference/classes/com/sun/midp/lcdui/GameCanvasLFImpl.java
@@ -0,0 +1,141 @@
+/*
+ *
+ *
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ */
+package com.sun.midp.lcdui;
+
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.game.GameCanvas;
+
+import com.sun.midp.configurator.Constants;
+
+
+/**
+* This is the look &amp; feel implementation for GameCanvas.
+*/
+public class GameCanvasLFImpl {
+
+    /**
+     * The owner of this view.
+     */
+    GameCanvas owner;
+
+    /**
+     * currently every GameCanvas has one offscreen buffer
+     * can be optimized so that we put a limit on no of offscreen buffers
+     * an application can have
+     */
+    private Image offscreen_buffer;
+
+
+    GameCanvasLFImpl(GameCanvas c) {
+        owner = c;
+        offscreen_buffer = Image.createImage(owner.getWidth(),owner.getHeight());
+    }
+
+    /**
+     * return offscreen buffer
+     * @return offscreen buffer
+     * @param w    width of displayable area available to the application
+     * @param h  - height of the displayable area available to the application
+     */
+    public Image getBuffer(int w, int h) {
+        return offscreen_buffer;
+    }
+
+    /**
+     * Notify return screen about screen size change
+     */
+    public void uCallSizeChanged(int w, int h) {
+            offscreen_buffer = Image.createImage(w, h);
+    }
+
+    /**
+     * Obtains the Graphics object for rendering a GameCanvas.  The returned
+     * Graphics object renders to the off-screen buffer belonging to this
+     * GameCanvas.
+     * @return  the Graphics object that renders to current GameCanvas
+     */
+    public Graphics getGraphics() {
+        if (offscreen_buffer != null) {
+            return offscreen_buffer.getGraphics();
+        }
+
+        return null; 
+    }
+
+    
+    public void drawImage(Graphics g) {
+        // NullPointerException will be thrown in drawImage if g == null
+       if (offscreen_buffer != null) {
+            g.drawImage(offscreen_buffer, 0, 0, Graphics.TOP|Graphics.LEFT);
+       }
+    }
+
+    /**
+     *  Flushes the off-screen buffer to the display.
+     */
+    public void flushGraphics() {
+        DisplayAccess displayAccess = GameMap.get(owner);
+        if (displayAccess != null && offscreen_buffer != null) {
+	        displayAccess.flush(owner, offscreen_buffer,
+			      0, 0, owner.getWidth(), owner.getHeight());
+        }
+    }
+
+    /**
+     * Flushes the specified region of the off-screen buffer to the display.
+     * @param x the left edge of the region to be flushed
+     * @param y the top edge of the region to be flushed
+     * @param width the width of the region to be flushed
+     * @param height the height of the region to be flushed
+     */
+    public void flushGraphics(int x, int y, int width, int height) {
+        if (width < 1 || height < 1) {
+	        return;
+	    }
+
+        DisplayAccess displayAccess = GameMap.get(owner);
+        if (displayAccess != null && offscreen_buffer != null) {
+            displayAccess.flush(owner, offscreen_buffer,
+    			  x, y,	width, height);
+        }
+    }
+
+    /**
+     * Gets the states of the physical game keys.
+      * @return An integer containing the key state information (one bit per
+     * key), or 0 if the GameCanvas is not currently shown.
+     */
+    public int getKeyStates() {
+        DisplayAccess displayAccess = GameMap.get(owner);
+            if (displayAccess != null) {
+                return displayAccess.getKeyMask();
+            }
+        return 0;
+    }
+
+}
---
+++ midp/src/highlevelui/lcdui/reference/native/lcdui_audio.c
@@ -0,0 +1,88 @@
+/*
+ *   
+ *
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt). 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ */
+
+/**
+ * @file
+ *
+ * This file contains Java native function for playing alert sound.
+ */
+
+#include <anc_audio.h>
+#include <midpEventUtil.h>
+#include <midp_constants_data.h>
+
+/**
+ * Calls platform specific function to play a sound.
+ * <p>
+ * Java declaration:
+ * <pre>
+ *     playAlertSound(II)Z
+ * </pre>
+ * Java parameters:
+ * <pre>
+ *   displayId The display ID associated with this Display
+ *   alertType  Type of alert
+ *   returns true if sound was played.
+ * </pre> 
+ */
+KNIEXPORT KNI_RETURNTYPE_BOOLEAN
+KNIDECL(javax_microedition_lcdui_Display_playAlertSound0) {
+    int alertType = KNI_GetParameterAsInt(2);
+    int displayId = KNI_GetParameterAsInt(1);
+
+    AncSoundType soundType;
+    
+    if (midpHasForeground(displayId)) {
+        switch (alertType) {
+            case LCDUI_ALERT_TYPE_INFO:
+                soundType = ANC_SOUND_INFO;
+                break;
+
+            case LCDUI_ALERT_TYPE_WARNING:
+                soundType = ANC_SOUND_WARNING;
+                break;
+
+            case LCDUI_ALERT_TYPE_ERROR:
+                soundType = ANC_SOUND_ERROR;
+                break;
+
+            case LCDUI_ALERT_TYPE_ALARM:
+                soundType = ANC_SOUND_ALARM;
+                break;
+
+            case LCDUI_ALERT_TYPE_CONFIRMATION:
+                soundType = ANC_SOUND_CONFIRMATION;
+                break;
+
+            default:
+                KNI_ReturnBoolean(KNI_FALSE);
+        }
+
+        KNI_ReturnBoolean(anc_play_sound(soundType));
+    }
+    
+    KNI_ReturnBoolean(KNI_FALSE);
+}
