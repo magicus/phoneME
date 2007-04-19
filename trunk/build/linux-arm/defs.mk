ee1e1dcdba946e118d77495c80fddb9f80838a14
cjplummer <cjplummer@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-04-19 13:12:23 +0000

    Added USE_AAPCS flag. This allows the user to control the AAPCS requirement on the command line rather than having to add "CVM_DEFINES += -DAAPCS" in a GNUmakefile. USE_AAPCS can also be set in a GNUmakefile.
    Reviewed by Hinkmond and Tom.

--- trunk/build/linux-arm/defs.mk
+++ trunk/build/linux-arm/defs.mk
@@ -97,6 +97,14 @@ ifeq ($(USE_GCC2), true)
 endif
 endif
 
+# Our source needs to know if AAPCS calling conventions are used
+USE_AAPCS ?= false
+CVM_FLAGS += USE_AAPCS
+USE_AAPCS_CLEANUP_ACTION = $(CVM_DEFAULT_CLEANUP_ACTION)
+ifeq ($(USE_AAPCS),true)
+CVM_DEFINES += -DAAPCS 
+endif
+
 #
 # JIT related settings
 #
