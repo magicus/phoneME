6ca599e915151b7067e29b5e3933bfd431bee226
sergey_tretyakov <sergey_tretyakov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-04-25 08:24:34 +0000

    add invoc stub

---
+++ src/jump_application/core/native/invoc.c
@@ -0,0 +1,59 @@
+/*
+ * 
+ *
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt). 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ */
+
+/**
+ * @file
+ * @brief Content Handler RegistryStore native functions implementation
+ */
+
+
+#include "jsr211_registry.h"
+#include "jsr211_invoc.h"
+		
+
+
+/**
+ * Checks whether the internal handlers, if any, are installed.
+ * Implemented in jsr211_deploy.c accordingly to JAMS/NAMS mode.
+ * @return JSR211_OK or JSR211_FAILED - if registry corrupted or OUT_OF_MEMORY.
+ */
+jint jsr211_check_internal_handlers(){
+	return 0;
+}
+
+
+/**
+ * Function to find a matching entry entry in the queue.
+ * The handlerID must match. The function seeks among new Invocations 
+ * (INIT status).
+ *
+ * @param handlerID a string identifying the requested handler
+ *
+ * @return the found invocation, or NULL if no matched invocation.
+ */
+StoredInvoc* jsr211_get_invocation(const jchar* handlerID){
+	return NULL;
+}
