c53c953d29fa0062c539d1b26b25aaced04123b3
billp <billp@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-06-04 15:39:36 +0000

    Fix a merge problem in solaris native code. Stub out some solaris time functions.  Pulled in some changes from trunk. Added MonitorInfo API to jvmtiEnv.c

--- src/solaris/native/java/net/net_util_md.c
+++ src/solaris/native/java/net/net_util_md.c
@@ -315,7 +315,7 @@ NET_AllocSockaddr(struct sockaddr **him, int *len) {
 }
 
 void
-NET_InetAddressToSockaddr(JNIEnv *env, jobject iaObj, int port, struct sockaddr *him, int *len, jboolean isLocalAddr) {
+NET_InetAddressToSockaddr(JNIEnv *env, jobject iaObj, int port, struct sockaddr *him, int *len) {
 #ifdef AF_INET6
     if (ipv6_available()) {
 	struct sockaddr_in6 *him6 = (struct sockaddr_in6 *)him;
@@ -389,14 +389,10 @@ NET_InetAddressToSockaddr(JNIEnv *env, jobject iaObj, int port, struct sockaddr 
     		if (uname(&sysinfo) == 0) {
     		    sysinfo.release[3] = '\0';
     		    if (strcmp(sysinfo.release, "2.2") != 0) {
-				  if (isLocalAddr) {
-					scope_id = getDefaultIPv6Interface( &(him6->sin6_addr) );
-				  } else {
-					scope_id = getDefaultIPv6Interface( &(him6->sin6_addr) );
-				  }
-				  (*env)->SetIntField(env, iaObj, scopeID, scope_id);
-				}
-			}
+		 	scope_id = getDefaultIPv6Interface( &(him6->sin6_addr) );
+	                (*env)->SetIntField(env, iaObj, scopeID, scope_id);
+		    }
+		}
 	    }
 
 	    /*
