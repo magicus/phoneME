94e81473ccd0654e443f0e05838629135825c92e
cjplummer <cjplummer@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-07-07 02:38:49 +0000

    Renamed CVM_FORCE_HARD_FLOAT #define to CVM_ARM_FLOAT_RESULT_IN_FLOAT_REGISTER, and set it up automatically by checking the __SOFTFP__ and __VFP_FP__ flags. Float results are only returned in a float register if not using __SOFTFP__ and also not using __VFP_FP__. Users should no longer need to build with CVM_FORCE_HARD_FLOAT=true when using hard float and not VFP, as long as GCC 3.x or later is used.
    Reviewed by Dean.

--- build/linux-arm/defs.mk
+++ build/linux-arm/defs.mk
@@ -86,9 +86,7 @@ CVM_SRCDIRS   += \
 CVM_INCLUDE_DIRS  += \
 	$(CVM_TOP)/src/$(TARGET_OS)-$(TARGET_CPU_FAMILY)
 
-ifeq ($(CVM_FORCE_HARD_FLOAT), true)
-	CVM_DEFINES     += -DCVM_FORCE_HARD_FLOAT
-else
+ifneq ($(CVM_FORCE_HARD_FLOAT), true)
 ifeq ($(USE_GCC2), true)
 	CC_ARCH_FLAGS   += -msoft-float
 	ASM_ARCH_FLAGS  += -msoft-float
