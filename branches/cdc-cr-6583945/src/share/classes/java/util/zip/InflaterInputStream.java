732736a272ee2fef47e98514209f6803d76ab67c
alexey_v_bakhtin <alexey_v_bakhtin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-07-23 13:44:35 +0000

    Add new coponent

---
+++ branches/cdc-cr-6583945/src/share/classes/java/util/zip/InflaterInputStream.java
@@ -0,0 +1,251 @@
+/*
+ * @(#)InflaterInputStream.java	1.38 06/10/10
+ *
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.  
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER  
+ *   
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License version  
+ * 2 only, as published by the Free Software Foundation.   
+ *   
+ * This program is distributed in the hope that it will be useful, but  
+ * WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  
+ * General Public License version 2 for more details (a copy is  
+ * included at /legal/license.txt).   
+ *   
+ * You should have received a copy of the GNU General Public License  
+ * version 2 along with this work; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+ * 02110-1301 USA   
+ *   
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa  
+ * Clara, CA 95054 or visit www.sun.com if you need additional  
+ * information or have any questions. 
+ *
+ */
+
+package java.util.zip;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+/**
+ * This class implements a stream filter for uncompressing data in the
+ * "deflate" compression format. It is also used as the basis for other
+ * decompression filters, such as GZIPInputStream.
+ * NOTE: <B>java.util.zip.GZIPInputStream</B> is found in J2ME CDC profiles 
+ * such as J2ME Foundation Profile.
+ *
+ * @see		Inflater
+ * @version 	1.28, 02/02/00
+ * @author 	David Connelly
+ */
+public
+class InflaterInputStream extends FilterInputStream {
+    /**
+     * Decompressor for this stream.
+     */
+    protected Inflater inf;
+
+    /**
+     * Input buffer for decompression.
+     */
+    protected byte[] buf;
+
+    /**
+     * Length of input buffer.
+     */
+    protected int len;
+
+    private boolean closed = false;
+    // this flag is set to true after EOF has reached
+    private boolean reachEOF = false;
+    
+    /**
+     * Check to make sure that this stream has not been closed
+     */
+    private void ensureOpen() throws IOException {
+	if (closed) {
+	    throw new IOException("Stream closed");
+        }
+    }
+
+
+    /**
+     * Creates a new input stream with the specified decompressor and
+     * buffer size.
+     * @param in the input stream
+     * @param inf the decompressor ("inflater")
+     * @param size the input buffer size
+     * @exception IllegalArgumentException if size is <= 0
+     */
+    public InflaterInputStream(InputStream in, Inflater inf, int size) {
+	super(in);
+        if (in == null || inf == null) {
+            throw new NullPointerException();
+        } else if (size <= 0) {
+            throw new IllegalArgumentException("buffer size <= 0");
+        }
+	this.inf = inf;
+	buf = new byte[size];
+    }
+
+    /**
+     * Creates a new input stream with the specified decompressor and a
+     * default buffer size.
+     * @param in the input stream
+     * @param inf the decompressor ("inflater")
+     */
+    public InflaterInputStream(InputStream in, Inflater inf) {
+	this(in, inf, 512);
+    }
+
+    boolean usesDefaultInflater = false;
+
+    /**
+     * Creates a new input stream with a default decompressor and buffer size.
+     * @param in the input stream
+     */
+    public InflaterInputStream(InputStream in) {
+	this(in, new Inflater());
+        usesDefaultInflater = true;
+    }
+
+    private byte[] singleByteBuf = new byte[1];
+
+    /**
+     * Reads a byte of uncompressed data. This method will block until
+     * enough input is available for decompression.
+     * @return the byte read, or -1 if end of compressed input is reached
+     * @exception IOException if an I/O error has occurred
+     */
+    public int read() throws IOException {
+	ensureOpen();
+	return read(singleByteBuf, 0, 1) == -1 ? -1 : singleByteBuf[0] & 0xff;
+    }
+
+    /**
+     * Reads uncompressed data into an array of bytes. This method will
+     * block until some input can be decompressed.
+     * @param b the buffer into which the data is read
+     * @param off the start offset of the data
+     * @param len the maximum number of bytes read
+     * @return the actual number of bytes read, or -1 if the end of the
+     *         compressed input is reached or a preset dictionary is needed
+     * @exception ZipException if a ZIP format error has occurred
+     * @exception IOException if an I/O error has occurred
+     */
+    public int read(byte[] b, int off, int len) throws IOException {
+	ensureOpen();
+        if ((off | len | (off + len) | (b.length - (off + len))) < 0) {
+	    throw new IndexOutOfBoundsException();
+	} else if (len == 0) {
+	    return 0;
+	}
+	try {
+	    int bytesRead = 0;
+	    while (len > 0) {
+		int n = inf.inflate(b, off, len);
+		if (n == 0) {
+		    if (inf.finished() || inf.needsDictionary()) {
+			reachEOF = true;
+			if (bytesRead > 0) {
+			    return bytesRead;
+			} else {
+			    return -1;
+			}
+		    }
+		    if (inf.needsInput()) {
+			fill();
+		    }
+		}
+		bytesRead += n;
+		off += n;
+		len -= n;
+	    }
+	    return bytesRead;
+	} catch (DataFormatException e) {
+	    String s = e.getMessage();
+	    throw new ZipException(s != null ? s : "Invalid ZLIB data format");
+	}
+    }
+
+    /**
+     * Returns 0 after EOF has reached, otherwise always return 1.
+     * <p>
+     * Programs should not count on this method to return the actual number
+     * of bytes that could be read without blocking.
+     *
+     * @return     1 before EOF and 0 after EOF.
+     * @exception  IOException  if an I/O error occurs.
+     * 
+     */
+    public int available() throws IOException {
+        ensureOpen();
+        if (reachEOF) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    private byte[] b = new byte[512];
+
+    /**
+     * Skips specified number of bytes of uncompressed data.
+     * @param n the number of bytes to skip
+     * @return the actual number of bytes skipped.
+     * @exception IOException if an I/O error has occurred
+     * @exception IllegalArgumentException if n < 0
+     */
+    public long skip(long n) throws IOException {
+        if (n < 0) {
+            throw new IllegalArgumentException("negative skip length");
+        }
+	ensureOpen();
+	int max = (int)Math.min(n, Integer.MAX_VALUE);
+	int total = 0;
+	while (total < max) {
+	    int len = max - total;
+	    if (len > b.length) {
+		len = b.length;
+	    }
+	    len = read(b, 0, len);
+	    if (len == -1) {
+                reachEOF = true;
+		break;
+	    }
+	    total += len;
+	}
+	return total;
+    }
+
+    /**
+     * Closes the input stream.
+     * @exception IOException if an I/O error has occurred
+     */
+    public void close() throws IOException {
+        if (!closed) {
+            if (usesDefaultInflater)
+                inf.end();
+	    in.close();
+            closed = true;
+        }
+    }
+
+    /**
+     * Fills input buffer with more data to decompress.
+     * @exception IOException if an I/O error has occurred
+     */
+    protected void fill() throws IOException {
+	ensureOpen();
+	len = in.read(buf, 0, buf.length);
+	if (len == -1) {
+	    throw new EOFException("Unexpected end of ZLIB input stream");
+	}
+	inf.setInput(buf, 0, len);
+    }
+}
