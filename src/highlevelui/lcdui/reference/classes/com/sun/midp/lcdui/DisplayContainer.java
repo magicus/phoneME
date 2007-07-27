cf2702918c7eda190f642f3cdc8fe06fd958ef03
stephen_flores <stephen_flores@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-07-27 18:11:04 +0000

    Change to fix the CDC OGGi demo build.
    Reviewed by Gary Adams.
    
    Changed the findDisplayById() method of DisplayContainer to be public. Note, the findDisplayByOwnerName method was already public.

--- src/highlevelui/lcdui/reference/classes/com/sun/midp/lcdui/DisplayContainer.java
+++ src/highlevelui/lcdui/reference/classes/com/sun/midp/lcdui/DisplayContainer.java
@@ -105,7 +105,7 @@ public class DisplayContainer {
      *
      * @return a display access object or null if not found
      */
-    synchronized DisplayAccess findDisplayById(int displayId) {
+    public synchronized DisplayAccess findDisplayById(int displayId) {
         int size = displays.size();
 
         for (int i = 0; i < size; i++) {
