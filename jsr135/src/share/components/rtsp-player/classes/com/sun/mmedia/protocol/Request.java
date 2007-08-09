bffa9c30caf9b59348ce6c4fba77252d65b78f16
ccarter <ccarter@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-08-09 18:38:09 +0000

    Copy jsr135-advanced-mr2-promo-b35 to https://phoneme.dev.java.net/svn/phoneme/builds.

---
+++ jsr135/src/share/components/rtsp-player/classes/com/sun/mmedia/protocol/Request.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt). 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ */
+package com.sun.mmedia.rtsp.protocol;
+
+import java.io.*;
+import java.util.*;
+
+public class Request extends Parser {
+    public RequestLine requestLine;
+    public Vector headers;
+
+    public Request(ByteArrayInputStream bin) {
+        String line = getLine(bin);
+
+        requestLine = new RequestLine(line);
+
+        headers = new Vector();
+
+        line = getLine(bin);
+
+        while (line.length() > 0) {
+            if (line.length() > 0) {
+                Header header = new Header(line);
+
+                headers.addElement(header);
+
+                line = getLine(bin);
+            }
+        }
+    }
+
+    public RequestLine getRequestLine() {
+        return requestLine;
+    }
+
+    public Header getHeader(int type) {
+        Header header = null;
+
+        for (int i = 0; i < headers.size(); i++) {
+            Header tmpHeader = (Header) headers.elementAt(i);
+
+            if (tmpHeader.type == type) {
+                header = tmpHeader;
+
+                break;
+            }
+        }
+
+        return header;
+    }
+}
