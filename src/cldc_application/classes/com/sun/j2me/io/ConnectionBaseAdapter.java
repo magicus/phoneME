a74e3abdf33f0c160ae47ef33eeafe3483fafa63
RomanZ <RomanZ@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-08-09 15:38:49 +0000

    ConnectionBaseInterface now implements ConnectionBaseAdapter.

--- src/cldc_application/classes/com/sun/j2me/io/ConnectionBaseAdapter.java
+++ src/cldc_application/classes/com/sun/j2me/io/ConnectionBaseAdapter.java
@@ -27,4 +27,5 @@ package com.sun.j2me.io;
  * Base class for Connection protocols.
  */
 public abstract class ConnectionBaseAdapter
-    extends com.sun.midp.io.ConnectionBaseAdapter {}
+    extends com.sun.midp.io.ConnectionBaseAdapter
+    implements ConnectionBaseInterface {}
