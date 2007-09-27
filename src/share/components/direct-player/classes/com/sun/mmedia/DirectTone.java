6b5044ab0a7c397d32addc878584279921ad37c8
alexey_v_bakhtin <alexey_v_bakhtin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-09-27 13:04:34 +0000

    cr6605543: Add DualToneControl and Tone Sequence implementation; merged from jsr135-cr-6605543 -r7330:7362; reviewed by Stanislav N. and Andrey K.

--- src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
+++ src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
@@ -37,12 +37,12 @@ import java.util.*;
 import com.sun.mmedia.DefaultConfiguration;
 import com.sun.j2me.log.Logging;
 import com.sun.j2me.log.LogChannels;
- 
+import com.sun.mmedia.control.DualToneControl; 
 /**
  * Java Tone Sequence Player
  * it implements ToneControl
  */
-public final class DirectTone extends DirectPlayer implements ToneControl {
+public final class DirectTone extends DirectPlayer {
 
     /**
      * It does not need data source
@@ -102,7 +102,8 @@ public final class DirectTone extends DirectPlayer implements ToneControl {
         }
         
         try {
-            this.setSequence(seqs);
+            ToneControl tControl = new DirectToneControl(this, false);
+            tControl.setSequence(seqs);
         } catch (Exception e) {
             throw new MediaException("unable to realize: " + e.getMessage());
         }
@@ -117,11 +118,17 @@ public final class DirectTone extends DirectPlayer implements ToneControl {
      */
     protected Control doGetControl(String type) {
         Control c = super.doGetControl(type);
-        if (c != null) return c;
+        if (c != null) {
+            return c;
+        }
 
         if (getState() >= REALIZED) {
-            if (type.equals("javax.microedition.media.control.ToneControl")) {
-                return this;
+            if (type.equals("javax.microedition.media.control.ToneControl") ||
+                type.equals("ToneControl")) {
+                return new DirectToneControl(this, false);
+            }
+            if (type.equals("com.sun.mmedia.control.DualToneControl")) {
+                return new DirectToneControl(this, true);
             }
         }
 
@@ -148,13 +155,11 @@ public final class DirectTone extends DirectPlayer implements ToneControl {
      * that this control belongs to is in the <i>PREFETCHED</i> or
      * <i>STARTED</i> state.
      */
-    public void setSequence(byte[] sequence)
+    void setSequence(byte[] sequence)
     {
         if (this.getState() >= Player.PREFETCHED)
             throw new IllegalStateException("cannot set seq after prefetched");
        
-        if(sequence == null) throw new IllegalArgumentException("null sequence");
-
         nFlushBuffer(hNative);
 
         nBuffering(hNative, sequence, sequence.length);
@@ -163,5 +168,118 @@ public final class DirectTone extends DirectPlayer implements ToneControl {
             throw new IllegalArgumentException("invalid sequence");
 
         hasToneSequenceSet = true;
+    }
+
+    /**
+     * Java Tone Sequence Control
+     * it implements ToneControl
+     */
+    public final class DirectToneControl implements ToneControl, DualToneControl {
+        private DirectTone _player;
+        private boolean dualTone;
+
+        DirectToneControl(DirectTone player, boolean dualTone) {
+            _player = player;
+            this.dualTone = dualTone;
+        }
+
+        private boolean checkSequence( byte[] seq )
+        {
+            int t;
+            int pos = 0;
+
+            boolean blk_defined[] = new boolean[128];
+            boolean res_defined = false;
+            boolean tmp_defined = false;
+            int  blk         = -1;
+            
+            if( seq.length < 2 ) return false;
+
+            for (int i = 0; i < 128; i++) blk_defined[i] = false;
+
+                while (pos < seq.length)
+                {
+                    switch (t = seq[pos++])
+                    {
+                        case ToneControl.VERSION:
+                            if (1 != seq[pos]) return false;
+                            pos++;
+                            break;
+                        case ToneControl.TEMPO:
+                            if (tmp_defined) return false;
+                            if (seq[pos] < 5 || seq[pos] > 127) return false;
+                            tmp_defined = true;
+                            pos++;
+                            break;
+                        case ToneControl.RESOLUTION:
+                            if (res_defined) return false;
+                            if (seq[pos] < 1 || seq[pos] > 127) return false;
+                            res_defined = true;
+                            pos++;
+                            break;
+                        case ToneControl.BLOCK_START:
+                            if (seq[pos] < 0 || seq[pos] > 127) return false;
+                            blk = seq[pos++];
+                            break;
+                        case ToneControl.BLOCK_END:
+                            if (-1 == blk) return false;
+                            if (seq[pos++] != blk) return true;
+                            blk_defined[blk] = true;
+                            blk = -1;
+                            break;
+                        case ToneControl.PLAY_BLOCK:
+                            if (seq[pos] < 0 || seq[pos] > 127) return false;
+                            if (!blk_defined[seq[pos]]) return false;
+                            pos++;
+                            break;
+                        case ToneControl.SET_VOLUME:
+                            if (seq[pos] < 0 || seq[pos] > 100) return false;
+                            pos++;
+                            break;
+                        case ToneControl.REPEAT:
+                            if (seq[pos] < 2 || seq[pos] > 127) return false;
+                            pos++;
+                            break;
+                        case ToneControl.SILENCE:
+                            if (seq[pos] < 1 || seq[pos] > 127) return false;
+                            pos++;
+                            break;
+                        case DualToneControl.DUALTONE:
+                            if (!dualTone) return false;
+                            if (seq[pos] < 0 || seq[pos] > 127) return false;
+                            pos++;
+                            if (seq[pos] < 0 || seq[pos] > 127) return false;
+                            pos++;
+                            if (seq[pos] < 0 || seq[pos] > 127) return false;
+                            pos++;
+                            break;
+                        default: // note
+                            if (t < 0 || t > 127) return false;
+                            if (seq[pos] < 1 || seq[pos] > 127) return false;
+                            pos++;
+                    }
+                }
+
+            return true;
+        }
+
+        /**
+         * Sets the tone sequence.<p>
+         * 
+         * @param sequence The sequence to set.
+         * @exception IllegalArgumentException Thrown if the sequence is 
+         * <code>null</code> or invalid.
+         * @exception IllegalStateException Thrown if the <code>Player</code>
+         * that this control belongs to is in the <i>PREFETCHED</i> or
+         * <i>STARTED</i> state.
+         */
+        public void setSequence(byte[] sequence)
+        {
+            if (sequence == null) 
+                throw new IllegalArgumentException("null sequence");
+            if (!checkSequence(sequence)) 
+                throw new IllegalArgumentException("invalid sequence");
+            _player.setSequence(sequence);
+        }
     }
 }
