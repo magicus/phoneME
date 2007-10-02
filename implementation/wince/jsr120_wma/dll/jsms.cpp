1d44e8446d35dcd83359d4e9b01e2d041cb0dc89
borisu <borisu@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-10-02 09:02:36 +0000

    jsms: final implementation

--- implementation/wince/jsr120_wma/dll/jsms.cpp
+++ implementation/wince/jsr120_wma/dll/jsms.cpp
@@ -23,8 +23,13 @@
  * information or have any questions. 
  */
 
+#define INITGUID
 #include "windows.h"
 #include "cemapi.h"
+
+// {FAE79405-C31A-4064-9623-5A4A5B34A729}
+DEFINE_GUID(CLSID_JSMS, 
+0xfae79405, 0xc31a, 0x4064, 0x96, 0x23, 0x5a, 0x4a, 0x5b, 0x34, 0xa7, 0x29);
 
 /**
  *  WriteMapFile function stores loaded data to Mapped file
@@ -152,32 +157,68 @@ private:
 
 HRESULT ClassFactoryImpl::QueryInterface(REFIID rif, LPVOID *ppvObject) {
 
-    return NULL;
+    if (ppvObject == NULL) {
+        return E_INVALIDARG;
+    }
+
+    if ((rif == IID_IUnknown) || (rif == IID_IClassFactory)) {
+        *ppvObject = (LPVOID) this;
+        ((LPUNKNOWN) *ppvObject)->AddRef();
+	return S_OK;
+    } else {
+        *ppvObject = NULL;
+	return E_NOINTERFACE;
+    }
 }
 
 HRESULT MailRuleClientImpl::QueryInterface(REFIID rif, LPVOID *ppvObject) {
 
-    return NULL;
+    if (ppvObject == NULL) {
+        return E_INVALIDARG;
+    }
+    
+    if ((rif == IID_IUnknown) || (rif == IID_IMailRuleClient)) {
+        *ppvObject = (LPVOID) this;
+        ((LPUNKNOWN) *ppvObject)->AddRef();
+	return S_OK;
+    } else {
+        *ppvObject = NULL;
+	return E_NOINTERFACE;
+    }
 }
 
 ULONG ClassFactoryImpl::AddRef() {
 
-    return NULL;
+    return countRef++;
 }
 
 ULONG ClassFactoryImpl::Release() {
 
-    return NULL;
+    countRef--;
+
+    if (countRef == 0) {
+        delete this;
+        return 0;
+    } else {
+        return countRef;
+    }
 }
 
 ULONG MailRuleClientImpl::AddRef() {
 
-    return NULL;
+    return countRef++;
 }
 
 ULONG MailRuleClientImpl::Release() {
 
-    return NULL;
+    countRef--;
+    
+    if (countRef == 0) {
+        delete this;
+        return 0;
+    } else {
+        return countRef;
+    }
 } 
 
 /**
@@ -188,12 +229,31 @@ ULONG MailRuleClientImpl::Release() {
 
 HRESULT ClassFactoryImpl::CreateInstance(LPUNKNOWN pUnkOuter, REFIID riid, LPVOID *ppvObject) {
 
-    return NULL;
+    MailRuleClientImpl *pClient = NULL;
+
+    if (pUnkOuter != NULL) {
+        return CLASS_E_NOAGGREGATION;
+    }
+
+    pClient = new MailRuleClientImpl();
+    if (pClient == NULL) {
+        return E_FAIL;
+    }
+    
+    return pClient->QueryInterface(riid, ppvObject);
 }
+
+int lockServerNum = 0;
 
 HRESULT ClassFactoryImpl::LockServer(BOOL fLock) {
 
-    return NULL;
+    if (fLock) {
+        lockServerNum++;
+    } else {
+        lockServerNum--;
+    }
+
+    return S_OK;
 }
 
 /**
@@ -204,15 +264,105 @@ HRESULT ClassFactoryImpl::LockServer(BOOL fLock) {
 
 HRESULT MailRuleClientImpl::Initialize(IMsgStore *pMsgStore, MRCACCESS *pmaDesired) {
 
-    return NULL;
+    (void)pMsgStore;
+    *pmaDesired = MRC_ACCESS_WRITE;
+    return S_OK;
+}
+
+HRESULT DeleteMsg(IMsgStore *pMsgStore, ULONG cbMsg, LPENTRYID lpMsg, ULONG cbDestFolder, 
+                  LPENTRYID lpDestFolder, ULONG *pulEventType, MRCHANDLED *pHandled) {
+
+    HRESULT result = S_OK;
+    IMAPIFolder *pFolder = NULL;
+    
+    result = pMsgStore->OpenEntry(cbDestFolder, lpDestFolder, NULL, 0, NULL, (LPUNKNOWN*)&pFolder);
+    if (result == S_OK) {
+        SBinary   binary;
+        ENTRYLIST entrylist;
+
+        binary.cb = cbMsg;
+        binary.lpb = (LPBYTE) lpMsg;
+        entrylist.cValues = 1;
+        entrylist.lpbin = &binary;
+
+        result = pFolder->DeleteMessages(&entrylist, NULL, NULL, 0); 
+        if (result == S_OK) {
+            *pulEventType = fnevObjectDeleted;
+            *pHandled = MRC_HANDLED_DONTCONTINUE;
+        }
+    }
+
+    if (pFolder) {
+        pFolder->Release();
+    }
+
+    return result;
 }
 
 HRESULT MailRuleClientImpl::ProcessMessage(
         IMsgStore *pMsgStore, ULONG cbMsg, LPENTRYID lpMsg, ULONG cbDestFolder, 
-        LPENTRYID lpDestFolder, ULONG *pulEventType, MRCHANDLED *pHandled) {
+        LPENTRYID lpDestFolder, ULONG *pulEventType, MRCHANDLED *pHandled)
+{
+    HRESULT result = S_OK;
+    IMessage *pMsg = NULL;
 
-    //WriteMapFile here
-    return NULL;
+    SizedSPropTagArray(1, propTagSubject) = {1, PR_SUBJECT}; 
+    SizedSPropTagArray(1, propTagTime)    = {1, PR_MESSAGE_DELIVERY_TIME}; 
+    SizedSPropTagArray(1, propTagEmail)   = {1, PR_SENDER_EMAIL_ADDRESS}; 
+    
+    SPropValue *propSubject = NULL;
+    SPropValue *propTime = NULL;
+    SPropValue *propEmail = NULL;
+
+    ULONG dummy = 0;
+
+    //MessageBox(NULL, L"ProcessMessage", L"ProcessMessage", MB_OK);
+
+    result = pMsgStore->OpenEntry(cbMsg, lpMsg, NULL, 0, NULL, (LPUNKNOWN *) &pMsg);
+    if (result == S_OK) {
+    
+        pMsg->GetProps((SPropTagArray *) &propTagSubject, NULL, &dummy, &propSubject);
+        pMsg->GetProps((SPropTagArray *) &propTagEmail,   NULL, &dummy, &propEmail);
+        pMsg->GetProps((SPropTagArray *) &propTagTime,    NULL, &dummy, &propTime);
+        
+        if (propSubject && propTime && propEmail) {
+            if (strncmp(propSubject->Value.lpszA, "//WMA", 5) == 0) {
+                //MessageBox(NULL, L"Message accepted", L"jsms.dll", MB_OK);
+
+                //size of datagram is not accessible by MAPI interface,
+                //the only way to get the info is to extract them from the package
+                unsigned char datagramSize = (propSubject->Value.lpszA[5+4+4] == 0x20) ? 
+                    propSubject->Value.lpszA[5+4+4+1+1] : propSubject->Value.lpszA[5+4+4+2+2+2+1+1];
+
+                WriteMapFile((char*)propSubject->Value.lpszW, datagramSize, //propContentLength->Value.l,
+                   &propTime->Value.ft, (wchar_t*)propEmail->Value.lpszW);
+
+                result = DeleteMsg(pMsgStore, cbMsg, lpMsg, cbDestFolder, lpDestFolder, pulEventType, pHandled);
+            }
+            else {
+                //MessageBox(NULL, L"Message rejected", L"jsms.dll", MB_OK);
+                *pHandled = MRC_NOT_HANDLED;    
+            }
+        } else {
+            result = S_FALSE;
+        }
+
+        if (propEmail) {
+            MAPIFreeBuffer(propEmail);
+        }
+        if (propSubject){
+            MAPIFreeBuffer(propSubject);
+        }
+        if (propTime){
+            MAPIFreeBuffer(propTime);
+        }
+    }
+
+    if (pMsg) {
+        pMsg->Release();
+    }
+
+    return result;
 }
 
 /**
@@ -224,16 +374,37 @@ HRESULT MailRuleClientImpl::ProcessMessage(
 
 STDAPI DllCanUnloadNow() {
 
-    return S_OK;
+    return lockServerNum ? S_FALSE : S_OK;
 }
 
 STDAPI DllGetClassObject(const CLSID& clsid, REFIID riid, LPVOID *ppv) {
 
-    return S_FALSE;
+    HRESULT result;
+
+    if (clsid != CLSID_JSMS) {
+        return CLASS_E_CLASSNOTAVAILABLE;
+    }
+
+    ClassFactoryImpl *pFactory = new ClassFactoryImpl;
+    if (pFactory == NULL) {
+        return E_OUTOFMEMORY;
+    }
+
+    result = pFactory->QueryInterface(riid, ppv);
+    pFactory->Release();
+
+    return result;
 }
 
 BOOL WINAPI DllMain(HANDLE hinst, DWORD dwReason, LPVOID lpv) {
 
+    (void)hinst;
+    (void)dwReason;
+    (void)lpv;
     return TRUE;
 }
 
+BOOL WINAPI _DllMainCRTStartup(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+
+    return DllMain(hinstDLL, fdwReason, lpReserved);
+}
