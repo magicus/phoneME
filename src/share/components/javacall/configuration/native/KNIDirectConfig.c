aba7e3151918f6d9677283b52fa568322054bcaa
alexey_v_bakhtin <alexey_v_bakhtin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-10-17 12:13:40 +0000

    change getSupportedContentTypes implementation

--- src/share/components/javacall/configuration/native/KNIDirectConfig.c
+++ src/share/components/javacall/configuration/native/KNIDirectConfig.c
@@ -24,6 +24,8 @@
  */
 
 #include "KNICommon.h"
+#include <jsrop_kni.h>
+#include <javautil_string.h>
 
 KNIEXPORT KNI_RETURNTYPE_BOOLEAN
 KNIDECL(com_sun_mmedia_DefaultConfiguration_nIsAmrSupported) {
@@ -63,4 +65,106 @@ KNIDECL(com_sun_mmedia_DefaultConfiguration_nIsJtsSupported) {
     }
 
     KNI_ReturnBoolean( res ); 
+}
+
+#define MAX_PROTOCOLNAME_LEN 10
+#define MAX_MIMETYPENAME_LEN 100
+typedef struct {
+    const javacall_media_caps* pCaps;
+    unsigned char protocol[MAX_PROTOCOLNAME_LEN];
+    int p;
+} ListCapsType;
+
+KNIEXPORT KNI_RETURNTYPE_INT
+KNIDECL(com_sun_mmedia_DefaultConfiguration_nListCapsOpen) {
+    ListCapsType *hdlr = NULL;
+    javacall_utf16_string protocolUTF16;
+    javacall_int32 protocolNameLen;
+    
+    KNI_StartHandles(1);
+    KNI_DeclareHandle(stringObj);
+    KNI_GetParameterAsObject(1, stringObj);
+
+    hdlr = MALLOC(sizeof(ListCapsType));
+    if (hdlr != NULL) {
+        hdlr->pCaps = javacall_media_get_caps();
+        hdlr->p = 0;
+        if (hdlr->pCaps != NULL) {
+            if (!KNI_IsNullHandle(stringObj)) {
+                if (JAVACALL_OK != 
+                    jsrop_jstring_to_utf16_string(stringObj, &protocolUTF16)) {
+                        FREE(hdlr);
+                        hdlr = NULL;
+                }
+                if (JAVACALL_OK != 
+                    javautil_unicode_utf16_to_utf8(protocolUTF16, javautil_unicode_utf16_ulength(protocolUTF16),
+                    hdlr->protocol, MAX_PROTOCOLNAME_LEN, &protocolNameLen)) {
+                        FREE(hdlr);
+                        hdlr = NULL;
+                }
+            } else {
+                hdlr->protocol[0] = 0;
+            }
+        } else {
+            FREE(hdlr);
+            hdlr = NULL;
+        }
+    }
+
+    KNI_EndHandles();
+    KNI_ReturnInt( (jint)hdlr ); 
+};
+
+KNIEXPORT KNI_RETURNTYPE_OBJECT
+KNIDECL(com_sun_mmedia_DefaultConfiguration_nListCapsNext) {
+    int i   = 0;
+    ListCapsType *hdlr = NULL;
+    javacall_utf16 mimeUTF16[MAX_MIMETYPENAME_LEN];
+    int mimeLen;
+
+    KNI_StartHandles(1);
+    KNI_DeclareHandle(stringObj);
+    hdlr = (ListCapsType *)KNI_GetParameterAsInt(1);
+    KNI_ReleaseHandle(stringObj);
+
+    if (hdlr != NULL && hdlr->p >=0) {
+        while(hdlr->pCaps[hdlr->p].mimeType != NULL) {
+            if (hdlr->protocol[0] == 0) {
+                break;
+            } else {
+                for (i=0; i<hdlr->pCaps[hdlr->p].protocolCount; i++) {
+                    if(javautil_string_equals(hdlr->pCaps[hdlr->p].protocols[i], hdlr->protocol)) {
+                        break;
+                    }
+                }
+                if (i<hdlr->pCaps[hdlr->p].protocolCount) {
+                    break;
+                }
+            }
+            hdlr->p++;
+        }
+        if (hdlr->pCaps[hdlr->p].mimeType != NULL) {
+            mimeLen = 0;
+            while(hdlr->pCaps[hdlr->p].mimeType[mimeLen]!=0) mimeLen++;
+            if (JAVACALL_OK == javautil_unicode_utf8_to_utf16(hdlr->pCaps[hdlr->p].mimeType, mimeLen, mimeUTF16, MAX_MIMETYPENAME_LEN, &mimeLen) ) {
+                mimeUTF16[mimeLen] = 0;
+                jsrop_jstring_from_utf16_string(KNIPASSARGS mimeUTF16, stringObj);
+            }
+            hdlr->p++;
+        }
+    }
+
+    KNI_EndHandlesAndReturnObject(stringObj);
+}
+
+KNIEXPORT KNI_RETURNTYPE_VOID
+KNIDECL(com_sun_mmedia_DefaultConfiguration_nListCapsClose) {
+    ListCapsType *hdlr = NULL;
+    hdlr = (ListCapsType *)KNI_GetParameterAsInt(1);
+    if (hdlr != NULL) {
+        if (hdlr->protocol != NULL) {
+            FREE(hdlr->protocol);
+            }    
+        FREE(hdlr);
+    }
 }
