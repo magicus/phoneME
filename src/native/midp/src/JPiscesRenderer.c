e30960cac590201e6fcd707151533a3d744a6671
janvalenta <janvalenta@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-10-31 11:07:15 +0000

    Fixes for use with abstraction's proxies (abstract classes and native methods problems).

--- src/native/midp/src/JPiscesRenderer.c
+++ src/native/midp/src/JPiscesRenderer.c
@@ -141,7 +141,7 @@ KNIDECL(com_sun_pisces_PiscesRenderer_nativeFinalize) {
 }
 
 KNIEXPORT KNI_RETURNTYPE_VOID
-KNIDECL(com_sun_pisces_PiscesRenderer_beginRendering__IIIII) {
+KNIDECL(com_sun_pisces_PiscesRenderer_beginRenderingIIIII) {
     KNI_StartHandles(2);
     KNI_DeclareHandle(objectHandle);
     KNI_DeclareHandle(surfaceHandle);
@@ -175,7 +175,7 @@ KNIDECL(com_sun_pisces_PiscesRenderer_beginRendering__IIIII) {
 }
 
 KNIEXPORT KNI_RETURNTYPE_VOID
-KNIDECL(com_sun_pisces_PiscesRenderer_beginRendering__I) {
+KNIDECL(com_sun_pisces_PiscesRenderer_beginRenderingI) {
     KNI_StartHandles(2);
     KNI_DeclareHandle(objectHandle);
     KNI_DeclareHandle(surfaceHandle);
@@ -334,7 +334,7 @@ KNIDECL(com_sun_pisces_PiscesRenderer_getTransformImpl) {
 }
 
 KNIEXPORT KNI_RETURNTYPE_VOID
-KNIDECL(com_sun_pisces_PiscesRenderer_setStroke__IIII_3II) {
+KNIDECL(com_sun_pisces_PiscesRenderer_setStrokeImpl) {
     KNI_StartHandles(2);
     KNI_DeclareHandle(objectHandle);
     KNI_DeclareHandle(arrayHandle);
@@ -383,7 +383,7 @@ KNIDECL(com_sun_pisces_PiscesRenderer_setStroke__IIII_3II) {
 }
 
 KNIEXPORT KNI_RETURNTYPE_VOID
-KNIDECL(com_sun_pisces_PiscesRenderer_setStroke__) {
+KNIDECL(com_sun_pisces_PiscesRenderer_setStrokeImplNoParam) {
     KNI_StartHandles(1);
     KNI_DeclareHandle(objectHandle);
 
