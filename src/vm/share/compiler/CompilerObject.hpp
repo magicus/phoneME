5863c637cf6953f9e97c11100297e075df48b4af
olegpliss <olegpliss@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-11-21 01:21:05 +0000

    CompilerObject fixed and moved to compiler directory

--- src/vm/share/compiler/CompilerObject.hpp
+++ src/vm/share/compiler/CompilerObject.hpp
@@ -34,6 +34,14 @@
 #define USE_COMPILER_OBJECT_HEADER 1
 
 class CompilerObject {
+public:
+  enum Type {
+    #define DEFINE_COMPILER_OBJECT_TYPE(name) COMPILER_OBJECT_TYPE(name),
+    COMPILER_OBJECTS_DO( DEFINE_COMPILER_OBJECT_TYPE )
+    #undef DEFINE_COMPILER_OBJECT_TYPE
+    number_of_compiler_object_types
+  };
+private:
 #if USE_COMPILER_OBJECT_HEADER
   enum {
     size_bits = 16,
@@ -44,31 +52,54 @@ class CompilerObject {
 
     size_mask  = (1 << size_bits) - 1
   };
+
   unsigned header;
 
-  static int get_size( const unsigned header ) {
+  static int size( const unsigned header ) {
     return header & size_mask;
   }
-  static Type get_type( const unsigned header ) {
+  static Type type( const unsigned header ) {
     return Type(header >> type_shift);
   }
-  int  get_size( void ) const { return get_size( header ); }
-  Type get_type( void ) const { return get_type( header ); }
+  int  size( void ) const { return size( header ); }
+  Type type( void ) const { return type( header ); }
+
+  static unsigned make_header( const Type type, const int size ) {
+    GUARANTEE( unsigned(type) < number_of_compiler_object_types, "sanity" );
+    GUARANTEE( (size & ~size_mask) == 0, "Compiler object is too large" );
+    return (type << type_shift) | size;
+  }
+  void set_header( const Type type, const int size ) {
+    header = make_header( type, size );
+  }
+
+  static const char* const _names[];
+  static const char* name( const Type type ) {
+    GUARANTEE( unsigned(type) < number_of_compiler_object_types, "sanity" );
+    return _names[ type ];
+  }
+
+  static CompilerObject* start ( void ) {
+    return (CompilerObject*)_compiler_area_temp_top;
+  }
+  static CompilerObject* end ( void ) {
+    return (CompilerObject*)_compiler_area_top;
+  }
+
+  static const CompilerObject* find( const void* ref );
+  static const CompilerObject* next( const CompilerObject* p ) {
+    return DERIVED( const CompilerObject*, p, p->size() );
+  }
+  const CompilerObject* next( void ) const { return next( this ); }
 #endif
 public:
-  enum Type {
-    #define DEFINE_COMPILER_OBJECT_TYPE(name) COMPILER_OBJECT_TYPE(name),
-    COMPILER_OBJECTS_DO( DEFINE_COMPILER_OBJECT_TYPE )
-    #undef DEFINE_COMPILER_OBJECT_TYPE
-    number_of_compiler_object_types
-  };
-
   static CompilerObject* allocate( const Type type, const int size JVM_TRAPS ) {
     CompilerObject* p = (CompilerObject*)
       ObjectHeap::allocate_temp(size JVM_NO_CHECK);
+    (void)type;
 #if USE_COMPILER_OBJECT_HEADER
     if( p ) {
-      set_header( type, size );
+      p->set_header( type, size );
     }
 #endif
     return p;
