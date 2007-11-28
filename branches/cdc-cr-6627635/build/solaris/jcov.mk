5be8f61768a7376dbb272efcda542f94c24f69d4
mpetrov <mpetrov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-11-28 19:13:56 +0000

    syncing with the trunk

--- branches/cdc-cr-6627635/build/solaris/jcov.mk
+++ branches/cdc-cr-6627635/build/solaris/jcov.mk
@@ -26,18 +26,6 @@
 #  Platform specific makefile for building the Jcov tool
 #
 
-ifeq ($(CVM_DEBUG), true)
-    CVM_JCOV_LIB    = libjcov_g.so
-else
-    CVM_JCOV_LIB    = libjcov.so
-endif
-
-#
-# Platform specific objects:
-#
-CVM_JCOV_TARGETOBJS += \
-        jcov_md.o
-
 #
 # Platform specific build flags:
 #
