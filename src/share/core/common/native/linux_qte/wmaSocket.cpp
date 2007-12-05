f6ba330244afce6897287fce8e6aaac4e72f6d0a
borisu <borisu@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-12-05 12:14:50 +0000

    putback: r3724 | borisu | 2007-03-23 wmaSocket.c: memory leak fix - pcsl_mem_free(recipientPhone), original comments: CR 6537398. Memory leak. merge -r3642:3643 from jsr120-cr-6537398-memleak

--- src/share/core/common/native/linux_qte/wmaSocket.cpp
+++ src/share/core/common/native/linux_qte/wmaSocket.cpp
@@ -279,6 +279,7 @@ void WMASocket::readableSlot(int socket) {
                     sms->timeStamp = getLongLong(p, &index);
                     /* The next field is the recipient's phone number */
                     recipientPhone = getString(p, &index);
+                    pcsl_mem_free(recipientPhone);
                     /* The next field is the sender's phone number */
                     sms->msgAddr = getString(p, &index);
 
