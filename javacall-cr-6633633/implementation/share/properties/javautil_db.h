e8cad9ab983900ba893459d14ad709c35887d209
dgomon <dgomon@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-12-12 09:17:02 +0000

    GNU headers update

--- javacall-cr-6633633/implementation/share/properties/javautil_db.h
+++ javacall-cr-6633633/implementation/share/properties/javautil_db.h
@@ -2,9 +2,29 @@
  * $LastChangedDate: 2006-03-29 20:41:10 +0200 $  
  */
  
-/* 
- * Copyright  1990-2006 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+/*
+ *
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
  */
 
 #ifndef _DB_H_
