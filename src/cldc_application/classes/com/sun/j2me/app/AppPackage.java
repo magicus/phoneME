9505c01f2778a5315d91212012b29e3a062c202b
borisu <borisu@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-12-14 09:07:55 +0000

    CR 6634716: CCB for jsr120 WMAPermission class for cldc added, merge -r8175:8501 from abstractions-cr-6634716, reviewed by Roman and Natalia

--- src/cldc_application/classes/com/sun/j2me/app/AppPackage.java
+++ src/cldc_application/classes/com/sun/j2me/app/AppPackage.java
@@ -50,7 +50,11 @@ public class AppPackage {
         return MIDletStateHandler.getMidletStateHandler().
             getMIDletSuite().getID();
     }
-    
+
+
+    /** Unused ID */
+    public static final int UNUSED_APP_ID = -1;
+   
     /**
      * Returns permission status for the specified permission
      *
