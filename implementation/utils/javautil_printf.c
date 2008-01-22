65bca7e31b11a7cab4cfd81fe40039eeb04e55f1
akreimer <akreimer@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-01-22 15:48:43 +0000

    Changes to the logging mechanism following review.

--- implementation/utils/javautil_printf.c
+++ implementation/utils/javautil_printf.c
@@ -26,51 +26,148 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+
+#include "javacall_time.h"
 #include "javacall_logging.h"
 #include "javautil_printf.h"
 
+/*Converts an integer to a string*/
 static char* convertInt2String(int inputInt, char *buffer);
+/*Converts an hexadecimal number to string*/
 static char* convertHexa2String(int inputHex, char *buffer);
+/*Prints a string*/
 static void  javautil_putstring(const char *outputString);
+/*Prints one character*/
 static void  javautil_putchar(const char outputChar);
+/* Gets the channel name */
+static char* get_channel_name(int channelID);
+/*Gets the severity description*/
+static char* get_severity_name(int severity);
+/*
+ * Supported types are signed integer, character, string, hexadecimal format integer: 
+ * d,u,i = int,c = char, s = string (char *), x = int in hexadecimal format. 
+ *
+ * The longest 64 bit integer could be 21 characters long including the '-' and '\0' 
+ * MAX_INT_64   0x7FFFFFFFFFFFFFFF = 9223372036854775807  
+ * MAX_UINT_64  0xFFFFFFFFFFFFFFFF = 18446744073709551615 
+ * MIN_INT_64   0x8000000000000000 = -9223372036854775808 
 
-//#define USING_64_BIT_INTEGER
+ * The longest 32 bit integer could be 12 characters long including the '-' and '\0' 
+ * MAX_INT_32   0x7FFFFFFF = 2147483647  
+ * MAX_UINT_32  0xFFFFFFFF = 4294967295  
+ * MIN_INT_32   0x80000000 = -2147483648
+ 
+ * The longest 16 bit integer could be 7 characters long including the '-' and '\0' 
+ * MAX_INT_16   0x7FFF = 32767  
+ * MAX_UINT_16  0xFFFF = 65535  
+ * MIN_INT_16   0x8000 = -32768  
+ */
 
-/* Supported types are signed integer, character, string, hexadecimal format integer: */
-/* d,u,i = int,c = char, s = string (char *), x = int in hexadecimal format. */
-
-#ifdef USING_64_BIT_INTEGER
-
-/* The longest 64 bit integer could be 21 characters long including the '-' and '\0' */
-/* MAX_INT_64   0x7FFFFFFFFFFFFFFF = 9223372036854775807  */
-/* MAX_UINT_64  0xFFFFFFFFFFFFFFFF = 18446744073709551615 */
-/* MIN_INT_64   0x8000000000000000 = -9223372036854775808 */
-#define STRIP_SIGNIFICANT_BIT_MASK  0x0FFFFFFFFFFFFFFF
-#define MIN_NEGATIVE_INT (-9223372036854775808)
+/* Definitions */
+#define MIN_NEGATIVE_INT_64 (-9223372036854775807-1)
+#define MIN_NEGATIVE_INT_32 (-2147483647-1)
+#define MIN_NEGATIVE_INT_16 (-32767-1)
+#define STRIP_SIGNIFICANT_BIT_MASK_64  0x0FFFFFFFFFFFFFFF
+#define STRIP_SIGNIFICANT_BIT_MASK_32  0x0FFFFFFF
+#define STRIP_SIGNIFICANT_BIT_MASK_16  0x0FFF
 
 #define CONVERSION_BUFFER_SIZE      21
-#else /* NOT USING_64_BIT_INTEGER */
 
-/* The longest 32 bit integer could be 12 characters long including the '-' and '\0' */
-/* MAX_INT_32   0x7FFFFFFF = 2147483647  */
-/* MAX_UINT_32  0xFFFFFFFF = 4294967295  */
-/* MIN_INT_32   0x80000000 = -2147483648 */
-#define MIN_NEGATIVE_INT (-2147483648)
+/* Gets the channel name */
+static char* get_channel_name(int channelID) {   
+    switch(channelID) {
+        case 0:
+            return("NONE");  
+        case 500:
+            return("AMS");
+        case 1000:
+            return("CORE");
+        case 2000:
+            return("LOWUI");
+        case 4000:
+            return("HIGHUI");
+        case 5000:
+            return("PROTOCOL");
+        case 6000:
+            return("RMS");
+        case 7000:
+            return("SECURITY");
+        case 8000:
+            return("SERVICES");
+        case 11000:
+            return("STORAGE");
+        case 11500:
+            return("PUSH");         
+        case 13000:
+            return("MMAPI");
+        case 12000:
+            return("LIFECYCLE");
+        case 1100:
+            return("MIDPSTRING");
+        case 1200:
+            return("MALLOC");
+        case 1300:
+            return("CORE_STACK");   
+        case 3000:
+            return("I18N");
+        case 4100:
+            return("HIGHUI_ITEM_LAYOUT");
+        case 4200:
+            return("HIGHUI_ITEM_REPAINT");
+        case 4300:
+            return("HIGHUI_FORM_LAYOUT");
+        case 4400:
+            return("HIGHUI_ITEM_PAINT");      
+        case 9000:
+            return("TOOL");
+        case 10000:
+            return("JSR180");
+        case 10500:
+            return("EVENTS");
 
-#define STRIP_SIGNIFICANT_BIT_MASK  0x0FFFFFFF
-#define CONVERSION_BUFFER_SIZE      12
-#endif /* USING_64_BIT_INTEGER */
+        default: 
+            return("LOG_DISABLED");
+    }
 
+}
+/*Gets the severity description*/
 
+static char* get_severity_name(int severity){
+    switch(severity) {
+        case 0:
+            return("INFORMATION");      
+        case 1:
+            return("WARNING");
+        case 2:
+            return("ERROR");
+        case 3:
+            return("CRITICAL");
+        case 4:
+            return("NONE");
+        default: 
+            return("LOG_DISABLED");
+    }
+}
 
+/**
+ * Not all compilers provide printf function, so we have 
+ * to use workaround. This function is to be used from javacall.
+ */
 
 void javautil_printf(int severity, int channelID, char *message, ...) {
     va_list list;
     va_start(list, message);
-    javautil_vsprintf(severity, channelID, 1, message, list);
+    javautil_vprintf(severity, channelID, -1, message, list);
     va_end(list);
 
 }
+
+/**
+ * Not all compilers provide vprintf function, so we have 
+ * to use workaround. This function is to be used from midp.
+ * Prints out thr DEBUG message in the following format:
+ *  elapsed time in milliseconds|severiry|channel ID|isolate ID|message
+ */
 
 void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_list vl) {
 
@@ -82,8 +179,12 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
         char    c;
         char   *s;
     } Printable;
+    char* separator = "|";
+    javacall_time_milliseconds ms;
+    ms = javacall_time_get_clock_milliseconds();
 
     if(msg == NULL) {
+        javautil_putstring("NULL\n");
         return;
     }
     /* 
@@ -91,6 +192,20 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
        others must be accessed using the variable-
        argument macros.
     */
+
+    str = convertInt2String(ms,tempBuffer);
+    javautil_putstring(str);
+    javautil_putstring(separator);
+    str = get_channel_name(channelID);
+    javautil_putstring(str);
+    javautil_putstring(separator);
+    str = get_severity_name(severity);
+    javautil_putstring(str);
+    javautil_putstring(separator);
+    str = convertInt2String(isolateID,tempBuffer);
+    javautil_putstring(str);
+    javautil_putstring(separator);
+    
 
     while(*msg) {
 
@@ -132,8 +247,6 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
                     break;
 
                 default:
-                /*FIXME I think we should call to va_arg here as well */
-                /*va_arg( vl, ???? );*/
                     javautil_putstring("\nUnsupported type. Cant print %");
                     javautil_putchar(*msg);
                     javautil_putstring(".\n");
@@ -144,25 +257,40 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
 
     }/*end of while*/
 
+    javautil_putstring("\n");
+
 }/* end of javautil_printf */
 
+/*Prints one character*/
 
 static void javautil_putchar(const char outputChar) {
     const char java_outputChar[2]= {outputChar, '\0'};
     javacall_print(java_outputChar);
 }
 
+/*Prints a string*/
 
 static void javautil_putstring(const char *outputString) {
     javacall_print(outputString);
 }
 
+/*Converts an hexadecimal number to string*/
 
 static char* convertHexa2String(int inputHex, char *buffer) {
     const char hexaCharactersTable[16] = "0123456789ABCDEF";
     char *pstr = buffer;
     int neg = 0;
     int rem;
+    int mask = 0;
+
+    if(sizeof(int) == 2) {
+        mask = STRIP_SIGNIFICANT_BIT_MASK_16;
+    } else if(sizeof(int) == 4) {
+        mask = STRIP_SIGNIFICANT_BIT_MASK_32;
+    } else if(sizeof(int) == 8) {
+        mask = STRIP_SIGNIFICANT_BIT_MASK_64;
+    }
+
     pstr+=(CONVERSION_BUFFER_SIZE-1);
     *pstr = 0;
 
@@ -176,7 +304,7 @@ static char* convertHexa2String(int inputHex, char *buffer) {
         inputHex = inputHex >> 4;
         *pstr = hexaCharactersTable[rem];
         if(neg) {
-            inputHex = inputHex & STRIP_SIGNIFICANT_BIT_MASK;
+            inputHex = inputHex & mask;
             neg = 0;
         }
     } while(inputHex > 0);
@@ -184,21 +312,34 @@ static char* convertHexa2String(int inputHex, char *buffer) {
     return pstr;
 }
 
+/*Converts an integer to a string*/
+
+
 static char* convertInt2String(int inputInt, char *buffer) {
     int base = 10;
     int neg = 0;
     unsigned int conversion_unit = 0;
     char *pstr = buffer;
+    int min_negative_int = 0;
     pstr+=(CONVERSION_BUFFER_SIZE-1);
 
     *pstr = 0;
+
+    if(sizeof(int) == 2) {
+        min_negative_int = MIN_NEGATIVE_INT_16;
+    } else if(sizeof(int) == 4) {
+        min_negative_int = MIN_NEGATIVE_INT_32;
+    } else {
+        min_negative_int = MIN_NEGATIVE_INT_64;
+    }
+
 
     if(inputInt < 0) {
         neg = 1;
         inputInt*=(-1);
     }
 
-    if(inputInt == MIN_NEGATIVE_INT) {
+    if(inputInt == min_negative_int) {
         conversion_unit = (unsigned int)inputInt;
         do {
             pstr--;
