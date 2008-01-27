9ae8b1064bf247add71123d43c9fb436e0f57328
oren <oren@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-01-27 10:55:22 +0000

    Fix Header in lfj_SoftButtonLayer_kni.c

--- src/highlevelui/lcdlf/lfjava/native/lfj_SoftButtonLayer_kni.c
+++ src/highlevelui/lcdlf/lfjava/native/lfj_SoftButtonLayer_kni.c
@@ -1,12 +1,34 @@
 /*
- * @(#)lfj_cskin.c	1.14 06/06/17
  *
- * Copyright  1990-2006 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
  */
 
-// This file contains kni calls related to the SoftButtonLayer
-// this is needed once the platfrm uses its own native SoftButtonLayer
+
+/**
+ * This file contains kni calls related to the SoftButtonLayer
+ * this is needed once the platfrm uses its own native SoftButtonLayer
+ */
 
 #include <commonKNIMacros.h>
 #include <lcdlf_export.h>
