120e0a8afb6d4018227e42f844089212620e79bd
borisu <borisu@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-01-30 16:30:48 +0000

    review finding: method rename

--- src/cldc_application/classes/com/sun/j2me/security/WMAPermission.java
+++ src/cldc_application/classes/com/sun/j2me/security/WMAPermission.java
@@ -34,7 +34,7 @@ public class WMAPermission extends Permission {
     static public WMAPermission SMS_SERVER = new WMAPermission(
         Permissions.getName(Permissions.SMS_SERVER), "sms:open");
 
-    static public WMAPermission sms_send_permission(String host, int numSeg) {
+    static public WMAPermission getSmsSendPermission(String host, int numSeg) {
         return new WMAPermission(
             Permissions.getName(Permissions.SMS_SEND), host);
     }
@@ -53,7 +53,7 @@ public class WMAPermission extends Permission {
     static public WMAPermission MMS_SERVER = new WMAPermission(
         Permissions.getName(Permissions.MMS_SERVER), "mms:open");
 
-    static public WMAPermission mms_send_permission(String addresses, String numSeg) {
+    static public WMAPermission getMmsSendPermission(String addresses, String numSeg) {
         return new WMAPermission(
             Permissions.getName(Permissions.MMS_SEND), addresses);
     }
