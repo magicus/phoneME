96864f420fa2ad1b2c7ede9386a87dd016297e63
dgomon <dgomon@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-03 11:01:28 +0000

    CR6658534:  intermediate branch commit

--- implementation/share/utils/javautil_printf.c
+++ implementation/share/utils/javautil_printf.c
@@ -3,22 +3,22 @@
  *
  * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
  * 2 only, as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
  * included at /legal/license.txt).
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
- * 
+ *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
  * information or have any questions.
@@ -42,23 +42,23 @@ static char* get_channel_name(int channelID);
 /*Gets the severity description*/
 static char* get_severity_name(int severity);
 /*
- * Supported types are signed integer, character, string, hexadecimal format integer: 
- * d,u,i = int,c = char, s = string (char *), x = int in hexadecimal format. 
+ * Supported types are signed integer, character, string, hexadecimal format integer:
+ * d,u,i = int,c = char, s = string (char *), x = int in hexadecimal format.
  *
- * The longest 64 bit integer could be 21 characters long including the '-' and '\0' 
- * MAX_INT_64   0x7FFFFFFFFFFFFFFF = 9223372036854775807  
- * MAX_UINT_64  0xFFFFFFFFFFFFFFFF = 18446744073709551615 
- * MIN_INT_64   0x8000000000000000 = -9223372036854775808 
+ * The longest 64 bit integer could be 21 characters long including the '-' and '\0'
+ * MAX_INT_64   0x7FFFFFFFFFFFFFFF = 9223372036854775807
+ * MAX_UINT_64  0xFFFFFFFFFFFFFFFF = 18446744073709551615
+ * MIN_INT_64   0x8000000000000000 = -9223372036854775808
 
- * The longest 32 bit integer could be 12 characters long including the '-' and '\0' 
- * MAX_INT_32   0x7FFFFFFF = 2147483647  
- * MAX_UINT_32  0xFFFFFFFF = 4294967295  
+ * The longest 32 bit integer could be 12 characters long including the '-' and '\0'
+ * MAX_INT_32   0x7FFFFFFF = 2147483647
+ * MAX_UINT_32  0xFFFFFFFF = 4294967295
  * MIN_INT_32   0x80000000 = -2147483648
- 
- * The longest 16 bit integer could be 7 characters long including the '-' and '\0' 
- * MAX_INT_16   0x7FFF = 32767  
- * MAX_UINT_16  0xFFFF = 65535  
- * MIN_INT_16   0x8000 = -32768  
+
+ * The longest 16 bit integer could be 7 characters long including the '-' and '\0'
+ * MAX_INT_16   0x7FFF = 32767
+ * MAX_UINT_16  0xFFFF = 65535
+ * MIN_INT_16   0x8000 = -32768
  */
 
 /* Definitions */
@@ -72,10 +72,10 @@ static char* get_severity_name(int severity);
 #define CONVERSION_BUFFER_SIZE      21
 
 /* Gets the channel name */
-static char* get_channel_name(int channelID) {   
+static char* get_channel_name(int channelID) {
     switch(channelID) {
         case 0:
-            return("NONE");  
+            return("NONE");
         case 500:
             return("AMS");
         case 1000:
@@ -95,7 +95,7 @@ static char* get_channel_name(int channelID) {
         case 11000:
             return("STORAGE");
         case 11500:
-            return("PUSH");         
+            return("PUSH");
         case 13000:
             return("MMAPI");
         case 12000:
@@ -105,7 +105,7 @@ static char* get_channel_name(int channelID) {
         case 1200:
             return("MALLOC");
         case 1300:
-            return("CORE_STACK");   
+            return("CORE_STACK");
         case 3000:
             return("I18N");
         case 4100:
@@ -115,7 +115,7 @@ static char* get_channel_name(int channelID) {
         case 4300:
             return("HIGHUI_FORM_LAYOUT");
         case 4400:
-            return("HIGHUI_ITEM_PAINT");      
+            return("HIGHUI_ITEM_PAINT");
         case 9000:
             return("TOOL");
         case 10000:
@@ -123,7 +123,7 @@ static char* get_channel_name(int channelID) {
         case 10500:
             return("EVENTS");
 
-        default: 
+        default:
             return("LOG_DISABLED");
     }
 
@@ -133,7 +133,7 @@ static char* get_channel_name(int channelID) {
 static char* get_severity_name(int severity){
     switch(severity) {
         case 0:
-            return("INFORMATION");      
+            return("INFORMATION");
         case 1:
             return("WARNING");
         case 2:
@@ -142,13 +142,13 @@ static char* get_severity_name(int severity){
             return("CRITICAL");
         case 4:
             return("NONE");
-        default: 
+        default:
             return("LOG_DISABLED");
     }
 }
 
 /*
- * Not all compilers provide printf function, so we have 
+ * Not all compilers provide printf function, so we have
  * to use workaround. This function is to be used from javacall.
  */
 
@@ -161,7 +161,7 @@ void javautil_printf(int severity, int channelID, char *message, ...) {
 }
 
 /*
- * Not all compilers provide vprintf function, so we have 
+ * Not all compilers provide vprintf function, so we have
  * to use workaround. This function is to be used from midp.
  * Prints out thr DEBUG message in the following format:
  *  elapsed time in milliseconds|severiry|channel ID|isolate ID|message
@@ -181,7 +181,7 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
     javacall_time_milliseconds ms;
     ms = javacall_time_get_clock_milliseconds();
 
-    /* 
+    /*
        msg is the last argument specified; all
        others must be accessed using the variable-
        argument macros.
@@ -205,7 +205,7 @@ void javautil_vprintf(int severity, int channelID, int isolateID, char *msg, va_
        return;
     }
 
-    
+
 
     while(*msg) {
 
@@ -282,7 +282,13 @@ static char* convertHexa2String(int inputHex, char *buffer) {
     } else if(sizeof(int) == 4) {
         mask = STRIP_SIGNIFICANT_BIT_MASK_32;
     } else if(sizeof(int) == 8) {
-        mask = STRIP_SIGNIFICANT_BIT_MASK_64;
+        /*
+         * The purpose of this casting operator is to disable compiler
+         * warning when compiling for machines whose word size is smaller
+         * than 8 bytes. The casting has no effect on machines where word
+         * size is 8 bytes.
+         */
+        mask = (int)STRIP_SIGNIFICANT_BIT_MASK_64;
     }
 
     pstr+=(CONVERSION_BUFFER_SIZE-1);
@@ -324,7 +330,13 @@ static char* convertInt2String(int inputInt, char *buffer) {
     } else if(sizeof(int) == 4) {
         min_negative_int = MIN_NEGATIVE_INT_32;
     } else {
-        min_negative_int = MIN_NEGATIVE_INT_64;
+        /*
+         * The purpose of this casting operator is to disable compiler
+         * warning when compiling for machines whose word size is smaller
+         * than 8 bytes. The casting has no effect on machines where word
+         * size is 8 bytes.
+         */
+        min_negative_int = (int)MIN_NEGATIVE_INT_64;
     }
 
 
