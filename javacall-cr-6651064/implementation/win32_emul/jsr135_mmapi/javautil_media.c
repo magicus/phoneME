fce9e95e84204ef97c811ae81e57c8ed88b2be0d
yevgenydiomidov <yevgenydiomidov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-07 10:17:05 +0000

    added javautil_media_rgbX888_to_png

--- javacall-cr-6651064/implementation/win32_emul/jsr135_mmapi/javautil_media.c
+++ javacall-cr-6651064/implementation/win32_emul/jsr135_mmapi/javautil_media.c
@@ -371,4 +371,92 @@ int javautil_media_rgb_to_png(unsigned char *input,
 
     return enc.offset;
 }
- 
+
+/**
+ * Encode rgbX888 format data to PNG data format (there is no compression)
+ * 
+ * @param input     Pointer to rgbX888 data
+ * @param output    Pointer to PNG encode buffer
+ * @param width     Width of image
+ * @param height    Height of image
+ * 
+ * @return Byte size of encoded PNG data
+ */
+int javautil_media_rgbX888_to_png(unsigned char *input, 
+                                  unsigned char *output,
+                                  int width, 
+                                  int height) {
+    int i,j,k;
+    unsigned int zcrc;
+    unsigned char zero = 0xF1;
+    unsigned char color = 0;
+    unsigned char filter = 0;
+    int GROUPS = height / ROWS_PER_GROUP;
+    int GROUP_BYTES = (width * 3 + 1) * ROWS_PER_GROUP;
+    PNGEnc enc;
+
+    enc.offset = 0;
+    enc.outBuf = output;
+    resetcrc(&enc);
+    /* Write the magic number */
+    for (i = 0; i < sizeof(png_magic); i++)
+    enc.outBuf[enc.offset++] = png_magic[i];
+
+    beginchunk(&enc, "IHDR", 0x0d);
+    writelongcrc(&enc, width);  /* width */
+    writelongcrc(&enc, height); /* height */
+    writebytecrc(&enc, BITS);   /* bit depth */
+    writebytecrc(&enc, 2);      /* color type : true color*/
+    writebytecrc(&enc, 0);      /* compression */
+    writebytecrc(&enc, 0);      /* filter */
+    writebytecrc(&enc, 0);      /* interlace */
+    endchunk(&enc);
+
+    beginchunk(&enc, "IDAT", (GROUPS * (GROUP_BYTES + 4 + 1)) + 4 + 2);
+    writewordcrc(&enc, ((0x0800 + 30) / 31) * 31 ); /* compression method */
+
+    zcrc = 1L;
+    for (i = 0; i < GROUPS; i++) {
+    writebytecrc(&enc, (unsigned char)(i == (GROUPS-1) ? 0x01 : 0)); 
+    /* not compressed */
+    writewordrevcrc(&enc, (short) GROUP_BYTES);
+    writewordrevcrc(&enc, (short) ~GROUP_BYTES);
+
+    for (j = 0; j < ROWS_PER_GROUP; j++) {
+        /* write PNG row filter - 0 = unfiltered */
+        zcrc = adler32(zcrc, &filter, 1);
+        writebytecrc(&enc, filter);
+        
+        /* write pixels */
+        for (k = 0; k < width; k++) {
+            if (*littleEndian) {
+                zcrc = adler32(zcrc, &(input[2]), 1);
+                writebytecrc(&enc, input[2]);
+                zcrc = adler32(zcrc, &(input[1]), 1);
+                writebytecrc(&enc, input[1]);
+                zcrc = adler32(zcrc, &(input[0]), 1);
+                writebytecrc(&enc, input[0]);
+            } else {
+                zcrc = adler32(zcrc, &(input[1]), 1);
+                writebytecrc(&enc, input[1]);
+                zcrc = adler32(zcrc, &(input[2]), 1);
+                writebytecrc(&enc, input[2]);
+                zcrc = adler32(zcrc, &(input[3]), 1);
+                writebytecrc(&enc, input[3]);
+            }
+            input += 4;
+        }
+    }
+    }
+    
+    writelongcrc(&enc, zcrc);
+
+    endchunk(&enc);
+
+    beginchunk(&enc, "IEND", 0);
+    endchunk(&enc);
+
+    /* Return the length of data written into output buffer */
+
+    return enc.offset;
+}
