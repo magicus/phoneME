6d4e88a70bc65cbb44b07f0e5e9ecaca43a9bf90
dgomon <dgomon@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-11 09:44:53 +0000

    CR 6654647 Intermediate branch commit

--- implementation/share/utils/javautil_stdio.c
+++ implementation/share/utils/javautil_stdio.c
@@ -30,14 +30,13 @@
 
 #include "javautil_stdio.h"
 
-#define LEFT	0x10    /* left justified */
+#define LEFT    0x10    /* left justified */
 
 /**
- * skip_atoi
- * Read number from input buffer, convert it to integer, forward buffer
+ * Reads number from input buffer, converts it to integer, forwards the buffer
  *
- * @s: the source buffer
- * @return: integer value
+ * @param s the source buffer
+ * @return integer value of the read number
  */
 static int skip_atoi(const char **buf) {
     int i = 0;
@@ -49,44 +48,43 @@ static int skip_atoi(const char **buf) {
 }
 
 /**
- * javautil_vsnprintf - Format a string and place it in a buffer
- * @buf: The buffer to place the result into
- * @fmt: The format string to use
- * @args: Arguments for the format string
+ * Formats a string and places it in a buffer
  *
- * Call this function if you are already dealing with a va_list.
+ * @param buf buffer to place the result into
+ * @param fmt format string to use
+ * @param args string arguments
  */
 static int javautil_vsnprintf(char *buf, size_t size, const char *fmt, va_list args) {
-	int len;
-	int i;
-	char *str, *end;
-	const char *s;
-	int flags;		/* flags to number() */
-	int field_width;	/* width of output field */
+    int len;
+    int i;
+    char *str, *end;
+    const char *s;
+    int flags;      /* flags to number() */
+    int field_width;    /* width of output field */
     int is_flag;
 
     /* calculate upper address delimiter */
     str = buf;
-	end = buf + size - 1;
+    end = buf + size - 1;
 
     if (end < buf - 1) {
-		end = ((void *)-1);
-		size = end - buf + 1;
-	}
+        end = ((void *)-1);
+        size = end - buf + 1;
+    }
 
-	for (; *fmt; fmt++) {
+    for (; *fmt; fmt++) {
         if (*fmt != '%') {
-			if (str <= end){
+            if (str <= end){
                 *str = *fmt;
             }
-			str++;
-			continue;
-		}
+            str++;
+            continue;
+        }
 
-		/* process flags */
-		flags = 0;
+        /* process flags */
+        flags = 0;
         for (is_flag = 1; 1 == is_flag;) {
-            fmt++;		/* this also skips first '%' */
+            fmt++;      /* this also skips first '%' */
             switch (*fmt) {
                 case '-':
                     flags |= LEFT;
@@ -96,89 +94,91 @@ static int javautil_vsnprintf(char *buf, size_t size, const char *fmt, va_list a
             }
         }
 
-		/* get field width */
-		field_width = -1;
-		if (isdigit(*fmt)){
+        /* get field width */
+        field_width = -1;
+        if (isdigit(*fmt)){
             field_width = skip_atoi(&fmt);
         }
 
-		switch (*fmt) {
+        switch (*fmt) {
             /* 's' format support */
             case 's':
-				s = va_arg(args, char *);
-				if (!s){
+                s = va_arg(args, char *);
+                if (!s){
                     s = "<NULL>";
                 }
 
-				len = strlen(s);
+                len = strlen(s);
 
-				if (!(flags & LEFT)) {
+                if (!(flags & LEFT)) {
                     /* alignment to right - pad with spaces */
-					while (len < field_width) {
-						if (str <= end){
+                    while (len < field_width) {
+                        if (str <= end){
                             *str = ' ';
                         }
                         field_width--;
-						str++;
-					}
-				}
-				for (i = 0; i < len; i++) {
-					if (str <= end){
+                        str++;
+                    }
+                }
+                for (i = 0; i < len; i++) {
+                    if (str <= end){
                         *str = *s;
                     }
-					str++;
+                    str++;
                     s++;
-				}
-				while (len < field_width) {
-					if (str <= end){
+                }
+                while (len < field_width) {
+                    if (str <= end){
                         *str = ' ';
                     }
-					str++;
+                    str++;
                     field_width--;
-				}
-				continue;
+                }
+                continue;
             /* Insert additional format supports here */
 
-			default:
-				continue;
-		}
-	}
-	if (str <= end){
+            default:
+                continue;
+        }
+    }
+    if (str <= end){
         *str = '\0';
     }
-	else if (size > 0){
-		/* don't write out a null byte if the buf size is zero */
-		*end = '\0';
+    else if (size > 0){
+        /* don't write out a null byte if the buf size is zero */
+        *end = '\0';
     }
 
-	return str - buf;
+    return str - buf;
 }
 
 
 /**
- * javautil_vsprintf - Format a string and place it in a buffer
- * @buf: The buffer to place the result into
- * @fmt: The format string to use
- * @args: Arguments for the format string
+ * Formats a string and places it in a buffer
+ *
+ * @param buf buffer to place the result into
+ * @param fmt format string to use
+ * @param args arguments for the format string
  */
 static int javautil_vsprintf(char *buf, const char *fmt, va_list args) {
-	return javautil_vsnprintf(buf, 0xFFFFFFFFUL, fmt, args);
+    return javautil_vsnprintf(buf, 0xFFFFFFFFUL, fmt, args);
 }
 
 
- /**
- * javautil_sprintf - Format a string and place it in a buffer
- * @buf: The buffer to place the result into
- * @fmt: The format string to use
- * @...: Arguments for the format string
+/**
+ * Formats a string and places it in a buffer
+ *
+ * @param buf buffer to place the result into
+ * @param fmt format string to use
+ * @param ... arguments for the format string
  */
 int javautil_sprintf(char *buf, const char *fmt, ...) {
-	va_list args;
-	int i;
+    va_list args;
+    int i;
 
-	va_start(args, fmt);
-	i = javautil_vsprintf(buf,fmt,args);
-	va_end(args);
-	return i;
+    va_start(args, fmt);
+    i = javautil_vsprintf(buf, fmt, args);
+    va_end(args);
+    return i;
 }
 
