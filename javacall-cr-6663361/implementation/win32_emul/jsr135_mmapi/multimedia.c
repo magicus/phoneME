068a868cca09e045c3c38eed9de02c471a4d526e
petkel <petkel@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-15 16:46:12 +0000

    JAVACALL_MEDIA_FORMAT_DEVICE_TONE and JAVACALL_MEDIA_FORMAT_DEVICE_MIDI have been added to caps

--- javacall-cr-6663361/implementation/win32_emul/jsr135_mmapi/multimedia.c
+++ javacall-cr-6663361/implementation/win32_emul/jsr135_mmapi/multimedia.c
@@ -36,6 +36,8 @@ static javacall_media_caps g_caps[] =
     { JAVACALL_MEDIA_FORMAT_SP_MIDI, "audio/sp-midi",        JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
     { JAVACALL_MEDIA_FORMAT_TONE,    "audio/x-tone-seq",     JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
     { JAVACALL_MEDIA_FORMAT_AMR,     "audio/amr",            JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_DEVICE_TONE, "audio/tone",       JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_DEVICE_MIDI, "audio/midi",       JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
     { NULL,                          NULL,                   0,                              0 }
 };
 
