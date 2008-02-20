10e5e7e940881963edd777555edfc863ffd94f40
ccarter <ccarter@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-20 16:25:50 +0000

    Copy jsr135-advanced-mr2-promo-b58 to https://phoneme.dev.java.net/svn/phoneme/builds.

---
+++ jsr135/src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
@@ -0,0 +1,313 @@
+ /*
+ * 
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
+ */
+ 
+package com.sun.mmedia;
+
+import com.sun.mmedia.DefaultConfiguration;
+import com.sun.j2me.app.AppPackage;
+
+import javax.microedition.media.*;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import javax.microedition.media.control.*;
+import java.util.*;
+
+import com.sun.mmedia.DefaultConfiguration;
+import com.sun.j2me.log.Logging;
+import com.sun.j2me.log.LogChannels;
+import com.sun.mmedia.control.DualToneControl; 
+/**
+ * Java Tone Sequence Player
+ * it implements ToneControl
+ */
+public final class DirectTone extends DirectPlayer {
+
+    private DirectToneControl toneCtl     = null;
+    private DirectToneControl dualToneCtl = null;
+
+    /**
+     * It does not need data source
+     */
+    public DirectTone() {
+        super();
+        hasDataSource = false;
+    }
+
+    /**
+     * the worker method to prefetch the player
+     *
+     * @exception  MediaException  Description of the Exception
+     */
+    protected void doPrefetch() throws MediaException {
+        /* prefetch native player */
+        nPrefetch(hNative);
+
+        // if no source stream, player is created from TONE_DEVICE_LOCATOR
+        // simply return it.
+        if (stream != null) {
+
+            // read the whole sequence from the source stream
+            int chunksize = 128;
+            byte[] tmpseqs = new byte[chunksize];
+            byte[] seqs = null;
+            // make use of BAOS, since it takes care of growing buffer
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(chunksize);
+
+            try {
+                int read;
+
+                while ((read = stream.read(tmpseqs, 0, chunksize)) != -1) {
+                    baos.write(tmpseqs, 0, read);
+                }
+
+                seqs = baos.toByteArray();
+                baos.close();
+                tmpseqs = null;
+                System.gc();
+
+            } catch (IOException ex) {
+                throw new MediaException("unable to realize: fail to read from source");
+            }
+        
+            try {
+                ToneControl tControl = new DirectToneControl(this, false);
+                tControl.setSequence(seqs);
+            } catch (Exception e) {
+                throw new MediaException("unable to realize: " + e.getMessage());
+            }
+        }
+        nAcquireDevice(hNative);
+    }
+
+    /**
+     * The worker method to actually obtain the control.
+     *
+     * @param  type  the class name of the <code>Control</code>.
+     * @return       <code>Control</code> for the class or interface
+     * name.
+     */
+    protected Control doGetControl(String type) {
+        Control c = super.doGetControl(type);
+        if (c != null) {
+            return c;
+        }
+
+        if (getState() >= REALIZED) {
+            if (type.equals("javax.microedition.media.control.ToneControl") ||
+                type.equals("ToneControl")) {
+                if( null == toneCtl ) toneCtl = new DirectToneControl(this, false);
+                return toneCtl;
+            }
+            if (type.equals("com.sun.mmedia.control.DualToneControl")) {
+                if (null == dualToneCtl) dualToneCtl = new DirectToneControl(this, true);
+                return dualToneCtl;
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Override getContentType from BasicPlayer
+     * Always return DefaultConfiguration.TONE content type
+     */
+    public String getContentType() {
+        chkClosed(true);
+        return DefaultConfiguration.MIME_AUDIO_TONE;
+    }
+
+    /**
+     * Sets the tone sequence.<p>
+     * 
+     * @param sequence The sequence to set.
+     * @exception IllegalArgumentException Thrown if the sequence is 
+     * <code>null</code> or invalid.
+     * @exception IllegalStateException Thrown if the <code>Player</code>
+     * that this control belongs to is in the <i>PREFETCHED</i> or
+     * <i>STARTED</i> state.
+     */
+    void setSequence(byte[] sequence)
+    {
+        if (this.getState() >= Player.PREFETCHED)
+            throw new IllegalStateException("cannot set seq after prefetched");
+       
+        nFlushBuffer(hNative);
+
+        long contentSize = sequence.length;
+        if (contentSize > 0) {
+            nSetWholeContentSize(hNative, contentSize);
+        }
+
+        PlayerImpl.nBuffering(hNative, sequence, sequence.length);
+        PlayerImpl.nBuffering(hNative, null, 0);
+
+        hasToneSequenceSet = true;
+    }
+
+    /**
+     * Java Tone Sequence Control
+     * it implements ToneControl
+     */
+    public final class DirectToneControl implements ToneControl, DualToneControl {
+        private DirectTone _player;
+        private boolean dualTone;
+
+        DirectToneControl(DirectTone player, boolean dualTone) {
+            _player = player;
+            this.dualTone = dualTone;
+        }
+
+        private boolean checkSequence( byte[] seq )
+        {
+            int note;
+            int blk = -1;
+            
+            if( seq.length < 4 )
+                return false;
+
+            boolean blk_defined[] = new boolean[128];
+            for (int i = 0; i < 128; i++)
+                blk_defined[i] = false;
+
+            if (seq[0] != ToneControl.VERSION || seq[1] != 1)
+                return false;
+            
+            int pos = 2;
+            
+            // 0 - version, 1 - tempo, 2 - resolution, 3 - blocks_started,
+            // 4 - block content, 5 - non-block sequences
+            int stage = 0;
+            
+            while (pos < seq.length) {
+                switch (note = seq[pos++]) {
+                    case ToneControl.TEMPO:
+                        if (stage != 0)
+                            return false; // only after version
+                        if (seq[pos] < 5 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = 1;
+                        break;
+                    case ToneControl.RESOLUTION:
+                        if (stage > 1)
+                            return false; // only after version and tempo
+                        if (seq[pos] < 1 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = 2;
+                        break;
+                    case ToneControl.BLOCK_START:
+                        if (stage > 2)
+                            return false;
+                        blk = seq[pos++];
+                        if (blk < 0 || blk > 127)
+                            return false;
+                        stage = 3;
+                        break;
+                    case ToneControl.BLOCK_END:
+                        if (stage != 4)
+                            return false; // block is empty
+                        if (-1 == blk)
+                            return false;
+                        if (seq[pos++] != blk)
+                            return false;
+                        blk_defined[blk] = true;
+                        blk = -1;
+                        stage = 3;
+                        break;
+                    case ToneControl.PLAY_BLOCK:
+                        if (seq[pos] < 0 || seq[pos] > 127)
+                            return false;
+                        if (!blk_defined[seq[pos]])
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                        break;
+                    case ToneControl.SET_VOLUME:
+                        if (seq[pos] < 0 || seq[pos] > 100)
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                        break;
+                    case ToneControl.REPEAT:
+                        if (seq[pos] < 2 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                        break;
+                    case ToneControl.SILENCE:
+                        if (seq[pos] < 1 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                        break;
+                    case DualToneControl.DUALTONE:
+                        if (!dualTone)
+                            return false;
+                        if (seq[pos] < 0 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        if (seq[pos] < 0 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        if (seq[pos] < 0 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                        break;
+                    default: // note
+                        if (note < 0 || note > 127)
+                            return false;
+                        if (seq[pos] < 1 || seq[pos] > 127)
+                            return false;
+                        pos++;
+                        stage = (blk == -1) ? 5 : 4;
+                }
+            }
+
+            return stage == 5; // at least 1 sequence event present
+        }
+
+        /**
+         * Sets the tone sequence.<p>
+         * 
+         * @param sequence The sequence to set.
+         * @exception IllegalArgumentException Thrown if the sequence is 
+         * <code>null</code> or invalid.
+         * @exception IllegalStateException Thrown if the <code>Player</code>
+         * that this control belongs to is in the <i>PREFETCHED</i> or
+         * <i>STARTED</i> state.
+         */
+        public void setSequence(byte[] sequence)
+        {
+            if (sequence == null) 
+                throw new IllegalArgumentException("null sequence");
+            if (!checkSequence(sequence)) 
+                throw new IllegalArgumentException("invalid sequence");
+            _player.setSequence(sequence);
+        }
+    }
+}
---
+++ jsr135/src/share/components/rtsp-player/classes/com/sun/mmedia/RtspUrl.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt). 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ */
+package com.sun.mmedia.rtsp;
+
+import java.io.IOException;
+
+/**
+ * Encapsulates an RTSP URL of the form rtsp://host:port/file.
+ *
+ * @author     Marc Owerfeldt
+ * @created    June 7, 2003
+ */
+public class RtspUrl {
+    /**
+     * The RTSP URL.
+     */
+    private String url;
+
+
+    /**
+     * Constructor for the RtspUrl object
+     *
+     * @param  url              The RTSP URL to be parsed.
+     * @exception  IOException  Thows an IOException if the URL cannot 
+     *                          be parsed as a valid RTP URL.
+     */
+    public RtspUrl(String url) throws IOException {
+        this.url = url;
+
+        if (url.length() < 7) {
+            throw new IOException("Malformed URL");
+        }
+
+        if (!url.startsWith("rtsp://")) {
+            throw new IOException("Malformed URL");
+        }
+    }
+
+
+    /**
+     * Gets the file attribute of the RtspUrl object.
+     *
+     * @return    The file value
+     */
+    public String getFile() {
+        String str = url.substring(7);
+
+        int start = str.indexOf('/');
+
+        String file = "";
+
+        if (start != -1) {
+            file = str.substring(start + 1);
+        }
+
+        return file;
+    }
+
+
+    /**
+     * Gets the host attribute of the RtspUrl object.
+     *
+     * @return    The host value
+     */
+    public String getHost() {
+        String host = null;
+
+        String str = url.substring(7);
+
+        int end = str.indexOf(':');
+
+        if (end == -1) {
+            end = str.indexOf('/');
+
+            if (end == -1) {
+                host = str;
+            } else {
+                host = str.substring(0, end);
+            }
+        } else {
+            host = str.substring(0, end);
+        }
+
+        return host;
+    }
+
+
+    /**
+     * Gets the port attribute of the RtspUrl object
+     *
+     * @return    The port value
+     */
+    public int getPort() {
+        int port = 554;
+        // default port for RTSP
+
+        String str = url.substring(7);
+
+        int start = str.indexOf(':');
+
+        if (start != -1) {
+            int end = str.indexOf('/');
+
+            port = Integer.parseInt(str.substring(start + 1, end));
+        }
+
+        return port;
+    }
+}
+
