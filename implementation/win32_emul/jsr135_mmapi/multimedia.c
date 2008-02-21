5129b9f1167aed66aeb0a94b256923d4ebda74db
alexey_v_bakhtin <alexey_v_bakhtin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-21 12:03:25 +0000

    CR6663620: add mime type synonyms; merged from /javacall-cr-6663620 (revisions 9797:9799); reviewed by Alexey P. and Stanislav N.; tested by TCK

--- implementation/win32_emul/jsr135_mmapi/multimedia.c
+++ implementation/win32_emul/jsr135_mmapi/multimedia.c
@@ -31,12 +31,12 @@
 static javacall_media_caps g_caps[] = 
 {
 //    mediaFormat,                   contentTypes,           'whole' protocols,              streaming protocols
-    { JAVACALL_MEDIA_FORMAT_MS_PCM,  "audio/x-wav audio/wav",JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
-    { JAVACALL_MEDIA_FORMAT_MIDI,    "audio/midi audio/mid", JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
-    { JAVACALL_MEDIA_FORMAT_SP_MIDI, "audio/sp-midi",        JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
-    { JAVACALL_MEDIA_FORMAT_TONE,    "audio/x-tone-seq",     JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
-    { JAVACALL_MEDIA_FORMAT_AMR,     "audio/amr",            JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
-    { JAVACALL_MEDIA_FORMAT_CAPTURE_AUDIO, "audio/x-wav",    JAVACALL_MEDIA_CAPTURE_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_MS_PCM,  "audio/x-wav audio/wav",             JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_MIDI,    "audio/midi audio/mid audio/x-midi", JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_SP_MIDI, "audio/sp-midi",                     JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_TONE,    "audio/x-tone-seq audio/tone",       JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_AMR,     "audio/amr",                         JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
+    { JAVACALL_MEDIA_FORMAT_CAPTURE_AUDIO, "audio/x-wav",                 JAVACALL_MEDIA_CAPTURE_PROTOCOL, 0 },
 #ifdef ENABLE_MMAPI_LIME   
     { JAVACALL_MEDIA_FORMAT_MPEG1_LAYER3, "audio/mpeg",      JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
     { JAVACALL_MEDIA_FORMAT_MPEG_1,  "video/mpeg",           JAVACALL_MEDIA_MEMORY_PROTOCOL, 0 },
@@ -313,17 +313,18 @@ javacall_media_format_type fmt_guess_from_url(javacall_const_utf16_string uri,
         javacall_media_format_type  fmt;
     } map[] =
     {
-        { L".wav",  JAVACALL_MEDIA_FORMAT_MS_PCM       },
-        { L".amr",  JAVACALL_MEDIA_FORMAT_AMR          },
-        { L".mid",  JAVACALL_MEDIA_FORMAT_MIDI         },
-        { L".midi", JAVACALL_MEDIA_FORMAT_MIDI         },
-        { L".jts",  JAVACALL_MEDIA_FORMAT_TONE         },
+        { L".wav",  JAVACALL_MEDIA_FORMAT_MS_PCM },
+        { L".amr",  JAVACALL_MEDIA_FORMAT_AMR    },
+        { L".mid",  JAVACALL_MEDIA_FORMAT_MIDI   },
+        { L".midi", JAVACALL_MEDIA_FORMAT_MIDI   },
+        { L".jts",  JAVACALL_MEDIA_FORMAT_TONE   },
 #ifdef ENABLE_MMAPI_LIME
         { L".mp3",  JAVACALL_MEDIA_FORMAT_MPEG1_LAYER3 },
         { L".mpg",  JAVACALL_MEDIA_FORMAT_MPEG_1       },
         { L".mov",  JAVACALL_MEDIA_FORMAT_MOV          },
 #endif /* ENABLE_MMAPI_LIME */
-        { L".gif",  JAVACALL_MEDIA_FORMAT_UNSUPPORTED  }
+        { L".gif",  JAVACALL_MEDIA_FORMAT_UNSUPPORTED   },
+        { L".wmv",  JAVACALL_MEDIA_FORMAT_UNSUPPORTED   }
     };
 
     int i, extlen;
