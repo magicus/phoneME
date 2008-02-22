a3938404cc65f293912d6c619f357565e909491b
xyzzy <xyzzy@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-22 06:59:38 +0000

    Unicode filesystem support has been moved from WinNT* to Win32* WinCE and WinNT both subclass Win32 now.  Non-Unicode filesystem support has been removed.

--- branches/cdc-cr-6627176/src/win32/classes/java/io/WinNTFileSystem.java
+++ branches/cdc-cr-6627176/src/win32/classes/java/io/WinNTFileSystem.java
@@ -29,7 +29,7 @@
 package java.io;
 
 /**
- * Unicode-aware FileSystem for Windows NT/2000.
+ * Unicode-aware FileSystem for Windows NT/2000 and later.
  * 
  * @author Konstantin Kladko
  * @version 1.9, 06/10/10
@@ -37,32 +37,7 @@ package java.io;
  */
 class WinNTFileSystem extends Win32FileSystem {
 
-    protected native String canonicalize0(String path)
-                                                throws IOException;
-
-    /* -- Attribute accessors -- */
-
-    public native int getBooleanAttributes(File f);
-    public native boolean checkAccess(File f, boolean write);
-    public native long getLastModifiedTime(File f);
-    public native long getLength(File f);
-
-
-    /* -- File operations -- */
-
-    public native boolean createFileExclusively(String path)
-	                                       throws IOException;
-    protected native boolean delete0(File f);
-    public synchronized native boolean deleteOnExit(File f);
-    public native String[] list(File f);
-    public native boolean createDirectory(File f);
-    protected native boolean rename0(File f1, File f2);
-    public native boolean setLastModifiedTime(File f, long time);
-    public native boolean setReadOnly(File f);
+    protected native int listRoots0();
     protected native String getDriveDirectory(int drive);
-    private static native void initIDs();
 
-    static {
-	    initIDs();
-    }
 }
