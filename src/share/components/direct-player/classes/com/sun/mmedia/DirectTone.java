f857b97a1b42d36c8b6a4316d7a9b87220446aac
alexey_v_bakhtin <alexey_v_bakhtin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-02-26 18:03:24 +0000

    CR6666581: improve media download; merged from /jsr135-cr-6666581 (revisions 9908:9967); reviewed by Alexey P., Stanislav N.; tested by TCK

--- src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
+++ src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
@@ -55,6 +55,50 @@ public final class DirectTone extends DirectPlayer {
     }
 
     /**
+     * Read header information from media data and determine media Format     *
+     * @exception  MediaException  Description of the Exception     */
+    protected void doRealize() throws MediaException {
+        // if no source stream, player is created from TONE_DEVICE_LOCATOR
+        // simply return it.
+        if (stream != null) {
+            long contLength = stream.getContentLength();
+            if (contLength == stream.tell()) {
+                return;
+            } else {
+                // read the whole sequence from the source stream
+                int chunksize = 128;
+                byte[] tmpseqs = new byte[chunksize];
+                byte[] seqs = null;
+                // make use of BAOS, since it takes care of growing buffer
+                ByteArrayOutputStream baos = new ByteArrayOutputStream(chunksize);
+
+                try {
+                    int read;
+
+                    while ((read = stream.read(tmpseqs, 0, chunksize)) != -1) {
+                        baos.write(tmpseqs, 0, read);
+                    }
+
+                    seqs = baos.toByteArray();
+                    baos.close();
+                    tmpseqs = null;
+                    System.gc();
+
+                    try {
+                        ToneControl t = new DirectToneControl(this, false);
+                        t.setSequence(seqs);
+                    } catch (Exception e) {
+                        throw new MediaException("unable to realize: " + e.getMessage());
+                    }
+
+                } catch (IOException ex) {
+                    throw new MediaException("unable to realize: fail to read from source");
+                }
+            }        
+        }
+    }
+
+    /**
      * the worker method to prefetch the player
      *
      * @exception  MediaException  Description of the Exception
@@ -62,41 +106,6 @@ public final class DirectTone extends DirectPlayer {
     protected void doPrefetch() throws MediaException {
         /* prefetch native player */
         nPrefetch(hNative);
-
-        // if no source stream, player is created from TONE_DEVICE_LOCATOR
-        // simply return it.
-        if (stream != null) {
-
-            // read the whole sequence from the source stream
-            int chunksize = 128;
-            byte[] tmpseqs = new byte[chunksize];
-            byte[] seqs = null;
-            // make use of BAOS, since it takes care of growing buffer
-            ByteArrayOutputStream baos = new ByteArrayOutputStream(chunksize);
-
-            try {
-                int read;
-
-                while ((read = stream.read(tmpseqs, 0, chunksize)) != -1) {
-                    baos.write(tmpseqs, 0, read);
-                }
-
-                seqs = baos.toByteArray();
-                baos.close();
-                tmpseqs = null;
-                System.gc();
-
-            } catch (IOException ex) {
-                throw new MediaException("unable to realize: fail to read from source");
-            }
-        
-            try {
-                ToneControl tControl = new DirectToneControl(this, false);
-                tControl.setSequence(seqs);
-            } catch (Exception e) {
-                throw new MediaException("unable to realize: " + e.getMessage());
-            }
-        }
         nAcquireDevice(hNative);
     }
 
