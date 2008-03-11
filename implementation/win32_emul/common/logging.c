10caecc529bce1ff0dd9a098be98c88ad9192091
LubomirNerad <LubomirNerad@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-03-11 14:24:45 +0000

    using win based implementation of output redirection (via sockets) from cdc

--- implementation/win32_emul/common/logging.c
+++ implementation/win32_emul/common/logging.c
@@ -42,6 +42,10 @@ extern "C" {
 #include "javacall_logging.h"
 #include "javacall_properties.h"
 
+#ifdef ENABLE_OUTPUT_REDIRECTION
+#include "io_sockets.h"
+#endif /* ENABLE_OUTPUT_REDIRECTION */
+
 char print_buffer[PRINT_BUFFER_SIZE];
 char debug_print_buffer[PRINT_BUFFER_SIZE];
 
@@ -54,6 +58,14 @@ void javacall_print(const char *s) {
     //OutputDebugString(s);
     printf("%s", s);
 	fflush(stdout);
+
+#ifdef ENABLE_OUTPUT_REDIRECTION
+    /**
+     * redirect output to sockets
+     * does nothing if the socket numbers are not set via command line
+     */
+	SIOWrite(1, s, strlen(s));
+#endif /* ENABLE_OUTPUT_REDIRECTION */
 }
 
 
