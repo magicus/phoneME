a7ccaa977a5d629e6fc055b144fda9de5b44da32
IlyaKrupenin <IlyaKrupenin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-03-12 08:11:14 +0000

    CR 6672050 -- Dangerous comparisons, unused variables, write to static field from instance Merged revisions 10243:10256 from branches/jsr135-cr-6672050 Reviewed by MMAPI team, thanks.

--- src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
+++ src/share/components/direct-player/classes/com/sun/mmedia/DirectTone.java
@@ -82,7 +82,6 @@ public final class DirectTone extends DirectPlayer {
                     seqs = baos.toByteArray();
                     baos.close();
                     tmpseqs = null;
-                    System.gc();
 
                     try {
                         ToneControl t = new DirectToneControl(this, false);
@@ -194,8 +193,8 @@ public final class DirectTone extends DirectPlayer {
 
         private boolean checkSequence( byte[] seq )
         {
-            int note;
-            int blk = -1;
+            byte note;
+            byte blk = -1;
             
             if( seq.length < 4 )
                 return false;
@@ -218,7 +217,7 @@ public final class DirectTone extends DirectPlayer {
                     case ToneControl.TEMPO:
                         if (stage != 0)
                             return false; // only after version
-                        if (seq[pos] < 5 || seq[pos] > 127)
+                        if (seq[pos] < 5)
                             return false;
                         pos++;
                         stage = 1;
@@ -226,7 +225,7 @@ public final class DirectTone extends DirectPlayer {
                     case ToneControl.RESOLUTION:
                         if (stage > 1)
                             return false; // only after version and tempo
-                        if (seq[pos] < 1 || seq[pos] > 127)
+                        if (seq[pos] < 1)
                             return false;
                         pos++;
                         stage = 2;
@@ -235,7 +234,7 @@ public final class DirectTone extends DirectPlayer {
                         if (stage > 2)
                             return false;
                         blk = seq[pos++];
-                        if (blk < 0 || blk > 127)
+                        if (blk < 0)
                             return false;
                         stage = 3;
                         break;
@@ -251,7 +250,7 @@ public final class DirectTone extends DirectPlayer {
                         stage = 3;
                         break;
                     case ToneControl.PLAY_BLOCK:
-                        if (seq[pos] < 0 || seq[pos] > 127)
+                        if (seq[pos] < 0)
                             return false;
                         if (!blk_defined[seq[pos]])
                             return false;
@@ -265,13 +264,13 @@ public final class DirectTone extends DirectPlayer {
                         stage = (blk == -1) ? 5 : 4;
                         break;
                     case ToneControl.REPEAT:
-                        if (seq[pos] < 2 || seq[pos] > 127)
+                        if (seq[pos] < 2)
                             return false;
                         pos++;
                         stage = (blk == -1) ? 5 : 4;
                         break;
                     case ToneControl.SILENCE:
-                        if (seq[pos] < 1 || seq[pos] > 127)
+                        if (seq[pos] < 1)
                             return false;
                         pos++;
                         stage = (blk == -1) ? 5 : 4;
@@ -279,21 +278,21 @@ public final class DirectTone extends DirectPlayer {
                     case DualToneControl.DUALTONE:
                         if (!dualTone)
                             return false;
-                        if (seq[pos] < 0 || seq[pos] > 127)
+                        if (seq[pos] < 0)
                             return false;
                         pos++;
-                        if (seq[pos] < 0 || seq[pos] > 127)
+                        if (seq[pos] < 0)
                             return false;
                         pos++;
-                        if (seq[pos] < 0 || seq[pos] > 127)
+                        if (seq[pos] < 0)
                             return false;
                         pos++;
                         stage = (blk == -1) ? 5 : 4;
                         break;
                     default: // note
-                        if (note < 0 || note > 127)
+                        if (note < 0)
                             return false;
-                        if (seq[pos] < 1 || seq[pos] > 127)
+                        if (seq[pos] < 1)
                             return false;
                         pos++;
                         stage = (blk == -1) ? 5 : 4;
