24912f70aa14abf6826a17fff13dfb1891fe2b50
olga_milovskaya <olga_milovskaya@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-03-13 10:02:19 +0000

    CR 6672269 TextFielItem should increase width to fill available space if LAYOUT_EXPAND directive is on Reviewed by Andrey Petushkov

--- src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/TextFieldLFImpl.java
+++ src/highlevelui/lcdlf/lfjava/classes/javax/microedition/lcdui/TextFieldLFImpl.java
@@ -433,8 +433,16 @@ class TextFieldLFImpl extends ItemLFImpl implements
        size[HEIGHT] = f.getHeight() + (2 * TextFieldSkin.PAD_V);
        size[WIDTH] = f.charWidth('W') * tf.buffer.capacity() +
             (2 * TextFieldSkin.PAD_H);
-       if (size[WIDTH] > availableWidth) {
+        
+       if (size[WIDTH] > availableWidth ) {
             size[WIDTH] = availableWidth;
+       } else if (shouldHExpand()){
+           if (size[WIDTH] + labelBounds[WIDTH] - (4 * TextFieldSkin.PAD_H) < bounds[WIDTH]) {
+                size[WIDTH] = bounds[WIDTH] - labelBounds[WIDTH] - (4 * TextFieldSkin.PAD_H);
+
+           } else {
+                size[WIDTH] = bounds[WIDTH];
+           }
        }
 
         // update scrollWidth used in scrolling UE text
@@ -473,7 +481,10 @@ class TextFieldLFImpl extends ItemLFImpl implements
      * @param height The height available for the Item's content
      */
     void lPaintContent(Graphics g, int width, int height) {
-        // Draw the TextField background region 
+        // Draw the TextField background region
+
+        System.out.println("TextFieldLFImpl.lPaintContent contentWidth=" + width);
+        System.out.println("TextFieldLFImpl.lPaintContent Width=" + bounds[WIDTH]);
         if (editable) {
             if (TextFieldSkin.IMAGE_BG != null) {
                 CGraphicsUtil.draw9pcsBackground(g, 0, 0, width, height,
