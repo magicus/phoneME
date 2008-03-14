f3690474fdba04d21fd25f25ac7204f1daf999dd
garyradams <garyradams@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-03-14 12:21:33 +0000

    Initial putback of checkout cfg files and scripts to align developer and release engineering build procedures for phone advanced builds. This should help aid open source developers with some common build variants used by the development team. This will also make adding new variants more routine.
    Not all variants are operational, yet. Followup putbacks
    will be used to straighten out any broken builds.
    
    Reviewed by Hinkmond and Yeygeney
    Tested with linux/{x86,arm} and wm5 builds.

---
+++ projects/pmea/build/win32-arm-wm5/opt_preload_jit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 only, as published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License version 2 for more details (a copy is
+# included at /legal/license.txt).
+# 
+# You should have received a copy of the GNU General Public License
+# version 2 along with this work; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# 
+# Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+# Clara, CA 95054 or visit www.sun.com if you need additional
+# information or have any questions.
+#
+
+make \
+  USE_VERBOSE_MAKE=true \
+  USE_VS2005=true \
+  CVM_COMPILER_INCOMPATIBLE=false \
+  CVM_JIT=true \
+  CVM_OPTIMIZED=true \
+  CVM_SYMBOLS=true \
+  CVM_PRELOAD_LIB=true \
+  USE_JPEG=true \
+    JPEG_DIR=$COMPONENTS_DIR/jpeg \
+  USE_MIDP=true \
+  ABSTRACTIONS_DIR=$COMPONENTS_DIR/abstractions \
+  CVM_INCLUDE_JAVACALL=false \
+  PCSL_TARGET=wince_arm  \
+  J2ME_CLASSLIB=basis \
+  CVM_BUILD_SUBDIR_NAME=opt_preload_jit \
+  $@
