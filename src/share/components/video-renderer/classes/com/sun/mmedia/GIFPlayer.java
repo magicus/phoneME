09449600da6a9ea6d5ca90eff21349550daa0b85
stanar <stanar@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-04-28 18:24:20 +0000

    the fix

--- src/share/components/video-renderer/classes/com/sun/mmedia/GIFPlayer.java
+++ src/share/components/video-renderer/classes/com/sun/mmedia/GIFPlayer.java
@@ -195,12 +195,7 @@ final public class GIFPlayer extends BasicPlayer implements Runnable {
      * @return    the media time in microseconds.
      */
     protected long doGetMediaTime() {
-        long mediaTime = getDefaultRateMediaTime() * rateControl.getRate() / 100000;
-
-        if (mediaTime > duration)
-            mediaTime = duration;
-
-        return mediaTime;
+        return getDefaultRateMediaTime() * rateControl.getRate() / 100000;
     }
 
     private long getDefaultRateMediaTime() {
@@ -478,7 +473,6 @@ final public class GIFPlayer extends BasicPlayer implements Runnable {
             startTime = 0;
 
             sendEvent(PlayerListener.END_OF_MEDIA,
-                duration != TIME_UNKNOWN ? new Long(duration) :
                       new Long(mediaTimeOffset * rateControl.getRate() / 100000));
         }
 
