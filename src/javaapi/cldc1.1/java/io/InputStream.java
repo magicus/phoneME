de54fcacfe5bf11784e31012fb69999a7df800d8
dgomon <dgomon@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-05-04 12:21:04 +0000

    CR 6680868 : ResourceInputStream and DataInputStream skip method should be optimized merged -r11047:11148 from cldc-cr-6680868. Reviewed by D. Sinopalnikov.

--- src/javaapi/cldc1.1/java/io/InputStream.java
+++ src/javaapi/cldc1.1/java/io/InputStream.java
@@ -1,24 +1,24 @@
 /*
- *   
  *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
  * 2 only, as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
  * included at /legal/license.txt).
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
- * 
+ *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
  * information or have any questions.
@@ -41,6 +41,10 @@ package java.io;
  * @since   JDK1.0, CLDC 1.0
  */
 public abstract class InputStream {
+
+    private static final int SKIP_BLOCK_SIZE = 4096;
+
+    private static final byte tmp_buf[] = new byte[SKIP_BLOCK_SIZE];
 
     /**
      * Reads the next byte of data from the input stream. The value byte is
@@ -210,14 +214,19 @@ public abstract class InputStream {
      * @exception  IOException  if an I/O error occurs.
      */
     public long skip(long n) throws IOException {
-        long m = n;
-        while (m > 0) {
-            if (read() < 0) {
-                break;
-            }
-            --m;
-        }
-        return n-m;
+	long bytesToReadTotal = n;
+	int readBytes = 0;
+
+	while (bytesToReadTotal > 0) {
+	    int bytesToRead = bytesToReadTotal < SKIP_BLOCK_SIZE ?
+		(int)bytesToReadTotal : SKIP_BLOCK_SIZE;
+		readBytes = read(tmp_buf, 0, bytesToRead);
+		if (readBytes <= 0) {
+		    break;
+		}
+		bytesToReadTotal -= readBytes;
+	}
+	return n - bytesToReadTotal;
     }
 
     /**
