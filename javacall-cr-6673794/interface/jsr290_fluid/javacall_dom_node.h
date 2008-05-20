e01fa63a2da729348562d8e4ff8453cb4c8bf79b
andrewzakonov <andrewzakonov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-05-20 12:16:12 +0000

    functions rename

--- javacall-cr-6673794/interface/jsr290_fluid/javacall_dom_node.h
+++ javacall-cr-6673794/interface/jsr290_fluid/javacall_dom_node.h
@@ -43,7 +43,7 @@ extern "C" {
  *
  */
 javacall_result 
-javacall_dom_node_name(javacall_handle handle,
+javacall_dom_node_get_node_name(javacall_handle handle,
                        javacall_utf16_string /* OUT */ nodeName,
                        int nodeNameMaxLen);
 
@@ -51,7 +51,7 @@ javacall_dom_node_name(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_node_value(javacall_handle handle,
+javacall_dom_node_get_node_value(javacall_handle handle,
                         javacall_utf16_string /* OUT */ nodeValue,
                         int nodeValueMaxLen);
                         
@@ -59,101 +59,101 @@ javacall_dom_node_value(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_set_node_value(javacall_handle handle,
+javacall_dom_node_set_node_value(javacall_handle handle,
                             javacall_utf16_string nodeValue,
                             int valueLength);
                         
 /**
  *
  */
-short javacall_dom_node_type(javacall_handle handle);
+short javacall_dom_node_get_node_type(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_parent_node(javacall_handle handle);
+long javacall_dom_node_get_parent_node(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_child_nodes(javacall_handle handle);
+long javacall_dom_node_get_child_nodes(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_first_child(javacall_handle handle);
+long javacall_dom_node_get_first_child(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_last_child(javacall_handle handle);
+long javacall_dom_node_get_last_child(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_previous_sibling(javacall_handle handle);
+long javacall_dom_node_get_previous_sibling(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_next_sibling(javacall_handle handle);
+long javacall_dom_node_get_next_sibling(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_attributes(javacall_handle handle);
+long javacall_dom_node_get_attributes(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_owner_document(javacall_handle handle);
+long javacall_dom_node_get_owner_document(javacall_handle handle);
                         
 /**
  *
  */
 javacall_result 
-javacall_dom_insert_before(javacall_handle handle,
+javacall_dom_node_insert_before(javacall_handle handle,
                            javacall_handle newHandle, 
                            javacall_handle refHandle);
                         
 /**
  *
  */
-long javacall_dom_replace_child(javacall_handle handle,
+long javacall_dom_node_replace_child(javacall_handle handle,
                                 javacall_handle newHandle, 
                                 javacall_handle oldHandle);
                         
 /**
  *
  */
-long javacall_dom_remove_child(javacall_handle handle, 
+long javacall_dom_node_remove_child(javacall_handle handle, 
                                javacall_handle oldHandle);
                         
 /**
  *
  */
-long javacall_dom_append_child(javacall_handle handle, 
+long javacall_dom_node_append_child(javacall_handle handle, 
                                javacall_handle newHandle);
                         
 /**
  *
  */
-javacall_bool javacall_dom_has_child_nodes(javacall_handle handle);
+javacall_bool javacall_dom_node_has_child_nodes(javacall_handle handle);
                         
 /**
  *
  */
-long javacall_dom_clone_node(javacall_handle handle, javacall_bool deep);
+long javacall_dom_node_clone_node(javacall_handle handle, javacall_bool deep);
                         
 /**
  *
  */
-javacall_result javacall_dom_normalize(javacall_handle handle);
+javacall_result javacall_dom_node_normalize(javacall_handle handle);
                         
 /**
  *
  */
-javacall_bool javacall_dom_is_supported(javacall_handle handle,
+javacall_bool javacall_dom_node_is_supported(javacall_handle handle,
                                javacall_utf16_string feature,
                                javacall_utf16_string version);
                         
@@ -161,7 +161,7 @@ javacall_bool javacall_dom_is_supported(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_namespace_URI(javacall_handle handle,
+javacall_dom_node_get_namespace_URI(javacall_handle handle,
                            javacall_utf16_string /* OUT */ uri,
                            int uriMaxLen);
                         
@@ -169,7 +169,7 @@ javacall_dom_namespace_URI(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_prefix(javacall_handle handle,
+javacall_dom_node_get_prefix(javacall_handle handle,
                     javacall_utf16_string /* OUT */ prefix,
                     int prefixMaxLen);
                         
@@ -177,27 +177,27 @@ javacall_dom_prefix(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_set_prefix(javacall_handle handle, 
+javacall_dom_node_set_prefix(javacall_handle handle, 
                         javacall_utf16_string prefix);
                         
 /**
  *
  */
 javacall_result
-javacall_dom_local_name(javacall_handle handle,
+javacall_dom_node_get_local_name(javacall_handle handle,
                         javacall_utf16_string /* OUT */ name,
                         int nameMaxLen);
                         
 /**
  *
  */
-javacall_bool javacall_dom_has_attributes(javacall_handle handle);
+javacall_bool javacall_dom_node_has_attributes(javacall_handle handle);
                         
 /**
  *
  */
 javacall_result
-javacall_dom_text_content(javacall_handle handle,
+javacall_dom_node_get_text_content(javacall_handle handle,
                            javacall_utf16_string /* OUT */ text,
                            int textMaxLen);
                         
@@ -205,7 +205,7 @@ javacall_dom_text_content(javacall_handle handle,
  *
  */
 javacall_result
-javacall_dom_set_text_content(javacall_handle handle,
+javacall_dom_node_set_text_content(javacall_handle handle,
                               javacall_utf16_string text,
                               int valueLength);
 
