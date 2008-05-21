2d30abd8f0e2b39a4b6e32f6b79d20f539c14c96
gilwasserstrom <gilwasserstrom@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-05-21 08:30:04 +0000

    file name change

--- src/security/midp_permissions/reference/classes/com/sun/midp/security/DomainPolicy.java
+++ src/security/midp_permissions/reference/classes/com/sun/midp/security/DomainPolicy.java
@@ -6,13 +6,13 @@
 package com.sun.midp.security;
 
 
-public class PolicyDomain {
+public class DomainPolicy {
     private String name;
     private boolean isTrusted;
     private byte[] defValues;
     private byte[] maxValues;
     
-    public PolicyDomain(String name,  boolean isTrusted) {
+    public DomainPolicy(String name,  boolean isTrusted) {
         this.name = name;
         this.isTrusted = isTrusted;
         loadValues();
@@ -49,29 +49,5 @@ public class PolicyDomain {
             defValues[i1] = Permissions.getDefaultValue(name, group);
             maxValues[i1] = Permissions.getMaxValue(name, group);
         }
-
-	/*
-	PermissionGroup [] list = Permissions.getSettingGroups();
-	System.out.println("\ndomain: " + name);
-	for (int i1 = 0; i1 < list.length; i1++) {
-	    String group = list[i1].getNativeName();
-	    System.out.println("          group: " + group + 
-			       ",  [d] " + value2str(Permissions.getDefaultValue(name, group)) +
-			       ",  [m] " + value2str(Permissions.getMaxValue(name, group)));
-	}
-	*/
-    }
-
-    private String value2str(byte value) {
-	switch ((int)value) {
-	case Permissions.ALLOW: return "allow";
-	case Permissions.BLANKET_GRANTED: return "blanket_granted";
-	case Permissions.BLANKET: return "blanket";
-	case Permissions.SESSION: return "session";
-	case Permissions.ONESHOT: return "oneshot";
-	case Permissions.BLANKET_DENIED: return "blanket_denied";
-	case Permissions.NEVER:
-	default: return "never";
-	}
     }
 }
