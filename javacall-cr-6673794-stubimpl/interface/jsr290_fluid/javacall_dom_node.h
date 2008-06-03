e3a917f5ce856a264b736753cf70f879d65ec508
n_medvedenko <n_medvedenko@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-06-03 12:55:50 +0000

    change copyrights

--- javacall-cr-6673794-stubimpl/interface/jsr290_fluid/javacall_dom_node.h
+++ javacall-cr-6673794-stubimpl/interface/jsr290_fluid/javacall_dom_node.h
@@ -1,6 +1,5 @@
 /*
-* Copyright  2000-2008 Sun Microsystems, Inc. All Rights
-* Reserved.  Use is subject to license terms.
+* Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
 * 
 * This program is free software; you can redistribute it and/or
