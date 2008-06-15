eb4aa6d404ec5d82fbd01af70e49a46421e02192
gilwasserstrom <gilwasserstrom@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-06-15 15:28:58 +0000

    typo

--- interface/midp/javacall_security.h
+++ interface/midp/javacall_security.h
@@ -240,7 +240,7 @@ void javanotify_security_permission_dialog_finish(
 /**
  *  Load list of security domains. The function returns in
  *  <code>array</code> an array of C strings contains the list.
- *  The callee is responsible to free the return pointer with
+ *  The caller is responsible to free the return pointer with
  *  javacall_free()
  * @param array a placeholder for the domain strings list that returns
  * @retval The number of strings in the list or 0 on error
@@ -251,7 +251,7 @@ int javacall_load_domain_list(javacall_utf8_string* array);
 /**
  *  Load list of groups. The function returns in
  *  <code>array</code> an array of C strings contains the list.
- *  The callee is responsible to free the return pointer with
+ *  The caller is responsible to free the return pointer with
  *  javacall_free()
  *  @param array a placeholder for the groups strings list
  *  that returns
@@ -262,7 +262,7 @@ int javacall_load_group_list(javacall_utf8_string* array);
 /** 
  * Load list of permissions members of the requested group. The 
  * function returns in <code>list</code> an array of C strings 
- *  contains the list. The callee is responsible to free the
+ *  contains the list. The caller is responsible to free the
  *  return pointer with javacall_free()
  * @param list a placeholder for the permission members strings list that 
  * returns
@@ -294,7 +294,7 @@ int javacall_get_max_value(javacall_utf8_string domain_name,
  * Load list of messages that are used in the UI dialogs when
  * propmting the user of specific action of the group. The 
  * function returns in <code>list</code> an array of C strings 
- *  contains the list. The callee is responsible to free the
+ *  contains the list. The caller is responsible to free the
  *  return pointer with javacall_free()
  * @param list a placeholder for the groups strings list that returns
  * @param group_name the name of the group
