c5a94ce12196f82c2ab7abaa7c705dbcba825861
wonkim <wonkim@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-06-16 15:20:40 +0000

    CR 6714249: resource limit structure does not initialized properly when 2nd time JVM launching and do some code cleanup. Merged -r11752:11772 from midp-cr-6714249.
    Reviewed by Alexey Zavitaev.

--- src/core/resource_manager/include/midpResourceLimit.h
+++ src/core/resource_manager/include/midpResourceLimit.h
@@ -54,7 +54,7 @@ typedef enum {
     RSC_TYPE_AUDIO_CHA,
     RSC_TYPE_IMAGE_MUT,
     RSC_TYPE_IMAGE_IMMUT,
-    RSC_TYPE_COUNT // Total number of resources
+    RSC_TYPE_COUNT /* Total number of resources */
 } RscType; 
 
 /**
@@ -110,6 +110,11 @@ extern int midpIncResourceCount(RscType type, int delta);
  *
  */
 extern int midpDecResourceCount(RscType type, int delta);
+
+/**
+ * Finalize the Resource limit structures.
+ */
+extern void midpFinalizeResourceLimit(void);
 
 #ifdef __cplusplus
 }
