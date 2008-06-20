27da0f3cdd91a8c71aa2bdfd44d253a5ec4d9b59
wonkim <wonkim@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-06-20 08:06:32 +0000

    Add new porting API 'javacall_os_flush_icache' to ease OsMisc_flush_icache CLDC function porting

--- javacall-cr-6710350/interface/common/javacall_os.h
+++ javacall-cr-6710350/interface/common/javacall_os.h
@@ -30,7 +30,7 @@
 extern "C" {
 #endif
 
-/*
+/**
  * Initialize the OS structure.
  * This is where timers and threads get started for the first
  * real_time_tick event, and where signal handlers and other I/O
@@ -40,13 +40,23 @@ extern "C" {
 void javacall_os_initialize(void);
 
 
-/*
+/**
  * Performs a clean-up of all threads and other OS related activity
  * to allow for a clean and complete restart.  This should undo
  * all the work that initialize does.
  */
 void javacall_os_dispose();
 
+
+/** 
+ * javacall_os_flush_icache is used, for example, to flush any caches used by a
+ * code segment that is deoptimized or moved during a garbage collection.
+ * flush at least [address, address + size] (may flush complete icache).
+ *
+ * @param address   Start address to flush
+ * @param size      Size to flush
+ */
+void javacall_os_flush_icache(unsigned char* address, int size);
 
 #ifdef __cplusplus
 }
