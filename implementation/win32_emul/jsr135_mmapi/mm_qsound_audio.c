21fd787afa5c3815fc97b2fc19d7c6b2567cb553
IlyaKrupenin <IlyaKrupenin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-02 13:13:15 +0000

    fix for AMR playback

--- implementation/win32_emul/jsr135_mmapi/mm_qsound_audio.c
+++ implementation/win32_emul/jsr135_mmapi/mm_qsound_audio.c
@@ -1209,7 +1209,6 @@ static javacall_result audio_qs_acquire_device(javacall_handle handle)
             }
 #endif 
             h->wav.bytesPerMilliSec = (h->wav.rate * h->wav.channels * (16 >> 3)) / 1000;
-            h->hdr.dataEnded        = TRUE;
 
             if(h->wav.stream != NULL)
             {
@@ -1762,9 +1761,15 @@ static javacall_result audio_qs_get_duration(javacall_handle handle, long* ms) {
         break;
 
         case JC_FMT_MS_PCM:
-        case JC_FMT_AMR: // will need revisit when real streaming will be used
         {
             if(h->wav.bytesPerMilliSec != 0 && h->hdr.dataEnded)
+                *ms = h->wav.playBufferLen / h->wav.bytesPerMilliSec;
+        }
+        break;
+
+        case JC_FMT_AMR: // will need revisit when real streaming will be used
+        {
+            if(h->wav.bytesPerMilliSec != 0 && h->hdr.dataStopped)
                 *ms = h->wav.playBufferLen / h->wav.bytesPerMilliSec;
         }
         break;
