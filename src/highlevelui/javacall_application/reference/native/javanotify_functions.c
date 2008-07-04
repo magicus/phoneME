45fa826a5bf0e4d1c7f535564e659cda041b3b80
IlyaKrupenin <IlyaKrupenin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-04 11:15:10 +0000

    Quickfix for ARM build. wchar_t replaced by javacall_utf16.

--- src/highlevelui/javacall_application/reference/native/javanotify_functions.c
+++ src/highlevelui/javacall_application/reference/native/javanotify_functions.c
@@ -1259,7 +1259,7 @@ void javanotify_on_amms_notification(javacall_amms_notification_type type,
     case JAVACALL_EVENT_AMMS_SNAP_SHOOTING_STOPPED:
     case JAVACALL_EVENT_AMMS_SNAP_STORAGE_ERROR:
         {
-            size_t size = sizeof( wchar_t ) * ( 1 + wcslen( (javacall_utf16_string)data ) );
+            size_t size = sizeof( javacall_utf16 ) * ( 1 + wcslen( (javacall_utf16_string)data ) );
             e.data.multimediaEvent.data.str16 = (javacall_utf16_string)malloc( size );
             wcscpy( e.data.multimediaEvent.data.str16, (javacall_utf16_string)data );
         }
