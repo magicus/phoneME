a4cab9d2823cfe9cb92c12cb07d19376a569013d
RomanZ <RomanZ@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-11 14:36:13 +0000

    CR 6723558: JSR248TCK-api/OTA/security/DefinedPermissions/DisplayPIMEntryUpdate.html#MAIN test fails. Merged from midp-cr-6723558 12368:12380. Reviewed by Andrey Nazarov.

--- src/security/midp_permissions/reference/classes/com/sun/midp/security/PermissionGroup.java
+++ src/security/midp_permissions/reference/classes/com/sun/midp/security/PermissionGroup.java
@@ -1,7 +1,5 @@
 /*
- *   
- *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
@@ -90,6 +88,7 @@ public final class PermissionGroup {
         disableSettingChoice = theDisableSettingChoice;
         runtimeDialogTitle = theRuntimeDialogTitle;
         runtimeQuestion = theRuntimeQuestion;
+        runtimeOneshotQuestion = theRuntimeOneshotQuestion;
         identifiedMaxiumLevel = theIdentifiedMaxiumLevel;
         identifiedDefaultLevel = theIdentifiedDefaultLevel;
         unidentifiedMaxiumLevel = theUnidentifiedMaxiumLevel;
