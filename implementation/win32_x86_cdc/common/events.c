c30c17eadac1488b4e4ddfe50ad5f1487e07d7a5
RomanZ <RomanZ@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-21 10:24:39 +0000

    Added CVM-specific event functions.

--- implementation/win32_x86_cdc/common/events.c
+++ implementation/win32_x86_cdc/common/events.c
@@ -1,6 +1,5 @@
 /*
- *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
@@ -152,45 +151,34 @@ javacall_result javacall_events_finalize() {
 }
 
 /**
- * Waits for an incoming event message and copies it to user supplied
- * data buffer
- * IMPL_NOTE:   binaryBuffer must be aligned to 4 bytes and contain 
- *              the subsystem id in first four bytes
- * @param timeTowaitInMillisec max number of seconds to wait
- *              if waitForever is false
- * @param binaryBuffer user-supplied buffer to copy event to
+ * Waits for an incoming event in the queue with the given ID and copies it to
+ * a user supplied data buffer.
+ *
+ * @param queueId identifier of an event queue, typically a JSR number.
+ * @param binaryBuffer user-supplied buffer to copy event to.
  * @param binaryBufferMaxLen maximum buffer size that an event can be
  *              copied to.
- *              If an event exceeds the binaryBufferMaxLen, then the first
- *              binaryBufferMaxLen bytes of the events will be copied
- *              to user-supplied binaryBuffer, and JAVACALL_OUT_OF_MEMORY will
- *              be returned
- * @param outEventLen user-supplied pointer to variable that will hold actual
- *              event size received
- *              Platform is responsible to set this value on success to the
- *              size of the event received, or 0 on failure.
+ * @param outEventLen user-supplied pointer to variable that will hold actual 
+ *              event size received, or desired buffer size if
+ *              binaryBufferMaxLen is insufficient.
  *              If outEventLen is NULL, the event size is not returned.
  * @return <tt>JAVACALL_OK</tt> if an event successfully received,
- *         <tt>JAVACALL_FAIL</tt> or if failed or no messages are avaialable
- *         <tt>JAVACALL_OUT_OF_MEMORY</tt> If an event's size exceeds the
- *         binaryBufferMaxLen
+ *         <tt>JAVACALL_OUT_OF_MEMORY</tt> if event size exceeds
+ *         binaryBufferMaxLen,
+ *         <tt>JAVACALL_FAIL</tt> on any other error.
  */
-javacall_result javacall_event_receive(
-                            long                      timeTowaitInMillisec,
-                            /*OUT*/ unsigned char*    binaryBuffer,
-                            /*IN*/  int               binaryBufferMaxLen,
-                            /*OUT*/ int*              outEventLen) {
+javacall_result javacall_event_receive_cvm(int queueId,
+    /*OUT*/ unsigned char *binaryBuffer, int binaryBufferMaxLen,
+    /*OUT*/ int *outEventLen) {
+
     javacall_bool ok = JAVACALL_TRUE;
     EventQueue *q;
     int totalRead = 0;
-    // extract the subsystem id (JSRno) from the message
-    // IMPL_NOTE: binaryBuffer must be aligned to javacall_int32
-    javacall_int32 *event = (javacall_int32 *) binaryBuffer;
 
     if (binaryBuffer == NULL || binaryBufferMaxLen <= sizeof (javacall_int32)) {
         return JAVACALL_FAIL;
     }
-    q = find_event_queue(event[0]);
+    q = find_event_queue(queueId);
     if (q == NULL) {
         return JAVACALL_FAIL;
     }
@@ -203,7 +191,7 @@ javacall_result javacall_event_receive(
             return JAVACALL_FAIL;
         }
         if (ok) {
-            totalRead = dequeueEventMessage(q, (unsigned char*) &event[1], 
+            totalRead = dequeueEventMessage(q, binaryBuffer,
                     binaryBufferMaxLen - sizeof (javacall_int32));
             if (q->head == NULL) {
                 ResetEvent(q->events_handle);
@@ -211,7 +199,7 @@ javacall_result javacall_event_receive(
             ok = ReleaseMutex(q->events_mutex);
         }
         if (0 == totalRead) {
-            ok = checkForEvents(q, timeTowaitInMillisec);
+            ok = checkForEvents(q, -1);
         } else {
             break;
         }
@@ -225,29 +213,26 @@ javacall_result javacall_event_receive(
 }
 
 /**
- * copies a user supplied event message to a queue of messages
- * IMPL_NOTE:   binaryBuffer must be aligned to 4 bytes and contain 
- *              the subsystem id in first four bytes
+ * Copies a user supplied event message to a queue of messages and wakes up the
+ * thread that is waiting for events on the queue with the given id.
  *
- * @param binaryBuffer a pointer to binary event buffer to send
- *        The platform should make a private copy of this buffer as
- *        access to it is not allowed after the function call.
- * @param binaryBufferLen size of binary event buffer to send
- * @return <tt>JAVACALL_OK</tt> if an event successfully sent,
- *         <tt>JAVACALL_FAIL</tt> or negative value if failed
+ * @param queueId identifier of an event queue, typically a JSR number.
+ * @param binaryBuffer a pointer to binary event buffer to send. The first int
+ *        is the event id.
+ * @param binaryBufferLen size of binary event buffer to send.
+ * @return <tt>JAVACALL_OK</tt> if an event has been successfully sent,
+ *         <tt>JAVACALL_FAIL</tt> otherwise.
  */
-javacall_result javacall_event_send(unsigned char* binaryBuffer,
-                                    int binaryBufferLen) {
+javacall_result javacall_event_send_cvm(int queueId,
+    unsigned char* binaryBuffer, int binaryBufferLen) {
+
     javacall_bool ok;
-    // extract the subsystem id (JSRno) from the message
-    // IMPL_NOTE: binaryBuffer must be aligned to javacall_int32
-    javacall_int32 *event = (javacall_int32 *) binaryBuffer;
     EventQueue *q;
 
     if (binaryBuffer == NULL || binaryBufferLen <= sizeof (javacall_int32)) {
         return JAVACALL_FAIL;
     }
-    q = find_event_queue(event[0]);
+    q = find_event_queue(queueId);
     if (q == NULL) {
         return JAVACALL_FAIL;
     }
@@ -257,7 +242,7 @@ javacall_result javacall_event_send(unsigned char* binaryBuffer,
         return JAVACALL_FAIL;
     }
     if (ok) {
-        enqueueEventMessage(q, (unsigned char*) &event[1], 
+        enqueueEventMessage(q, binaryBuffer,
                 binaryBufferLen - sizeof (javacall_int32));
         ok = ReleaseMutex(q->events_mutex);
     }
