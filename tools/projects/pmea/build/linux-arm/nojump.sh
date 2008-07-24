55d0caddb0a13da930babfea5ab59bb73874578b
amy_wang <amy_wang@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-24 11:11:21 +0000

    Copy tools-advanced-mr2-promo-b81 to https://phoneme.dev.java.net/svn/phoneme/builds.

---
+++ tools/projects/pmea/build/linux-arm/nojump.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version
+# 2 only, as published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License version 2 for more details (a copy is
+# included at /legal/license.txt).
+# 
+# You should have received a copy of the GNU General Public License
+# version 2 along with this work; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+# 
+# Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+# Clara, CA 95054 or visit www.sun.com if you need additional
+# information or have any questions.
+#
+make \
+  AWK=awk \
+  J2ME_CLASSLIB=basis \
+  CVM_BUILD_SUBDIR_NAME=nojump \
+  QTEMBEDDED=true \
+  QTOPIA=false \
+  QT_TARGET_DIR=/opt/Qtopia_arm \
+  CVM_TARGET_TOOLS_PREFIX=/opt/montavista/cee/devkit/arm/v4t_le/armv4tl-hardhat-linux/bin/ \
+  JC_DIR=/opt/java_card_kit-2_2_1 \
+  USE_MIDP=true \
+  USE_JUMP=false \
+   $@
