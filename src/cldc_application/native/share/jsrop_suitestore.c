af983707e445aef0bf4641b1e2901c8486d1af56
oren <oren@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-07-30 08:14:29 +0000

    Removed warnings from build

--- src/cldc_application/native/share/jsrop_suitestore.c
+++ src/cldc_application/native/share/jsrop_suitestore.c
@@ -43,7 +43,7 @@ int jsrop_suiteid_string_size(SuiteIdType suite_id){
  * @return non zero in case of success zero in case of error
  */
 int jsrop_suiteid_to_string(SuiteIdType suite_id, /* OUT */ jchar* suite_id_str_out){
-	char* chrs= midp_suiteid2chars(suite_id);
+	const char* chrs= midp_suiteid2chars(suite_id);
 	if (!chrs) return 0;
 	while (*chrs) *suite_id_str_out++ = (jchar)*chrs++;
 	*suite_id_str_out=0;
