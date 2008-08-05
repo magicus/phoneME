aa5181206ea382b03421b54fd5376fb5ba37b800
sergio_n <sergio_n@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-08-05 10:02:32 +0000

    Copy for CR6731377 fix review

---
+++ javacall-cr-6731377-review/implementation/win32_emul/jsr135_mmapi/javautil_media.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt). 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions. 
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+ * IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ */
+ 
+#ifndef __JAVAUTIL_MEDIA_H
+#define __JAVAUTIL_MEDIA_H
+
+/**
+ * Get PNG buffer size for image that has width and height
+ * 
+ * @param width     Width of image
+ * @param height    Height of image
+ * 
+ * @return PNG buffer size need by javautil_media_rgb_to_png API
+ */
+int javautil_media_get_png_size(int width, int height);
+
+/**
+ * Encode RGB888 format data to PNG data format (there is no compression)
+ * 
+ * @param input     Pointer to RGB565 data
+ * @param output    Pointer to PNG encode buffer
+ * @param width     Width of image
+ * @param height    Height of image
+ * 
+ * @return Byte size of encoded PNG data
+ */
+int javautil_media_rgb_to_png(unsigned char *input, 
+                              unsigned char *output,
+                              int width, 
+                              int height);
+
+/**
+ * Encode rgbX888 format data to PNG data format (there is no compression)
+ * 
+ * @param input     Pointer to rgbX888 data
+ * @param output    Pointer to PNG encode buffer
+ * @param width     Width of image
+ * @param height    Height of image
+ * 
+ * @return Byte size of encoded PNG data
+ */
+int javautil_media_rgbX888_to_png(unsigned char *input, 
+                                  unsigned char *output,
+                                  int width, 
+                                  int height);
+
+#endif  /* __JAVAUTIL_MEDIA_H */
