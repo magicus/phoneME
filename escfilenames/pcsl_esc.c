2fe767cb009f8f5928f608a43bd42df1c01c7e01
MichaelG256 <MichaelG256@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-08-11 17:12:32 +0000

    minor bug

--- escfilenames/pcsl_esc.c
+++ escfilenames/pcsl_esc.c
@@ -461,6 +461,7 @@ void pcsl_esc_extract_attached(const int offset, const pcsl_string *src, pcsl_st
         case PCSL_ESC_NEW_BLOCK:
         case PCSL_ESC_TOGGLE:
         case PCSL_ESC_FULL_CODES:
+	  {
             int tuple, nbytes = 0, utf16, utf16_incomplete = 0;
             int cmd = c;
             ++p;
@@ -510,6 +511,7 @@ void pcsl_esc_extract_attached(const int offset, const pcsl_string *src, pcsl_st
                     }
                 }
             } while (nbytes != 0 || p < plimit && *p != PCSL_ESC_TOGGLE);
+	  }
         }
         p++;
     }
