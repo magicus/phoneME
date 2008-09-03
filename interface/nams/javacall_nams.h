1de9f442533475e6e2b33f9194eee87222c2c498
sergio_n <sergio_n@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-09-03 17:14:52 +0000

    Permission check functions were moved to javacall_security.h

--- interface/nams/javacall_nams.h
+++ interface/nams/javacall_nams.h
@@ -1,5 +1,5 @@
 /*
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
@@ -731,50 +731,6 @@ javacall_result
 javacall_ams_set_permissions(javacall_suite_id suiteID,
                              javacall_ams_permission_set* pPermissions);
 
-/**
- * Checks for security permission.
- * @param suite_id      the MIDlet Suite the permission should be checked with
- * @param permission    permission type
- * @param enable_block  enable user interaction. If it is
- *                      JAVACALL_FALSE the call should never be blocked.
- * @param result        address of variable to receive the
- *                      security status (1 - granted, 0 -
- *                      denied), or the handle of check session
- *                      that blocks this call. Actual result
- *                      will be notified through
- *                      <code>javanotify_security_set_permission</code>.
- * 
- * @return JAVACALL_OK if check was performed correctly and
- *         result is stored at <code>result</code>,
- *         JAVACALL_FALSE if the function fails to perform
- *         checking, JAVACALL_WOULD_BLOCK if user intercation
- *         dialog is created and actual result will be delivered
- *         later.
- * @note the function MUST NOT return JAVACALL_WOULD_BLOCK if
- *       <code>enable_block</code> equals to JAVACALL_FALSE
- * @note it is possible to have several security session in
- *       parallel.
- */
-javacall_result
-javacall_ams_check_permission(const javacall_suite_id suite_id,
-                              const javacall_ams_permission permission,
-                              const javacall_bool enable_block,
-                              unsigned int* const result);
-/**
- * Notifies the result of permission check.
- * 
- * @param suite_id      the MIDlet Suite the permission was
- *                      checked with
- * @param permission    permission type
- * @param session       the handle of security session the java
- *                      thread waiting for
- * @param result        the result of permission check (1 -
- *                      granted, 0 - denied)
- */
-void javanotify_ams_permission_check_result(const javacall_suite_id suite_id, 
-                                            const javacall_ams_permission permission,
-                                            const unsigned int session,
-                                            const unsigned int result);
 
 /**
  * Get specified property value of the suite.
