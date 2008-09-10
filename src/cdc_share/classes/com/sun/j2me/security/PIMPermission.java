8a957b2c82f57631397e4365711ab121ed9d24dd
RomanZ <RomanZ@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-09-10 08:22:18 +0000

    CR 6740397: JSR248TCK: api/OTA/security/DefinedPermissions/DisplayPIMEntryUpdate.html#UNTRUSTED test fail. Merged from abstractions-cr-6740397 13669:13721. Reviewed by Andrey Nazarov, Gil Wasserstrom, Alexey Zavitaev.

--- src/cdc_share/classes/com/sun/j2me/security/PIMPermission.java
+++ src/cdc_share/classes/com/sun/j2me/security/PIMPermission.java
@@ -1,54 +1,60 @@
 /*
- *
- *
- * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
- * 2 only, as published by the Free Software Foundation. 
+ * 2 only, as published by the Free Software Foundation.
  * 
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
- * included at /legal/license.txt). 
+ * included at /legal/license.txt).
  * 
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA 
+ * 02110-1301 USA
  * 
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
- * information or have any questions. 
+ * information or have any questions.
  */
 
 package com.sun.j2me.security;
 
+import com.sun.j2me.i18n.Resource;
+import com.sun.j2me.i18n.ResourceConstants;
 
 /**
  * PIM access permissions.
  */
 public class PIMPermission extends Permission {
     
-    static public PIMPermission CONTACT_READ  = new PIMPermission(
-        "javax.microedition.pim.ContactList.read", null);
+    static public final PIMPermission CONTACT_READ = new PIMPermission(
+        "javax.microedition.pim.ContactList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_CONTACTS));
 
-    static public PIMPermission CONTACT_WRITE = new PIMPermission(
-        "javax.microedition.pim.ContactList.write", null);
+    static public final PIMPermission CONTACT_WRITE = new PIMPermission(
+        "javax.microedition.pim.ContactList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_CONTACTS));
 
-    static public PIMPermission EVENT_READ = new PIMPermission(
-        "javax.microedition.pim.EventList.read", null);
+    static public final PIMPermission EVENT_READ = new PIMPermission(
+        "javax.microedition.pim.EventList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_EVENTS));
 
-    static public PIMPermission EVENT_WRITE = new PIMPermission(
-        "javax.microedition.pim.EventList.write", null);
+    static public final PIMPermission EVENT_WRITE = new PIMPermission(
+        "javax.microedition.pim.EventList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_EVENTS));
 
-    static public PIMPermission TODO_READ = new PIMPermission(
-        "javax.microedition.pim.ToDoList.read", null);
+    static public final PIMPermission TODO_READ = new PIMPermission(
+        "javax.microedition.pim.ToDoList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_TODO));
 
-    static public PIMPermission TODO_WRITE = new PIMPermission(
-        "javax.microedition.pim.ToDoList.write", null);
+    static public final PIMPermission TODO_WRITE = new PIMPermission(
+        "javax.microedition.pim.ToDoList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_TODO));
     
     public PIMPermission(String name, String resource) {
         super(name, resource);
--- src/cldc_application/classes/com/sun/j2me/security/PIMPermission.java
+++ src/cldc_application/classes/com/sun/j2me/security/PIMPermission.java
@@ -1,21 +1,21 @@
 /*
- * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
- * 2 only, as published by the Free Software Foundation. 
+ * 2 only, as published by the Free Software Foundation.
  * 
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
- * included at /legal/license.txt). 
+ * included at /legal/license.txt).
  * 
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA 
+ * 02110-1301 USA
  * 
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
@@ -24,34 +24,38 @@
 
 package com.sun.j2me.security;
 
+import com.sun.j2me.i18n.Resource;
+import com.sun.j2me.i18n.ResourceConstants;
 
+/**
+ * PIM access permissions.
+ */
 public class PIMPermission extends Permission {
     
-    static String LIST_ACCESS_READ  = "read";
-    static String LIST_ACCESS_WRITE = "write";
+    static public final PIMPermission CONTACT_READ = new PIMPermission(
+        "javax.microedition.pim.ContactList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_CONTACTS));
+
+    static public final PIMPermission CONTACT_WRITE = new PIMPermission(
+        "javax.microedition.pim.ContactList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_CONTACTS));
+
+    static public final PIMPermission EVENT_READ = new PIMPermission(
+        "javax.microedition.pim.EventList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_EVENTS));
+
+    static public final PIMPermission EVENT_WRITE = new PIMPermission(
+        "javax.microedition.pim.EventList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_EVENTS));
+
+    static public final PIMPermission TODO_READ = new PIMPermission(
+        "javax.microedition.pim.ToDoList.read",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_TODO));
+
+    static public final PIMPermission TODO_WRITE = new PIMPermission(
+        "javax.microedition.pim.ToDoList.write",
+        Resource.getString(ResourceConstants.ABSTRACTIONS_PIM_TODO));
     
-    static String LIST_TYPE_CONTACT = "javax.microedition.pim.ContactList.";
-    static String LIST_TYPE_EVENT   = "javax.microedition.pim.EventList.";
-    static String LIST_TYPE_TODO    = "javax.microedition.pim.ToDoList.";
-    
-    static public PIMPermission CONTACT_READ =
-        new PIMPermission(LIST_TYPE_CONTACT + LIST_ACCESS_READ, null);
-
-    static public PIMPermission CONTACT_WRITE =
-        new PIMPermission(LIST_TYPE_CONTACT + LIST_ACCESS_WRITE, null);
-
-    static public PIMPermission EVENT_READ =
-        new PIMPermission(LIST_TYPE_EVENT + LIST_ACCESS_READ, null);
-
-    static public PIMPermission EVENT_WRITE =
-        new PIMPermission(LIST_TYPE_EVENT + LIST_ACCESS_WRITE, null);
-
-    static public PIMPermission TODO_READ =
-        new PIMPermission(LIST_TYPE_TODO + LIST_ACCESS_READ, null);
-
-    static public PIMPermission TODO_WRITE =
-        new PIMPermission(LIST_TYPE_TODO + LIST_ACCESS_WRITE, null);
-
     public PIMPermission(String name, String resource) {
         super(name, resource);
     }
