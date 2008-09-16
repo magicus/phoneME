62de690723e46b4beac64f1681bd7b19518ece00
leonidr <leonidr@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-09-16 11:32:37 +0000

    Works in progress

--- src/events/input_port/qvfb/native/qvfb_handle_input.c
+++ src/events/input_port/qvfb/native/qvfb_handle_input.c
@@ -34,8 +34,6 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include <midpServices.h>
 #include <midpEvents.h>
@@ -95,8 +93,8 @@ void handle_key_port(MidpReentryData* pNewSignal, MidpEvent* pNewMidpEvent) {
     struct QVFbKeyEvent {
         unsigned int unicode;
         unsigned int modifiers;
-        char press;
-        char repeat;
+        int press;
+        int repeat;
     } qvfbKeyEvent;
 
     /* IMPL_NOTE: We don't handle repeats, but this seems OK. When you hold */
@@ -105,10 +103,6 @@ void handle_key_port(MidpReentryData* pNewSignal, MidpEvent* pNewMidpEvent) {
     read(fbapp_get_keyboard_fd(), &qvfbKeyEvent, sizeof(qvfbKeyEvent));
     midpKeyCode = map_raw_keycode(qvfbKeyEvent.unicode);
     isPressed = qvfbKeyEvent.press ? KNI_TRUE : KNI_FALSE;
-    if (midpKeyCode == KEYMAP_KEY_SELECT){
-        fprintf(stderr, "SELECT: %d\n", isPressed);
-    }
-    
     repeatSupport = KNI_FALSE;
 
     fbapp_map_keycode_to_event(
