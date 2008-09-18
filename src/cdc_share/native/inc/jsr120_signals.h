196beee7ac308d681fab82cddd570374e55a6370
RomanZ <RomanZ@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-09-18 12:13:22 +0000

    CR 6748283: Integrate optional JSRs into ARM build. Merged from jsr120-cr-6748283 13855:13860. Reviewed by Boris Ulasevich.

--- src/cdc_share/native/inc/jsr120_signals.h
+++ src/cdc_share/native/inc/jsr120_signals.h
@@ -1,7 +1,5 @@
 /*
- *
- *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  *
  * This program is free software; you can redistribute it and/or
@@ -40,11 +38,11 @@ void jsr120_throw_signal(int handle, int signal);
 /**
  * Initializes signal system.
  */
-jsr120_init_signal();
+void jsr120_init_signal();
 
 /**
  * Finalizes signal system.
  */
 void jsr120_finalize_signal();
 
-#endif _JSR120_SIGNALS_H_
+#endif /* _JSR120_SIGNALS_H_ */
