516c982b0f9bbe698e0eba58b3a5d71a0e97fe22
MichaelG256 <MichaelG256@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-09-23 15:27:14 +0000

    cr 6750405 fixed reviewed by Alexei Zavitaev svn merge -r 13987:14016 https://phoneme.dev.java.net/svn/phoneme/components/midp/branches/midp-cr-6750405-appman-odd-glitch Mostly changes in the logic of RunningMIDletSuiteInfo.sameSuite(); factored out MIDletSuiteInfo.isInternal()

--- src/ams/midlet_suite_info/reference/classes/com/sun/midp/midletsuite/MIDletSuiteInfo.java
+++ src/ams/midlet_suite_info/reference/classes/com/sun/midp/midletsuite/MIDletSuiteInfo.java
@@ -29,6 +29,7 @@ package com.sun.midp.midletsuite;
 import com.sun.midp.main.*;
 
 import com.sun.midp.configurator.Constants;
+import com.sun.midp.midlet.MIDletSuite;
 
 /** Simple attribute storage for MIDlet suites */
 public class MIDletSuiteInfo {
@@ -156,5 +157,16 @@ public class MIDletSuiteInfo {
         b.append(", folderId = " + folderId);
         b.append(", midletToRun = " + midletToRun);
         return b.toString();
+    }
+
+    /**
+     * Check if this MIDletSuiteInfo object describes a MIDlet from the
+     * internal suite (rather than from an installed suite).
+     * The internal MIDlets are a special case: there is one MIDletSuiteInfo
+     * per MIDlet, and they share the same suite id.
+     * @return true if the suite id is INTERNAL_SUITE_ID
+     */
+    final public boolean isInternal() {
+        return suiteId == MIDletSuite.INTERNAL_SUITE_ID;
     }
 }
