e9613ab50b6ab14a133fdfa17010d5be09ad77ca
asg024 <asg024@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-10-08 22:46:35 +0000

    CR 6709508 linux_fb implementation part: Upmerge -r12228:14300 https://phoneme.dev.java.net/svn/phoneme/components/midp/branches/midp-cr-6709508/

--- src/highlevelui/fb_port/include/fbport_export.h
+++ src/highlevelui/fb_port/include/fbport_export.h
@@ -38,11 +38,19 @@
 extern "C" {
 #endif
 
+#include <gxj_screen_buffer.h>
+
 /** Initialize frame buffer video device */
-extern void connectFrameBuffer();
+extern void connectFrameBuffer(int hardwareId);
 
 /** Allocate system screen buffer according to the screen geometry */
 extern void initScreenBuffer(int width, int height);
+
+extern void initScreenList();
+
+extern  void initSystemScreen(int id, int isFullScreen, int reverse_orientation, int width, int height);
+
+extern void clearScreens();
 
 /** Free allocated resources and restore system state */
 extern void finalizeFrameBuffer();
@@ -51,7 +59,13 @@ extern void finalizeFrameBuffer();
  * Change screen orientation to landscape or portrait,
  * depending on the current screen mode
  */
-extern void reverseScreenOrientation();
+extern jboolean reverseScreenOrientation(int id);
+
+extern jboolean setFullScreenMode(int id, int mode, int width, int height);
+
+extern int getReverseOrientation(int id);
+
+extern jboolean isFullScreenMode(int id);
 
 /**
  * Resizes system screen buffer to fit the screen dimensions.
@@ -59,11 +73,7 @@ extern void reverseScreenOrientation();
  */
 extern void resizeScreenBuffer(int width, int height);
 
-/** Refresh screen with offscreen bufer content */
-extern void refreshScreenNormal(int x1, int y1, int x2, int y2);
-
-/** Refresh rotated screen with offscreen bufer content */
-extern void refreshScreenRotated(int x1, int y1, int x2, int y2);
+extern void refreshScreen(int id, int x1, int y1, int x2, int y2);
 
 /** Return file descriptor of keyboard device, or -1 in none */
 extern int getKeyboardFd();
@@ -72,14 +82,20 @@ extern int getKeyboardFd();
 extern int getMouseFd();
 
 /** Clear screen device */
-extern void clearScreen();
+extern void clearScreen(int hardwareId);
 
 /** Get x-coordinate of screen origin */
-extern int getScreenX(int screenRotated);
+extern int getScreenX(int hardwareId, int width);
 
 /** Get y-coordinate of screen origin */
-extern int getScreenY(int screenRotated);
+extern int getScreenY(int hardwareId, int height);
 
+  /** Get the list of display ids */
+extern jint* getDisplayIds(jint* n );
+
+extern void displayStateChanged(int hardwareId, int state);
+extern int getNextDisplayId();
+extern int getCurrentDisplayId();
 
 #ifdef __cplusplus
 }
