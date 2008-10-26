5b7ad14495865d65885124fc073ef1a78c8b7202
asg024 <asg024@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-10-26 18:56:48 +0000

    Correct display removal for cdc suit loader

--- src/ams/cdc_application/classes/com/sun/midp/main/CdcMIDletSuiteLoader.java
+++ src/ams/cdc_application/classes/com/sun/midp/main/CdcMIDletSuiteLoader.java
@@ -385,7 +385,7 @@ public class CdcMIDletSuiteLoader extends AbstractMIDletSuiteLoader
     public void midletDestroyed(MIDletSuite suite, String className,
                                 MIDlet midlet) {
         if (midlet != null) {
-            displayContainer.removeDisplay(midlet);
+            displayContainer.removeDisplaysByOwner(midlet);
         }
     }
 
