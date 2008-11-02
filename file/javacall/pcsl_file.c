d1fd6fa63ada2ec6795dc5616c3b37274cff1e74
dglushko <dglushko@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-11-02 10:04:32 +0000

    revert mistakingly done commit.

--- file/javacall/pcsl_file.c
+++ file/javacall/pcsl_file.c
@@ -29,7 +29,6 @@
 #include <javacall_memory.h>
 #include <pcsl_memory.h>
 #include <pcsl_file.h>
-#include <pcsl_network.h>
 #include <pcsl_print.h>
 #include <javacall_logging.h>
 #include <stdio.h>
@@ -44,9 +43,7 @@
  */
 int
 pcsl_file_init() {
-		
     int ret = javacall_file_init();
-    pcsl_network_init();
 
 #ifdef PCSL_DEBUG
     if (ret != JAVACALL_OK)
