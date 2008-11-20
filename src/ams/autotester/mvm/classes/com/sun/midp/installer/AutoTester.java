78ea9cfb0e5bd788be63f9ef0112bb8eb504ea46
leonidr <leonidr@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-11-20 23:23:16 +0000

    Work in progress

--- src/ams/autotester/mvm/classes/com/sun/midp/installer/AutoTester.java
+++ src/ams/autotester/mvm/classes/com/sun/midp/installer/AutoTester.java
@@ -65,28 +65,19 @@ import com.sun.midp.events.*;
  * If arg-0 is not given then a form will be used to query the tester for
  * the arguments.</p>
  */
-public class AutoTester extends AutoTesterBase
-        implements AutoTesterInterface, EventListener {
-    /** True if all events in our queue were processed. */
-    private boolean eventsInQueueProcessed;
-
-    /** Our event queue. */
-    EventQueue eventQueue;
+public final class AutoTester extends AutoTesterBase {
 
     /**
      * Create and initialize a new auto tester MIDlet.
      */
     public AutoTester() {
-        super();
-
-        eventQueue = EventQueue.getEventQueue();
-        eventQueue.registerEventListener(EventTypes.AUTOTESTER_EVENT, this);
+        super(new AutoTesterHelper());
 
         if (url != null) {
-            startBackgroundTester();
-        } else if (restoreSession()) {
+            startBackgroundTester(true);
+        } else if (helper.restoreSession()) {
             // continuation of a previous session
-            startBackgroundTester();
+            startBackgroundTester(false);
         } else {
             /**
              * No URL has been provided, ask the user.
@@ -99,172 +90,14 @@ public class AutoTester extends AutoTesterBase
 
     /** Run the installer. */
     public void run() {
-        installAndPerformTests(midletSuiteStorage, installer, url);
-    }
-
-    /**
-     * Restore the data from the last session, since this version of the
-     * autotester does not have sessions it just returns false.
-     *
-     * @return true if there was data saved from the last session
-     */
-    public boolean restoreSession() {
-        return false;
-    }
-
-    /**
-     * Installs and performs the tests.
-     *
-     * @param midletSuiteStorage MIDletSuiteStorage object
-     * @param inp_installer Installer object
-     * @param inp_url URL of the test suite
-     */
-    public void installAndPerformTests(
-        MIDletSuiteStorage midletSuiteStorage,
-        Installer inp_installer, String inp_url) {
-
-        MIDletInfo midletInfo;
-        int suiteId = MIDletSuite.UNUSED_SUITE_ID;
-
         try {
-            Isolate testIsolate;
-
-            for (; loopCount != 0; ) {
-                // force an overwrite and remove the RMS data
-                suiteId = inp_installer.installJad(inp_url,
-                    Constants.INTERNAL_STORAGE_ID, true, true, null);
-
-                midletInfo = getFirstMIDletOfSuite(suiteId,
-                                                   midletSuiteStorage);
-
-                Isolate[] isolatesBefore = Isolate.getIsolates();
-
-                testIsolate =
-                    AmsUtil.startMidletInNewIsolate(suiteId,
-                        midletInfo.classname, midletInfo.name, null,
-                        null, null);
-
-                testIsolate.waitForExit();
-
-                boolean newIsolatesFound;
-
-                do {
-                    newIsolatesFound = false;
-
-                    /*
-                     * Send an event to ourselves.
-                     * Main idea of it is to process all events that are in the
-                     * queue at the moment when the test isolate has exited
-                     * (because when testing CHAPI there may be requests to
-                     * start new isolates). When this event arrives, all events
-                     * that were placed in the queue before it are guaranteed
-                     * to be processed.
-                     */
-                    synchronized (this) {
-                        eventsInQueueProcessed = false;
-
-                        NativeEvent event = new NativeEvent(
-                                EventTypes.AUTOTESTER_EVENT);
-                        eventQueue.sendNativeEventToIsolate(event,
-                                MIDletSuiteUtils.getIsolateId());
-
-                        // and wait until it arrives
-                        do {
-                            try {
-                                wait();
-                            } catch(InterruptedException ie) {
-                                // ignore
-                            }
-                        } while (!eventsInQueueProcessed);
-                    }
-
-                    Isolate[] isolatesAfter = Isolate.getIsolates();
-
-                    /*
-                     * Wait for termination of all isolates contained in
-                     * isolatesAfter[], but not in isolatesBefore[].
-                     * This is needed to pass some tests (for example, CHAPI)
-                     * that starting several isolates.
-                     */
-                    int i, j;
-                    for (i = 0; i < isolatesAfter.length; i++) {
-                        for (j = 0; j < isolatesBefore.length; j++) {
-                            try {
-                                if (isolatesBefore[j].equals(
-                                        isolatesAfter[i])) {
-                                    break;
-                                }
-                            } catch (Exception e) {
-                                // isolatesAfter[i] might already exit,
-                                // no need to wait for it
-                                break;
-                            }
-                        }
-
-                        if (j == isolatesBefore.length) {
-                            try {
-                                newIsolatesFound = true;
-                                isolatesAfter[i].waitForExit();
-                            } catch (Exception e) {
-                                // ignore: the isolate might already exit
-                            }
-                        }
-                    }
-                } while (newIsolatesFound);
-
-                if (loopCount > 0) {
-                    loopCount -= 1;
-                }
-            }
+            helper.installAndPerformTests();
         } catch (Throwable t) {
-            handleInstallerException(suiteId, t);
+            String errorMessage = helper.getInstallerExceptionMessage(t); 
+            handleInstallerException(errorMessage);
         }
 
-        if (midletSuiteStorage != null &&
-                suiteId != MIDletSuite.UNUSED_SUITE_ID) {
-            try {
-                midletSuiteStorage.remove(suiteId);
-            } catch (Throwable ex) {
-                // ignore
-            }
-        }
-
-        notifyDestroyed();
+        notifyDestroyed();        
     }
 
-    /**
-     * Preprocess an event that is being posted to the event queue.
-     * This method will get called in the thread that posted the event.
-     *
-     * @param event event being posted
-     *
-     * @param waitingEvent previous event of this type waiting in the
-     *     queue to be processed
-     *
-     * @return true to allow the post to continue, false to not post the
-     *     event to the queue
-     */
-    public boolean preprocess(Event event, Event waitingEvent) {
-        return true;
-    }
-
-    /**
-     * Process an event.
-     * This method will get called in the event queue processing thread.
-     *
-     * @param event event to process
-     */
-    public void process(Event event) {
-        NativeEvent nativeEvent = (NativeEvent)event;
-
-        switch (nativeEvent.getType()) {
-            case EventTypes.AUTOTESTER_EVENT: {
-                synchronized (this) {
-                    eventsInQueueProcessed = true;
-                    notify();
-                }
-                break;
-            }
-        }
-    }
 }
---
+++ src/ams/autotester_base/reference/classes/com/sun/midp/installer/AutoTesterHelperBase.java
@@ -0,0 +1,354 @@
+/*
+ *
+ *
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ * 
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
+ */
+
+package com.sun.midp.installer;
+
+import java.io.IOException;
+import com.sun.midp.midletsuite.MIDletSuiteLockedException;
+
+import com.sun.midp.i18n.Resource;
+import com.sun.midp.i18n.ResourceConstants;
+
+import com.sun.midp.midlet.MIDletSuite;
+
+import com.sun.midp.midletsuite.MIDletInfo;
+import com.sun.midp.midletsuite.MIDletSuiteStorage;
+
+import com.sun.midp.security.Permissions;
+
+abstract class AutoTesterHelperBase {
+    /** MIDlet suite storage object. */
+    protected MIDletSuiteStorage midletSuiteStorage;
+    /** The installer. */
+    protected Installer installer;
+
+    /** URL of the test suite. */
+    protected String url;
+    /** Security domain to assign to unsigned suites. */
+    protected String domain;
+    /** How many iterations to run the suite */
+    protected int loopCount = -1;
+
+    /** ID of the suite being installed */
+    protected int suiteId = MIDletSuite.UNUSED_SUITE_ID;
+   
+    AutoTesterHelperBase() {
+        midletSuiteStorage = MIDletSuiteStorage.getMIDletSuiteStorage();
+        installer = new HttpInstaller();
+
+        url = null;
+        domain = null;
+    }
+
+    void setTestRunParams(String inp_url, String inp_domain, 
+            int inp_count) {
+
+        url = inp_url;
+        domain = inp_domain;
+        loopCount = inp_count;       
+
+        if (domain != null) {
+            String additionalPermissions = null;
+            int index = domain.indexOf(":");
+            int len = domain.length();
+
+            if (index > 0 && index + 1 < len) {
+                additionalPermissions = domain.substring(index + 1, len);
+                domain = domain.substring(0, index);
+            }
+
+            installer.setUnsignedSecurityDomain(domain);
+            installer.setExtraPermissions(additionalPermissions);
+        }
+    }
+
+    abstract void installAndPerformTests() 
+        throws IOException, MIDletSuiteLockedException;
+
+    abstract boolean restoreSession();
+
+    /**
+     * Returns the class name of the first MIDlet of the newly installed
+     * suite.
+     *
+     * @param suiteId ID of the MIDlet Suite
+     * @param midletSuiteStorage MIDlet suite storage to look up properties
+     *
+     * @return an object with the class name and display name of
+     * the suite's MIDlet-1 property
+     */
+    protected MIDletInfo getFirstMIDletOfSuite() {
+        MIDletSuite ms = null;
+        String name = null;
+
+        try {
+            ms = midletSuiteStorage.getMIDletSuite(suiteId, false);
+            name = ms.getProperty("MIDlet-1");
+        } catch (Exception e) {
+            throw new RuntimeException("midlet properties corrupted");
+        } finally {
+            if (ms != null) {
+                ms.close();
+            }
+        }
+
+        if (name == null) {
+            throw new RuntimeException("MIDlet-1 missing");
+        }
+
+        // found the entry now parse out the class name, and display name
+        return new MIDletInfo(name);
+    }
+
+    protected String getInstallerExceptionMessage(Throwable ex) {
+        String message = null;
+
+        if (ex instanceof InvalidJadException) {
+            InvalidJadException ije = (InvalidJadException)ex;
+
+            /*
+             * The server will signal the end of testing with not found
+             * status. However print out the JAD not found error if this
+             * is the first download. (suiteId == null)
+             */
+            int reason = ije.getReason();
+            if ((reason != InvalidJadException.JAD_NOT_FOUND &&
+                reason != InvalidJadException.JAD_SERVER_NOT_FOUND) ||
+                    suiteId == MIDletSuite.UNUSED_SUITE_ID) {
+                message = "** Error installing suite (" + reason + "): " + 
+                    messageForInvalidJadException(ije);
+            }
+        } else if (ex instanceof IOException) {
+            message = "** I/O Error installing suite: " + ex.getMessage();
+        } else {
+            message = "** Error installing suite: " + ex.toString();
+        }
+
+        return message;
+    }
+    
+    /**
+     * Returns the associated message for the given exception.
+     * This function is here instead of in the exception its self because
+     * it not need on devices, it needed only on development platforms that
+     * have command line interface.
+     *
+     * @param ije reason reason code for the exception
+     *
+     * @return associated message for the given reason
+     */
+    private static String messageForInvalidJadException(
+            InvalidJadException ije) {
+
+        switch (ije.getReason()) {
+        case InvalidJadException.MISSING_PROVIDER_CERT:
+        case InvalidJadException.MISSING_SUITE_NAME:
+        case InvalidJadException.MISSING_VENDOR:
+        case InvalidJadException.MISSING_VERSION:
+        case InvalidJadException.MISSING_JAR_URL:
+        case InvalidJadException.MISSING_JAR_SIZE:
+        case InvalidJadException.MISSING_CONFIGURATION:
+        case InvalidJadException.MISSING_PROFILE:
+            return "A required attribute is missing";
+
+        case InvalidJadException.SUITE_NAME_MISMATCH:
+        case InvalidJadException.VERSION_MISMATCH:
+        case InvalidJadException.VENDOR_MISMATCH:
+            return "A required suite ID attribute in the JAR manifest " +
+                "do not match the one in the JAD";
+
+        case InvalidJadException.ATTRIBUTE_MISMATCH:
+            return "The value for " + ije.getExtraData() + " in the " +
+                "trusted JAR manifest did not match the one in the JAD";
+
+        case InvalidJadException.CORRUPT_PROVIDER_CERT:
+            return "The content provider certificate cannot be decoded.";
+
+        case InvalidJadException.UNKNOWN_CA:
+            return "The content provider certificate issuer " +
+                ije.getExtraData() + " is unknown.";
+
+        case InvalidJadException.INVALID_PROVIDER_CERT:
+            return "The signature of the content provider certificate " +
+                "is invalid.";
+
+        case InvalidJadException.CORRUPT_SIGNATURE:
+            return "The JAR signature cannot be decoded.";
+
+        case InvalidJadException.INVALID_SIGNATURE:
+            return "The signature of the JAR is invalid.";
+
+        case InvalidJadException.UNSUPPORTED_CERT:
+            return "The content provider certificate is not a supported " +
+                "version.";
+
+        case InvalidJadException.EXPIRED_PROVIDER_CERT:
+            return "The content provider certificate is expired.";
+
+        case InvalidJadException.EXPIRED_CA_KEY:
+            return "The public key of " + ije.getExtraData() +
+                " has expired.";
+
+        case InvalidJadException.JAR_SIZE_MISMATCH:
+            return "The Jar downloaded was not the size in the JAD";
+
+        case InvalidJadException.OLD_VERSION:
+            return "The application is an older version of one that is " +
+                "already installed";
+
+        case InvalidJadException.NEW_VERSION:
+            return "The application is an newer version of one that is " +
+                "already installed";
+
+        case InvalidJadException.INVALID_JAD_URL:
+            return "The JAD URL is invalid";
+
+        case InvalidJadException.JAD_SERVER_NOT_FOUND:
+            return "JAD server not found";
+
+        case InvalidJadException.JAD_NOT_FOUND:
+            return "JAD not found";
+
+        case InvalidJadException.INVALID_JAR_URL:
+            return "The JAR URL in the JAD is invalid: " +
+                ije.getExtraData();
+
+        case InvalidJadException.JAR_SERVER_NOT_FOUND:
+            return "JAR server not found: " + ije.getExtraData();
+
+        case InvalidJadException.JAR_NOT_FOUND:
+            return "JAR not found: " + ije.getExtraData();
+
+        case InvalidJadException.CORRUPT_JAR:
+            return "Corrupt JAR, error while reading: " +
+                ije.getExtraData();
+
+        case InvalidJadException.INVALID_JAR_TYPE:
+            if (ije.getExtraData() != null) {
+                return "JAR did not have the correct media type, it had " +
+                    ije.getExtraData();
+            }
+
+            return "The server did not have a resource with an " +
+                "acceptable media type for the JAR URL. (code 406)";
+
+        case InvalidJadException.INVALID_JAD_TYPE:
+            if (ije.getExtraData() != null) {
+                String temp = ije.getExtraData();
+
+                if (temp.length() == 0) {
+                    return "JAD did not have a media type";
+                }
+
+                return "JAD did not have the correct media type, it had " +
+                    temp;
+            }
+
+            /*
+             * Should not happen, the accept field is not send
+             * when getting the JAD.
+             */
+            return "The server did not have a resource with an " +
+                "acceptable media type for the JAD URL. (code 406)";
+
+        case InvalidJadException.INVALID_KEY:
+            return "The attribute key [" + ije.getExtraData() +
+                "] is not in the proper format";
+
+        case InvalidJadException.INVALID_VALUE:
+            return "The value for attribute " + ije.getExtraData() +
+                " is not in the proper format";
+
+        case InvalidJadException.INSUFFICIENT_STORAGE:
+            return "There is insufficient storage to install this suite";
+
+        case InvalidJadException.UNAUTHORIZED:
+            return "Authentication required or failed";
+
+        case InvalidJadException.JAD_MOVED:
+            return "The JAD to be installed is for an existing suite, " +
+                "but not from the same domain as the existing one: " +
+                ije.getExtraData();
+
+        case InvalidJadException.CANNOT_AUTH:
+            return
+                "Cannot authenticate with the server, unsupported scheme";
+
+        case InvalidJadException.DEVICE_INCOMPATIBLE:
+            return "Either the configuration or profile is not supported.";
+
+        case InvalidJadException.ALREADY_INSTALLED:
+            return
+                "The JAD matches a version of a suite already installed.";
+
+        case InvalidJadException.AUTHORIZATION_FAILURE:
+            return "The suite is not authorized for " + ije.getExtraData();
+
+        case InvalidJadException.PUSH_DUP_FAILURE:
+            return "The suite is in conflict with another application " +
+                "listening for network data on " + ije.getExtraData();
+
+        case InvalidJadException.PUSH_FORMAT_FAILURE:
+            return "Push attribute in incorrectly formated: " +
+                ije.getExtraData();
+
+        case InvalidJadException.PUSH_PROTO_FAILURE:
+            return "Connection in push attribute is not supported: " +
+                ije.getExtraData();
+
+        case InvalidJadException.PUSH_CLASS_FAILURE:
+            return "The class in push attribute not in a MIDlet-<n> " +
+                "attribute: " + ije.getExtraData();
+
+        case InvalidJadException.TRUSTED_OVERWRITE_FAILURE:
+            return "Cannot update a trusted suite with an untrusted " +
+                "version";
+
+        case InvalidJadException.INVALID_CONTENT_HANDLER:
+            return "Content handler attribute(s) incorrectly formatted: " +
+                ije.getExtraData();
+
+        case InvalidJadException.CONTENT_HANDLER_CONFLICT:
+            return "Content handler would conflict with another handler: " +
+                ije.getExtraData();
+
+        case InvalidJadException.CA_DISABLED:
+            return "The application can't be authorized because " +
+                ije.getExtraData() + " is disabled.";
+
+        case InvalidJadException.UNSUPPORTED_CHAR_ENCODING:
+            return "Unsupported character encoding: " + ije.getExtraData();
+
+        case InvalidJadException.REVOKED_CERT:
+            return "The content provider certificate has been revoked.";
+
+        case InvalidJadException.UNKNOWN_CERT_STATUS:
+            return "The content provider certificate status is unknown.";
+        }
+
+        return ije.getMessage();
+    }
+}
