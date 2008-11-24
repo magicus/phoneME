8d3cf7bf807bd46fb36c3ab822acb78c36ca0ab3
oren <oren@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-11-24 14:23:26 +0000

    More documentation

--- src/ams/suitestore/internal_api/reference/native/platform_install_notifier.h
+++ src/ams/suitestore/internal_api/reference/native/platform_install_notifier.h
@@ -1,3 +1,17 @@
+/**
+ * Inform on installation of a new Midlet.
+ *
+ * Java invokes this function when an installation of a new
+ * Midlet completes successfully.
+ * Allows the platform to create shortcuts for the installed Midlets which enables one click
+ * launch of a Midlet
+ *
+ * @param midletName     Midlet name string (as appears in the *.jad file)
+ * @param className       First Midlet class name (as appears in the *.jad file)
+ * @param suiteID            Midlet Suite ID
+ * @param midletIcon       Icon file name to use (It should be valid icon path or NULL)
+ */
+
 void platform_lifecycle_install(
      jchar *midletName,
      jsize midletNameLen,
@@ -7,5 +21,12 @@ void platform_lifecycle_install(
      jchar* midletIcon,
      jsize midletIconLen
 );
+
+
+/**
+ * Inform on uninstallation of a Midlet.
+ *
+ * @param suiteID            Midlet Suite ID
+ */
 
 void platform_uninstall_notify(int suiteId);
