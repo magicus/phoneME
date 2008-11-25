940d7fdcd418725e8be5309999d18b557fefce86
assafyavnai <assafyavnai@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-11-25 16:01:52 +0000

    Changed stub implementation to hold changes in javacall_push.h

--- implementation/stubs/midp/push.c
+++ implementation/stubs/midp/push.c
@@ -29,27 +29,143 @@ extern "C" {
     
     
 #include "javacall_push.h" 
+#include "javacall_memory.h"
 
 
+/**
+ * Register push into platform's pushDB.
+ * @param suiteID    unique ID of the MIDlet suite
+ * @param connection connection string pass by
+ *                   javax.microedition.io.PushRegistry.registerConnection.
+ *                   for example, "sms://:12345"
+ * @param midlet  the midlet class name of MIDlet should be launched
+ * @param fillter pass by javax.microedition.io.PushRegistry.registerConnection.
+ *
+ * @return  <tt>JAVACALL_PUSH_OK</tt> on success,
+ *          <tt>JAVACALL_PUSH_ILLEGAL_ARGUMENT</tt> if
+ *          connection or filter string is invalid,
+ *          <tt>JAVACALL_PUSH_UNSUPPORTED</tt> if the runtime
+ *          system does not support push delivery for the
+ *          requested connection protocol,
+ *          <tt>JAVACALL_PUSH_MIDLET_NOT_FOUND</tt> if class
+ *          name was not found,
+ *          <tt>JAVACALL_PUSH_SUITE_NOT_FOUND</tt> if suite ID
+ *          was not found,
+ *          <tt>JAVACALL_PUSH_CONNECTION_IN_USE</tt> if the
+ *          connection is registered already
+ */
+javacall_push_result
+javacall_push_register(const javacall_suite_id suiteID,
+                       const javacall_push_entry* entry) {
+    return JAVACALL_PUSH_OK;
+}
     
 /**
- * Popup native dialog and block till user provides a reply: 
- * either permit or deny.
- * If user permits to run Java, then device focus needs to be passed to 
- * the Java task.
+ * Unregister push from platform's pushDB.
+ * @param suiteID    unique ID of the MIDlet suite
+ * @param connection connection string pass by
+ *                   javax.microedition.io.PushRegistry.registerConnection.
+ *                   for example, "sms://12345"
  *
- * @param midletName UNICODE name of midlet to launch
- * @param midletNameLen length of name of midlet to launch
- * @param midletSuite UNICODE midlet suite to launch
- * @param midletSuiteLen length of midlet suite to launch
+ * @return  <tt>JAVACALL_PUSH_OK</tt> on success,
+ *          <tt>JAVACALL_PUSH_ILLEGAL_ARGUMENT</tt> if
+ *          connection was not registered by any MIDlet suite,
+ *          <tt>JAVACALL_PUSH_SUITE_NOT_FOUND</tt> if suite ID
+ *          was not found,
+ *          <tt>JAVACALL_PUSH_PERMISSION_DENIED</tt> if the
+ *          connection was registered by another MIDlet suite
+ */
+javacall_push_result
+javacall_push_unregister(const javacall_suite_id suiteID,
+                         const javacall_utf16_string connection) {
+    return JAVACALL_PUSH_OK;
+}
+
+/**
+ * Register alarm into platform's pushDB.
+ * @param suiteID  unique ID of the MIDlet suite
+ * @param midlet   the midlet class name of MIDlet should be launched
+ * @param time     the number of milliseconds since January 1, 1970, 00:00:00 GMT
+ * @param prev_time If a wakeup time is already registered, the
+ *                  previous value will be returned, otherwise a
+ *                  zero is returned the first time the alarm is
+ *                  registered.
  *
- * @return <tt>JAVACALL_TRUE</tt> if permit, <tt>JAVACALL_FALSE</tt> if deny or on error
+ * @return  <tt>JAVACALL_PUSH_OK</tt> on success,
+ *          <tt>JAVACALL_PUSH_UNSUPPORTED</tt> if the runtime
+ *          system does not support push delivery for the
+ *          requested connection protocol,
+ *          <tt>JAVACALL_PUSH_MIDLET_NOT_FOUND</tt> if class
+ *          name was not found,
+ *          <tt>JAVACALL_PUSH_SUITE_NOT_FOUND</tt> if suite ID
+ *          was not found
+ */
+javacall_push_result
+javacall_push_alarm_register(const javacall_suite_id suiteID,
+                             const javacall_utf16_string midlet,
+                             javacall_int64 time,
+                             /*OUT*/javacall_int64* prev_time) {
+    *prev_time = 0;
+    return JAVACALL_PUSH_OK;
+}
+
+/**
+ * Return a list of registered connections for the current MIDlet suite.
+ *
+ * @param suiteID       unique ID of the MIDlet suite
+ * @param available     if true, only return the list of
+ *                      connections with input available,
+ *                      otherwise return the complete list of
+ *                      registered connections for the current
+ *                      MIDlet suite.
+ * @param entries       OUT pointer to be set to an array of
+ *                      registered connections for calling
+ *                      MIDlet suite.
+ * @param pNumOfConnections  OUT pointer to be set to the number
+ *                           of connections returned
+ *
+ * @return  <tt>JAVACALL_PUSH_OK</tt> on success,
+ *          <tt>JAVACALL_PUSH_SUITE_NOT_FOUND</tt> if suite ID
+ *          was not found
+ * @note  use <tt>javacall_push_release_entries</tt> to free
+ *        allocated resources
+ *  */
+javacall_push_result
+javacall_push_listentries(const javacall_suite_id suiteID,
+                          const javacall_bool available,
+                          javacall_push_entry const** entries,
+                          int* const  pNumOfConnections) {
+    static javacall_utf16_string con = L"sms:///:12345";
+    static javacall_utf16_string mid = L"com.sun.midlet";
+    static javacall_utf16_string fil = L"*";
+    javacall_push_entry* entry = javacall_malloc(sizeof(javacall_push_entry));
+    entry->connection = con;
+    entry->connectionLen = sizeof(con)/sizeof(javacall_utf16);
+    entry->connection = mid;
+    entry->connectionLen = sizeof(mid)/sizeof(javacall_utf16);
+    entry->connection = fil;
+    entry->connectionLen = sizeof(fil)/sizeof(javacall_utf16);
+    *entries = entry;
+    *pNumOfConnections = 1;
+    return JAVACALL_PUSH_OK;
+}
+
+/**
+ * Release resources allocated by <tt>javacall_push_listentries</tt>.
+ * 
+ * @param entries       OUT pointer to an array of registered
+ *                      connections for calling MIDlet suite.
+ * @param pNumOfConnections  OUT the number of connections
  *                
  */
-javacall_bool javacall_push_show_request_launch_java(
-        const javacall_utf16* midletName,  int midletNameLen,
-        const javacall_utf16* midletSuite, int midletSuiteLen)  {
-    return JAVACALL_FALSE;                                        
+void javacall_push_release_entries(const javacall_push_entry* entries,
+                                   int pNumOfConnections) {
+    while (pNumOfConnections--) {
+        javacall_free(entries[pNumOfConnections].connection);
+        javacall_free(entries[pNumOfConnections].midlet);
+        javacall_free(entries[pNumOfConnections].filter);
+    }
+    javacall_free(entries);
 }
     
 
