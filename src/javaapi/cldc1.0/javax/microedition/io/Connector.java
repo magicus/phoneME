29e2345b5f2791e977ab46928bfc0fee17629be2
mishab <mishab@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-11-25 14:54:46 +0000

    Resolving CR 6775673 - Network Monitor: ConnectionNotFoundException is thrown protocol isn't covered by netmon impl. Reviewed by Danila, thanks! Merged from branch cldc-cr-6775673

--- src/javaapi/cldc1.0/javax/microedition/io/Connector.java
+++ src/javaapi/cldc1.0/javax/microedition/io/Connector.java
@@ -133,6 +133,11 @@ public class Connector {
     private static String classRoot;
 
     /**
+     * The fallback root of the classes.
+     */
+    private static String classRootFallback;
+
+    /**
      * Class initializer.
      */
     static {
@@ -147,6 +152,8 @@ public class Connector {
         if (classRoot == null) {
             classRoot = "com.sun.cldc.io";
         }
+        classRootFallback = System.getProperty(
+            "javax.microedition.io.Connector.protocolpath.fallback");
     }
 
     /**
@@ -236,16 +243,16 @@ public class Connector {
         throws IOException, ClassNotFoundException {
 
         /* Test for correct mode value */
-		if (mode != READ && 
-		    mode != WRITE &&
-			mode != READ_WRITE) {
+        if (mode != READ && 
+            mode != WRITE &&
+            mode != READ_WRITE) {
             throw new IllegalArgumentException(
 /* #ifdef VERBOSE_EXCEPTIONS */
 /// skipped                       "Wrong mode value"
 /* #endif */
             );
-		}
-		
+        }
+        
         /* Test for null argument */
         if (name == null) {
             throw new IllegalArgumentException(
@@ -297,10 +304,22 @@ public class Connector {
 
             /* Use the platform and protocol names to look up */
             /* a class to implement the connection */
-            Class clazz =
-                Class.forName(classRoot +
+            Class clazz;
+            try {
+                clazz =
+                    Class.forName(classRoot +
                               "." + platform +
                               "." + protocol + ".Protocol");
+            } catch (ClassNotFoundException exc) {
+                if (classRootFallback != null) {
+                    clazz =
+                        Class.forName(classRootFallback +
+                              "." + platform +
+                              "." + protocol + ".Protocol");
+                } else {
+                    throw exc;
+                }
+            }
 
             /* Construct a new instance */
             ConnectionBaseInterface uc =
