0c02e9e10a39b90ce52aa2c8d57915ff1fff8936
themik <themik@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-01 14:28:53 +0000

    CR 6766488. Using uninitialized array elements. merged -r15874:15875 /branches/restricted-crypto-cr-6766488. Reviewed by Alexey Zavitaev.

--- src/restricted_crypto/reference/native/bnlib.c
+++ src/restricted_crypto/reference/native/bnlib.c
@@ -1185,6 +1185,8 @@ BN_CTX *ctx;
         INTEGER start=1;
         BIGNUM *d, *t, *aa;
         BIGNUM *val[16];
+        /* maximal initialized position at val[] */
+        INTEGER valbound = 0;
         BN_MONT_CTX *mont=NULL;
 
         if (!(m->d[0] & 1))
@@ -1254,6 +1256,7 @@ BN_CTX *ctx;
         j=1<<(window-1);
         for (i=1; i<j; i++)
                 {
+                valbound = i;
                 val[i]=BN_new(ctx->bn[0]->byteSize);
                 if (val[i] == NULL) goto err;
                 if (!BN_mod_mul_montgomery(val[i],val[i-1],d,mont,ctx))
@@ -1324,7 +1327,7 @@ BN_CTX *ctx;
 err:
         if (mont != NULL) BN_MONT_CTX_free(mont);
         ctx->tos-=2;
-        for (i=0; i<16; i++)
+        for (i=0; i<=valbound; i++)
                 if (val[i] != NULL) BN_clear_free(val[i]);
         return(ret);
 }
