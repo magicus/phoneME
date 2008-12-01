b772d680978869d0b4d2f5aa81eaac2604efee25
aap031 <aap031@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-01 14:16:14 +0000

    CR: 6777908 Dead lock bug in Java event queue. Reviewed by SergeyT and Liang Xu. Merged as -r15858:15859 components/midp/branches/midp-cr-6777908

--- src/protocol/http/reference/classes/com/sun/midp/io/j2me/http/StreamConnectionPool.java
+++ src/protocol/http/reference/classes/com/sun/midp/io/j2me/http/StreamConnectionPool.java
@@ -123,6 +123,9 @@ public class StreamConnectionPool {
 
         StreamConnectionElement oldestNotInUse = null;
 
+        System.out.println("### Connection.add: " +p_protocol +
+                           ", " + p_host + ", " + p_port);
+
         // find the last unused element
         Enumeration cons = m_connections.elements();
         while (cons.hasMoreElements()) {
@@ -131,6 +134,7 @@ public class StreamConnectionPool {
 
             if (sce.m_in_use) {
                 if (p_host.equals(sce.m_host) && p_port == sce.m_port) {
+                    System.out.println("### Connection.add: false");
                     return false;
                 }
 
@@ -166,9 +170,11 @@ public class StreamConnectionPool {
             oldestNotInUse.close();
             m_connections.removeElement(oldestNotInUse);
         }
-     
-        m_connections.addElement(new StreamConnectionElement(p_protocol,
-                          p_host, p_port, sc, dos, dis));
+
+        StreamConnectionElement sce = new StreamConnectionElement(p_protocol,
+                                                                  p_host, p_port, sc, dos, dis);
+        m_connections.addElement(sce);
+        System.out.println("### Connection.add: true, " + sce);
         return true;
     }
     
@@ -179,6 +185,7 @@ public class StreamConnectionPool {
      * @param sce                 The stream connection element to remove
      */
     synchronized void remove(StreamConnectionElement sce) {
+        System.out.println("### Connection.remove " + sce);
 	sce.close();
         m_connections.removeElement(sce);
     }
@@ -212,6 +219,7 @@ public class StreamConnectionPool {
                 (StreamConnectionElement)cons.nextElement();
             
             if ((c_time - sce.m_time) > m_connectionLingerTime) {
+                System.out.println("### Connection.get: " + sce + " is no more. inuse " + sce.m_in_use);
                 if (!sce.m_in_use) {
                     sce.close();
                 } else {
@@ -236,6 +244,9 @@ public class StreamConnectionPool {
             result.m_in_use = true;
         }
 
+        System.out.println("### Connection.get: " + p_protocol + ", " + p_host +
+                           ", " + p_port + ": " + result);
+
         return result;
     }
 
@@ -252,9 +263,13 @@ public class StreamConnectionPool {
         if (returned.m_removed) {
             // the connection was out too long
             returned.close();
+            System.out.println("### Connection.return: closed");
+        
             return;
         }
 
         returned.m_time = System.currentTimeMillis();
+        System.out.println("### Connection.returned: for reuse");
+        
     }
 }
