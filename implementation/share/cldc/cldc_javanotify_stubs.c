7285f5c39d9d3986c331cd6d8891f39e5ed23023
aka1278 <aka1278@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-12 16:58:01 +0000

    Conditional stubbing out by ENABLE_NATIVE_AMS_UI.

--- implementation/share/cldc/cldc_javanotify_stubs.c
+++ implementation/share/cldc/cldc_javanotify_stubs.c
@@ -36,7 +36,9 @@
 #include <javacall_lcd.h>
 #include <javacall_penevent.h>
 
+#if ENABLE_NATIVE_AMS_UI
 #include <javacall_ams_installer.h>
+#endif
 
 #if ENABLE_JSR_120
 #include <javacall_sms.h>
@@ -101,17 +103,20 @@ void javanotify_start(void) {}
 
 void javanotify_start_java_with_arbitrary_args(int argc, char* argv[]) {}
 
+
+#if ENABLE_NATIVE_AMS_UI
+
 void javanotify_start_i3test(char* arg1, char* arg2) {}
 
 void javanotify_start_handler(char* handlerID, char* url, char* action) {}
+
+void javanotify_start_tck(char* url, javacall_lifecycle_tck_domain domain) {}
 
 void javanotify_install_content(const char * httpUrl,
                                 const javacall_utf16* descFilePath,
                                 unsigned int descFilePathLen,
                                 javacall_bool isJadFile,
                                 javacall_bool isSilent) {}
-
-void javanotify_start_tck(char* url, javacall_lifecycle_tck_domain domain) {}
 
 javacall_result
 javanotify_ams_install_suite(javacall_app_id appId,
@@ -121,6 +126,9 @@ javanotify_ams_install_suite(javacall_app_id appId,
                              javacall_folder_id folderId) {
     return JAVACALL_NOT_IMPLEMENTED;
 }
+
+#endif /* ENABLE_NATIVE_AMS_UI */
+
 
 void JavaTask(void) {}
 
