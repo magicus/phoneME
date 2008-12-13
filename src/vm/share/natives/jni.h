6b85ce15b018e4078b3eb8691b57a0f3fb25b469
yevgenydiomidov <yevgenydiomidov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-13 18:34:48 +0000

    CR 6781500 : missing copyrights block release of the product merged 16278:16287 cldc-cr-6781500 updated ending year

--- src/vm/share/natives/jni.h
+++ src/vm/share/natives/jni.h
@@ -1,5 +1,6 @@
 /*
- * Portions Copyright  2000-2008 Sun Microsystems, Inc. All Rights Reserved.
+ * Portions Copyright  2000-2008 Sun Microsystems, Inc. All Rights
+ * Reserved.  Use is subject to license terms.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
