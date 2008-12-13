3081e4a8649ebf79125c24a9b05f01e3a4f767ca
yevgenydiomidov <yevgenydiomidov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-13 18:16:25 +0000

    CR 6781500 : missing copyrights block release of the product merged 16278:16287 cldc-cr-6781500 updated ending year

--- src/tools/jcc/runtime/CLDC_HI_NativesWriter.java
+++ src/tools/jcc/runtime/CLDC_HI_NativesWriter.java
@@ -1,7 +1,7 @@
 /*
  *   
  *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
@@ -477,7 +477,7 @@ public class CLDC_HI_NativesWriter
         out.println();
         out.println("/*");
         out.println(" *");
-        out.println(" * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.");
+        out.println(" * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved.");
         out.println(" * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER");
         out.println(" * ");
         out.println(" * This program is free software; you can redistribute it and/or");
