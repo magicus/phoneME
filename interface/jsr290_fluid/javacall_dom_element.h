06366568c77dc32436488e315883aecbd61c7b23
n_medvedenko <n_medvedenko@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-12-24 12:43:30 +0000

    CR 6786196, JSR290: Implement getFeature methods. Merged -r16369:16371 from javacall-cr-6786196. Reviewed by jsr290 team

--- interface/jsr290_fluid/javacall_dom_element.h
+++ interface/jsr290_fluid/javacall_dom_element.h
@@ -1541,6 +1541,7 @@ javacall_dom_element_set_id_attribute_node_finish(void *context,
  * <code>false</code> otherwise. 
  * 
  * @return JAVACALL_OK if all done successfuly,
+ *         JAVACALL_FAIL if error in native code occured
  *         JAVACALL_OUT_OF_MEMORY if function fails to allocate memory for the 
  *             context,
  *         JAVACALL_WOULD_BLOCK caller must call the 
@@ -1564,6 +1565,7 @@ javacall_dom_element_is_html_element_start(javacall_handle handle,
  * <code>false</code> otherwise. 
  * 
  * @return JAVACALL_OK if all done successfuly,
+ *         JAVACALL_FAIL if error in native code occured
  *         JAVACALL_WOULD_BLOCK caller must call the 
  *             javacall_dom_element_is_html_element_finish function to complete the 
  *             operation,
