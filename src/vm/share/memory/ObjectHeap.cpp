d74f0b2a7f827d38367407bd37f856915ec56035
olegpliss <olegpliss@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-01-06 00:34:15 +0000

    Fixed bug in ObjectHeap::notify_objects_disposed

--- src/vm/share/memory/ObjectHeap.cpp
+++ src/vm/share/memory/ObjectHeap.cpp
@@ -143,7 +143,7 @@ inline void ObjectHeap::notify_objects_disposed ( const OopDesc* const* from,
                                                   const bool all ) {
   do {
     const int size = ((const OopDesc*)from)->object_size();
-    if( all || test_bit_for( (OopDesc**) from ) ) {
+    if( all || !test_bit_for( (OopDesc**) from ) ) {
       if(Arguments::_monitor_memory) {
         MemoryMonitor::notify_object( (const OopDesc*)from, size, false );
       }
