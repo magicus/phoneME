d0b80dae27c093750893428e4b9b443a51394405
dgomon <dgomon@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-01-07 08:32:10 +0000

    set text message fix

--- src/highlevelui/lfpport/javacall/native/lfpport_textfield.c
+++ src/highlevelui/lfpport/javacall/native/lfpport_textfield.c
@@ -180,14 +180,13 @@ MidpError lfpport_textfield_create(MidpItem* itemPtr,
     pcsl_string_convert_to_utf8(label, label_buf, MAX_TEXT_LENGTH, &label_len);
     pcsl_string_convert_to_utf8(text, text_buf,  MAX_TEXT_LENGTH, &text_len);
 
-
     frame = gtk_frame_new(NULL);
     if (label_len > 0) {
         gtk_frame_set_label(frame, label_buf);
     }
     text_field_text = gtk_entry_new();
     if (text_len > 0) {
-        gtk_label_set_label(text_field_text, text_buf);
+        gtk_entry_set_text(text_field_text, text_buf);
     }
     gtk_entry_set_editable(text_field_text, TRUE);
 
