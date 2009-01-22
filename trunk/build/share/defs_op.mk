4a6ed8bb8bff79437827c6aeb9a9432a3f048af0
borisu <borisu@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-01-22 16:23:32 +0000

    CR 6775778: jsr239 on cdc, minor fix, build files path changed merge -r15562:15564 from cdc-cr-6775778, reviewed by Roman Z.

--- trunk/build/share/defs_op.mk
+++ trunk/build/share/defs_op.mk
@@ -287,7 +287,11 @@ endif
 # Include JSR 239
 ifeq ($(USE_JSR_239), true)
 export JSR_239_DIR ?= $(COMPONENTS_DIR)/jsr239
-JSR_239_MAKE_FILE = $(JSR_239_DIR)/build/$(SUBSYSTEM_MAKE_FILE)
+ifeq ($(PROJECT_JSR_239_DIR),)
+JSR_239_MAKE_FILE = $(JSR_239_DIR)/build/cdc_share/$(SUBSYSTEM_MAKE_FILE)
+else
+JSR_239_MAKE_FILE = $(PROJECT_JSR_239_DIR)/build/cdc_share/$(SUBSYSTEM_MAKE_FILE)
+endif
 ifeq ($(wildcard $(JSR_239_MAKE_FILE)),)
 $(error JSR_239_DIR must point to a directory containing JSR 239 sources)
 endif
