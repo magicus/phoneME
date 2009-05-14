fe34977b1f5aef7fee2a84c9dcfd4433f868ff0b
A_Krotov <A_Krotov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-05-14 12:47:02 +0000

    new player added

---
+++ src/share/components/direct-player/classes/com/sun/mmedia/MPEG2Player.java
@@ -0,0 +1,126 @@
+/*
+ *  Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License version
+ *  2 only, as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License version 2 for more details (a copy is
+ *  included at /legal/license.txt).
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  version 2 along with this work; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA
+ *
+ *  Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ *  Clara, CA 95054 or visit www.sun.com if you need additional
+ *  information or have any questions.
+ */
+
+package com.sun.mmedia;
+
+import javax.microedition.media.Control;
+import javax.microedition.media.MediaException;
+import javax.microedition.media.Player;
+
+class MPEG2Player extends LowLevelPlayer implements VideoSource {
+
+    private int nativeHandle = 0;
+
+    MPEG2Player( HighLevelPlayer owner )
+    {
+        super( owner );
+    }
+
+    protected void doNotifySnapshotFinished() {
+    }
+
+    protected void doReceiveRSL() {
+    }
+
+    protected void doRealize() throws MediaException {
+    }
+
+    protected void doPrefetch() throws MediaException {
+    }
+
+    private native boolean nStart( int handle );
+    private native int nCreate( String locator );
+    
+    protected boolean doStart() {
+        nativeHandle = nCreate( getOwner().source.getLocator() );
+        return nStart( nativeHandle );
+    }
+
+    protected void doPostStart() {
+    }
+
+    protected void doStop() throws MediaException {
+    }
+
+    protected void doPreStop() {
+    }
+
+    protected void doDeallocate() {
+    }
+
+    protected void doClose() {
+    }
+
+    protected long doSetMediaTime(long now) throws MediaException {
+        throw new MediaException( "Not supported" );
+    }
+
+    protected long doGetMediaTime() {
+        return Player.TIME_UNKNOWN;
+    }
+
+    protected long doGetDuration() {
+        return Player.TIME_UNKNOWN;
+    }
+
+    protected Control doGetNewControl(String type) {
+        return null;
+    }
+
+    protected void doSetStopTime(long time) {
+    }
+
+    protected String doGetContentType() {
+        return "video/mpeg";
+    }
+
+    
+    public boolean setVideoLocation(int x, int y, int w, int h) {
+        return false;
+    }
+
+
+    public byte[] getVideoSnapshot(String imageType) throws MediaException {
+        throw new MediaException( "Not supported" );
+    }
+
+
+    public boolean setVideoFullScreen(boolean fullscreen) {
+        return false;
+    }
+
+
+    public boolean setVideoVisible(boolean visible) {
+        return true;
+    }
+
+
+    public boolean setColorKey(boolean on, int colorKey) {
+        return false;
+    }
+
+
+    public void notifyDisplaySizeChange() {}
+
+}
