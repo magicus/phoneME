3f393f7d6375abedc856e0ab85206761f3c3c8d9
andrewzakonov <andrewzakonov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-06-12 11:03:21 +0000

    Fixing commit (r18372). Two files were forgotten. CR 6833862 : Add support of javascript's alert, prompt, and confirm functions Merged -r18239:18369 from abstractions-cr-6833862-3 Reviewed by Natalia Medvedenko, Alexey Petkel, thanks!

--- src/cdc_share/classes/com/sun/j2me/dialog/MessageDialog.java
+++ src/cdc_share/classes/com/sun/j2me/dialog/MessageDialog.java
@@ -26,9 +26,6 @@
 
 package com.sun.j2me.dialog;
 
-import com.sun.j2me.i18n.Resource;
-import com.sun.j2me.i18n.ResourceConstants;
-
 import com.sun.j2me.security.Token;
 
 /**
@@ -85,116 +82,6 @@ public class MessageDialog {
             }
             catch (NumberFormatException e) {
                 System.out.println("Value for chosen item is incorrect");
-            }
-        }
-    }
-
-    /**
-     * Displays dialog with new PIN parameters.
-     * @param token security token
-     * @return array of strings or null if cancelled. Array contains new
-     * PIN label and PIN value.
-     * @throws InterruptedException  if interrupted
-     */
-    public static String[] enterNewPIN(Token token)
-            throws InterruptedException {
-        String label = ""; /* New pin's label */
-        String pin1 = "";  /* New pin's value */
-        String pin2 = "";  /* Confirmation of new pin's value */
-        
-        Dialog d = new Dialog(
-            // "New PIN", 
-            Resource.getString(
-                ResourceConstants.JSR177_PINDIALOG_TITLE_NEWPIN),
-            true);
-
-        while (true) {
-            System.out.println(// "PIN label ", 
-                Resource.getString(
-                    ResourceConstants.JSR177_PINDIALOG_LABEL));
-            label = d.append();
-
-            System.out.println(// "Enter PIN ", 
-                Resource.getString(
-                    ResourceConstants.JSR177_PINDIALOG_ENTERPIN));
-            pin1 = d.append();
-
-            System.out.println(// "Confirm PIN ", 
-                Resource.getString(
-                    ResourceConstants.JSR177_PINDIALOG_CONFIRMPIN));
-            pin2 = d.append();
-
-            if (d.waitForAnswer() == Dialog.CANCELLED) {
-                return null;
-            }
-
-            if (label.equals("")) {
-                System.out.println("Label is invalid");
-                continue;
-            }
-
-            if (pin1.equals("") ||
-                !pin1.equals(pin2) ||
-                pin1.length() < 4) {
-                System.out.println("Pins' values are invalid");
-                continue;
-            }
-            return new String[] {label, pin1};
-        }
-    }
-
-    /**
-     * Displays dialog with new PIN parameters.
-     * @param title dialog title
-     * @param label1 PIN1 label
-     * @param pin1IsNumeric PIN1 is a number
-     * @param pin1Length length of the PIN1 text field
-     * @param label2 PIN2 label
-     * @param pin2IsNumeric PIN2 is a numbere     
-     * @param pin2Length length of the PIN2 text field
-     * @param token security token
-     * @return null if PIN entry was cancelled by user, otherwise an array
-     * containing PIN value(s).
-     * @throws InterruptedException  if interrupted
-     */
-    public static Object[] enterPins(String title,
-                      String label1, boolean pin1IsNumeric, int pin1Length,
-                      String label2, boolean pin2IsNumeric, int pin2Length,
-                      Token token)
-           throws InterruptedException {
-        String data1 = "";  /* First pin to enter */
-        String data2 = "";  /* Second pin to enter */
-        
-        Dialog d = new Dialog(title, true);
-
-        while (true) {
-            System.out.println(label1);
-            data1 = d.append();
-
-            if (pin2Length != 0) {
-                System.out.println(label2);
-                data2 = d.append();
-            }
-
-            if (d.waitForAnswer() == Dialog.CANCELLED) {
-                return null;
-            }
-
-            if (data1.equals("")) {
-                System.out.println("Pin1 value is invalid");
-                continue;
-            }
-
-            if (pin2Length != 0) {
-                if (data2.equals("")) {
-                    System.out.println("Pin2 value is invalid");
-                    continue;
-                }
-
-                return new Object[] { data1, data2 };
-            }
-            else {
-                return new Object[] { data1 };
             }
         }
     }
