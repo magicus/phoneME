fd387aa4c1e21f7c7d05614b6acc73e95936a049
IlyaKrupenin <IlyaKrupenin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-08-25 11:43:08 +0000

    added jpeg snapshot support

--- implementation/win32_emul/jsr135_mmapi/fake_camera.c
+++ implementation/win32_emul/jsr135_mmapi/fake_camera.c
@@ -55,11 +55,14 @@ typedef struct _fake_camera
 
     long                  video_width;
     long                  video_height;
-    javacall_pixel*       video_frame;
+    javacall_uint8*       video_frame;
 
     int                   out_width;
     int                   out_height;
     javacall_pixel*       out_frame;
+
+    javacall_uint8*       snapshot;
+    javacall_uint32       snapshot_len;
 
     volatile BOOL         playing;
     BOOL                  visible;
@@ -68,68 +71,70 @@ typedef struct _fake_camera
     CRITICAL_SECTION      cs;
 } fake_camera;
 
+static javacall_pixel rgb2pixel( javacall_uint8 r, 
+                                 javacall_uint8 g,
+                                 javacall_uint8 b )
+{
+    return    ( ((javacall_pixel)b >> 3) & 0x1f)
+           | (( ((javacall_pixel)g >> 2) & 0x3f) << 5 )
+           | (( ((javacall_pixel)r >> 3) & 0x1f) << 11);
+}
+
 static void fake_camera_prepare_scaled_frame( fake_camera* c )
 {
     int x, y, srcx, srcy;
 
+    double kx = (double)(c->video_width) / (double)(c->out_width);
+    double ky = (double)(c->video_height) / (double)(c->out_height);
+    javacall_uint8* p;
+
     if( NULL == c->out_frame ) 
         c->out_frame = (javacall_pixel*)MALLOC( sizeof(javacall_pixel) * c->out_width * c->out_height );
 
-    if( c->out_width == c->video_width && c->out_height == c->video_height )
+    for( y = 0; y < c->out_height; y++ )
     {
-        memcpy( c->out_frame, c->video_frame, 
-                sizeof( javacall_pixel ) * c->video_width * c->video_height );
-    }
-    else
-    {
-        double kx = (double)(c->video_width) / (double)(c->out_width);
-        double ky = (double)(c->video_height) / (double)(c->out_height);
-
-        for( y = 0; y < c->out_height; y++ )
+        for( x = 0; x < c->out_width; x++ )
         {
-            for( x = 0; x < c->out_width; x++ )
-            {
-                srcx = (int)( 0.5 + kx * x );
-                srcy = (int)( 0.5 + ky * y );
-                *( c->out_frame + y * c->out_width + x )
-                    = *( c->video_frame + srcy * c->video_width + srcx );
-            }
+            srcx = (int)( 0.5 + kx * x );
+            srcy = (int)( 0.5 + ky * y );
+            p = c->video_frame + 3 * ( srcy * c->video_width + srcx );
+            *( c->out_frame + y * c->out_width + x )
+                = rgb2pixel( *(p++), *(p++), *(p++) );
         }
     }
 }
 
 static void fake_camera_frame_ready( fake_camera* c )
 {
-    EnterCriticalSection( &(c->cs) );
-
     if( c->visible )
     {
         fake_camera_prepare_scaled_frame( c );
         lcd_output_video_frame( c->out_frame );
     }
-
-    LeaveCriticalSection( &(c->cs) );
 }
 
 static void fake_camera_generator_thread( void* param )
 {
     int x, y;
+    javacall_uint8* p;
     fake_camera* c = (fake_camera*)param;
 
     while( c->playing )
     {
+        EnterCriticalSection( &(c->cs) );
         for( x = 0; x < c->video_width; x++ )
         {
             for( y = 0; y < c->video_height; y++ )
             {
-                *( c->video_frame + y * c->video_width + x )
-                    = RGB2PIXELTYPE( rand() & 0xFF, 
-                                     rand() & 0xFF, 
-                                     rand() & 0xFF );
+                p = c->video_frame + 3 * ( y * c->video_width + x );
+                *(p++) = rand() & 0xFF;
+                *(p++) = rand() & 0xFF;
+                *(p++) = rand() & 0xFF;
             }
         }
 
         fake_camera_frame_ready( c );
+        LeaveCriticalSection( &(c->cs) );
 
         Sleep( 50 );
     }
@@ -147,10 +152,12 @@ static javacall_result fake_camera_create(javacall_impl_player* outer_player)
     c->out_width   = c->video_width  = 160;
     c->out_height  = c->video_height = 120;
 
-    c->video_frame = (javacall_pixel*)MALLOC( sizeof(javacall_pixel) 
-                                              * c->video_width 
-                                              * c->video_height );
+    c->video_frame = (javacall_uint8*)MALLOC( 3 * c->video_width 
+                                                * c->video_height );
     c->out_frame   = NULL;
+
+    c->snapshot     = NULL;
+    c->snapshot_len = 0;
 
     c->playing     = FALSE;
     c->visible     = FALSE;
@@ -179,6 +186,8 @@ static javacall_result fake_camera_destroy(javacall_handle handle)
         lcd_output_video_frame( NULL );
         FREE( c->out_frame );
     }
+
+    if( NULL != c->snapshot ) javacall_media_release_data( c->snapshot, c->snapshot_len );
 
     DeleteCriticalSection( &(c->cs) );
     FREE( c );
@@ -280,7 +289,7 @@ static javacall_result fake_camera_run(javacall_handle handle)
 
 static javacall_result fake_camera_get_time(javacall_handle handle, javacall_int32* ms)
 {
-    fake_camera* ñ = (fake_camera*)handle;
+    fake_camera* c = (fake_camera*)handle;
     *ms = -1;
     PRINTF( "--- get_time: %ld",*ms );
     return JAVACALL_OK;
@@ -314,52 +323,52 @@ static javacall_result fake_camera_switch_to_background(javacall_handle handle,
 
 static javacall_result fake_camera_get_video_size(javacall_handle handle, long* width, long* height)
 {
-    fake_camera* ñ = (fake_camera*)handle;
+    fake_camera* c = (fake_camera*)handle;
 
-    *width  = ñ->video_width;
-    *height = ñ->video_height;
+    *width  = c->video_width;
+    *height = c->video_height;
 
     return JAVACALL_OK;
 }
 
 static javacall_result fake_camera_set_video_visible(javacall_handle handle, javacall_bool visible)
 {
-    fake_camera* ñ = (fake_camera*)handle;
+    fake_camera* c = (fake_camera*)handle;
 
-    EnterCriticalSection( &(ñ->cs) );
-    ñ->visible = ( JAVACALL_TRUE == visible );
+    EnterCriticalSection( &(c->cs) );
+    c->visible = ( JAVACALL_TRUE == visible );
     if( visible )
     {
-        if( NULL != ñ->out_frame ) lcd_output_video_frame( ñ->out_frame );
+        if( NULL != c->out_frame ) lcd_output_video_frame( c->out_frame );
     }
     else
     {
         lcd_output_video_frame( NULL );
     }
-    LeaveCriticalSection( &(ñ->cs) );
+    LeaveCriticalSection( &(c->cs) );
 
     return JAVACALL_OK;
 }
 
 static javacall_result fake_camera_set_video_location(javacall_handle handle, long x, long y, long w, long h)
 {
-    fake_camera* ñ = (fake_camera*)handle;
+    fake_camera* c = (fake_camera*)handle;
     javacall_pixel* old_frame;
 
-    EnterCriticalSection( &(ñ->cs) );
+    EnterCriticalSection( &(c->cs) );
 
-    if( ñ->out_width != w || ñ->out_height != h )
+    if( c->out_width != w || c->out_height != h )
     {
-        ñ->out_width  = w;
-        ñ->out_height = h;
+        c->out_width  = w;
+        c->out_height = h;
 
-        old_frame = ñ->out_frame;
-        ñ->out_frame = NULL;
+        old_frame = c->out_frame;
+        c->out_frame = NULL;
 
-        if( NULL != ñ->video_frame ) fake_camera_prepare_scaled_frame( ñ );
+        if( NULL != c->video_frame ) fake_camera_prepare_scaled_frame( c );
 
         lcd_set_video_rect( x, y, w, h );
-        lcd_output_video_frame( ñ->out_frame );
+        lcd_output_video_frame( c->out_frame );
 
         if( NULL != old_frame ) FREE( old_frame );
     }
@@ -368,7 +377,7 @@ static javacall_result fake_camera_set_video_location(javacall_handle handle, lo
         lcd_set_video_rect( x, y, w, h );
     }
 
-    LeaveCriticalSection( &(ñ->cs) );
+    LeaveCriticalSection( &(c->cs) );
 
     return JAVACALL_OK;
 }
@@ -382,6 +391,56 @@ static javacall_result fake_camera_set_video_alpha(javacall_handle handle, javac
 static javacall_result fake_camera_set_video_fullscreenmode(javacall_handle handle, javacall_bool fullScreenMode)
 {
     return JAVACALL_FAIL;
+}
+
+/*****************************************************************************\
+                      S N A P S H O T   S U P P O R T
+\*****************************************************************************/
+
+static javacall_result fake_camera_start_video_snapshot( javacall_handle handle, 
+                                                         const javacall_utf16* imageType, 
+                                                         long length )
+{
+    javacall_result res;
+    fake_camera* c = (fake_camera*)handle;
+
+    EnterCriticalSection( &(c->cs) );
+
+    res = javacall_media_encode_start( c->video_frame,
+                                       (javacall_uint8)c->video_width,
+                                       (javacall_uint8)c->video_height,
+                                       JAVACALL_JPEG_ENCODER,
+                                       100, // javacall_uint8 quality,
+                                       &( c->snapshot ),
+                                       &( c->snapshot_len ),
+                                       NULL );
+
+    LeaveCriticalSection( &(c->cs) );
+
+    return res;
+}
+
+static javacall_result fake_camera_get_video_snapshot_data_size( javacall_handle handle, 
+                                                                 long* size )
+{
+    fake_camera* c = (fake_camera*)handle;
+
+    if( NULL == c->snapshot ) return JAVACALL_FAIL;
+
+    *size = c->snapshot_len;
+    return JAVACALL_OK;
+}
+
+static javacall_result fake_camera_get_video_snapshot_data( javacall_handle handle, 
+                                                            char* buffer,
+                                                            long size )
+{
+    fake_camera* c = (fake_camera*)handle;
+
+    if( NULL == c->snapshot ) return JAVACALL_FAIL;
+    if( NULL == buffer || size < c->snapshot_len ) return JAVACALL_INVALID_ARGUMENT;
+    memcpy( buffer, c->snapshot, c->snapshot_len );
+    return JAVACALL_OK;
 }
 
 /*****************************************************************************\
@@ -404,9 +463,11 @@ static media_basic_interface _fake_camera_basic_itf =
     NULL,
     NULL,
     NULL,
+
     fake_camera_get_time,
     fake_camera_set_time,
     fake_camera_get_duration,
+
     fake_camera_switch_to_foreground,
     fake_camera_switch_to_background
 };
@@ -418,17 +479,23 @@ static media_video_interface _fake_camera_video_itf = {
     fake_camera_set_video_alpha,
     fake_camera_set_video_fullscreenmode
 };
+    
+static media_snapshot_interface _fake_camera_snapshot_itf = {
+    fake_camera_start_video_snapshot,
+    fake_camera_get_video_snapshot_data_size,
+    fake_camera_get_video_snapshot_data
+};
 
 media_interface g_fake_camera_itf =
 {
     &_fake_camera_basic_itf,
     NULL,
     &_fake_camera_video_itf,
+    &_fake_camera_snapshot_itf,
     NULL,
     NULL,
     NULL,
     NULL,
-    NULL, //&_fake_camera_rate_itf
     NULL,
     NULL,
     NULL,
