9730a3869bfa1def5205b88dce612d9dc085056a
tbrandalik <tbrandalik@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-10-12 12:33:53 +0000

    correct parameter in KNI_GetstringLength and add 0 ending for a string.

--- src/cldc_application/core/native/jsr211_registry_impl.c
+++ src/cldc_application/core/native/jsr211_registry_impl.c
@@ -143,7 +143,7 @@ jsr211_result jsr211_finalize(void){
  * @return JSR211_OK if content handler registered successfully
  */
 jsr211_result jsr211_register_handler(const jsr211_content_handler* ch) {
-    javacall_utf16 JAVA_APPLICATION[] = {'J','a','v','a',' ','A','p','p','l','i','c','a','t','i','o','n'};
+    javacall_utf16 JAVA_APPLICATION[] = {'J','a','v','a',' ','A','p','p','l','i','c','a','t','i','o','n', 0};
     jsr211_result status = JSR211_FAILED;
     javacall_utf16_string *types = NULL;
     javacall_utf16_string *suffixes = NULL;
