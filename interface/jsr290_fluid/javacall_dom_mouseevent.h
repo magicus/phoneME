d6d2c6a04defa523f1e00c0b0e61ebb68de49dd1
vmorgunov <vmorgunov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-10-22 07:02:02 +0000

    CR 6835483, 6811939 bug fix; Reviewed by Natalia Medvedenko, Alexey Konstantinov

--- interface/jsr290_fluid/javacall_dom_mouseevent.h
+++ interface/jsr290_fluid/javacall_dom_mouseevent.h
@@ -618,12 +618,10 @@ javacall_dom_mouseevent_init_mouse_event_finish(void *context);
  *    Refer to the 
  *   <code>MouseEvent.initMouseEvent()</code> method for a description 
  *   of this parameter. 
- * @param modifiers_list  A 
- *   <a href='http://www.w3.org/TR/2004/REC-xml-20040204/#NT-S'>
- *   white space</a> separated list of modifier key identifiers to be 
- *   activated on this object. As an example, <code>"Control Alt"</code> 
- *   will activated the control and alt modifiers.
- *
+ * @param ctrl_key_arg  Specifies <code>MouseEvent.ctrlKey</code>. 
+ * @param alt_key_arg  Specifies <code>MouseEvent.altKey</code>. 
+ * @param shift_key_arg  Specifies <code>MouseEvent.shiftKey</code>. 
+ * @param meta_key_arg  Specifies <code>MouseEvent.metaKey</code>. 
  * 
  * @return JAVACALL_OK if all done successfuly,
  *         JAVACALL_FAIL if error in native code occured
@@ -650,7 +648,10 @@ javacall_dom_mouseevent_init_mouse_event_ns_start(javacall_handle handle,
                                                   javacall_int32 client_y_arg,
                                                   javacall_int16 button_arg,
                                                   javacall_handle related_target_arg,
-                                                  javacall_const_utf16_string modifiers_list);
+                                                  javacall_bool ctrl_key_arg,
+                                                  javacall_bool alt_key_arg,
+                                                  javacall_bool shift_key_arg,
+                                                  javacall_bool meta_key_arg);
 
 /**
  * Forms request to the native engine and returns with JAVACALL_WOULD_BLOCK code 
@@ -659,11 +660,10 @@ javacall_dom_mouseevent_init_mouse_event_ns_start(javacall_handle handle,
  * as <code>UIEvent.initUIEventNS()</code>. 
  * 
  * @param context The context saved during asynchronous operation.
- *   <a href='http://www.w3.org/TR/2004/REC-xml-20040204/#NT-S'>
- *   white space</a> separated list of modifier key identifiers to be 
- *   activated on this object. As an example, <code>"Control Alt"</code> 
- *   will activated the control and alt modifiers.
- *
+ * @param ctrl_key_arg  Specifies <code>MouseEvent.ctrlKey</code>. 
+ * @param alt_key_arg  Specifies <code>MouseEvent.altKey</code>. 
+ * @param shift_key_arg  Specifies <code>MouseEvent.shiftKey</code>. 
+ * @param meta_key_arg  Specifies <code>MouseEvent.metaKey</code>. 
  * 
  * @return JAVACALL_OK if all done successfuly,
  *         JAVACALL_FAIL if error in native code occured
